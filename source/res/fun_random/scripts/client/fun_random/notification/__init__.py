a9753349aefaa40db93635b1fcdb4288b775afaf
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:48 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/notification/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/notification/__init__.py
@@ -1,3 +1,18 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/notification/__init__.py
-pass
+# This is the initialization file for the fun\_random notification system.
+# It does not contain any directly executable code, but instead sets up
+# the necessary modules and namespaces for the rest of the notification system.
+
+# fun_random/scripts/client/fun_random/notification/__init__.py
+# This file is part of the fun_random project.
+
+# Imports
+# No code in this file imports any external modules or functions.
+
+# Functions
+# There are no functions defined in this file.
+
+# Classes
+# There are no classes defined in this file.
+
+# Global variables
+# There are no global variables defined in this file.

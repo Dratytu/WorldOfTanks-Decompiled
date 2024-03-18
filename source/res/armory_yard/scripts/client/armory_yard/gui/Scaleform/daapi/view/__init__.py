cf96dea09f36486d9b9afe55172c0d94e2565dd8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:07 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/Scaleform/daapi/view/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/Scaleform/daapi/view/__init__.py
@@ -1,3 +1,7 @@
 # Python bytecode 3.7
+# This script is for the Armory Yard project.
+
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/Scaleform/daapi/view/__init__.py
 
+# The file doesn't contain any code, but it's likely serving as an entry point
+# for organizing the module structure of the gui.Scaleform.daapi.view package.

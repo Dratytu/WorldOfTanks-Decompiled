838b02eb078d5dd0b55571c418f7c8d32535339f
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:37 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/BCTooltipsWindowMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/BCTooltipsWindowMeta.py
@@ -1,20 +1,47 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/BCTooltipsWindowMeta.py
-from gui.Scaleform.framework.entities.View import View
+
+import sys # Imported, but not used in the code
+from gui.Scaleform.framework.entities.View import View # Inherited from View class
 
 class BCTooltipsWindowMeta(View):
+    """
+    A meta class for the BCTooltipsWindow, which is a view for displaying tooltips in the game.
+    """
 
     def animFinish(self):
+        """
+        A method that calls the _printOverrideError method with the argument 'animFinish'.
+        This method is intended to be overridden in a subclass.
+        """
         self._printOverrideError('animFinish')
 
     def as_setRotateTipVisibilityS(self, Visible):
+        """
+        A method that returns the result of as_setRotateTipVisibility(Visible) method call on the flashObject.
+        This method is used for setting the visibility of the rotate tip.
+
+        :param Visible: A boolean value indicating whether the rotate tip should be visible or not.
+        :return: None
+        """
         return self.flashObject.as_setRotateTipVisibility(Visible) if self._isDAAPIInited() else None
 
     def as_showHandlerS(self):
+        """
+        A method that returns the result of as_showHandler() method call on the flashObject.
+        This method is used for showing the tooltip.
+
+        :return: None
+        """
         return self.flashObject.as_showHandler() if self._isDAAPIInited() else None
 
     def as_completeHandlerS(self):
+        """
+        A method that returns the result of as_completeHandler() method call on the flashObject.
+        This method is used for handling the completion of the tooltip animation.
+
+        :return: None
+        """
         return self.flashObject.as_completeHandler() if self._isDAAPIInited() else None
 
-    def as_hideHandlerS(self):
-        return self.flashObject.as_hideHandler() if self._isDAAPIInited() else None
+   

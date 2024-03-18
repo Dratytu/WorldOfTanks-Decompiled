9edc69da94dd88b54771850d6e0b768354321362
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:02 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/framework/managers/TextManager.py
+++ source/res/scripts/client/gui/Scaleform/framework/managers/TextManager.py
@@ -1,29 +1,33 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/framework/managers/TextManager.py
-from debug_utils import LOG_ERROR
-from gui.Scaleform.framework.entities.abstract.TextManagerMeta import TextManagerMeta
-from gui.Scaleform.genConsts.TEXT_MANAGER_STYLES import TEXT_MANAGER_STYLES as _TMS
-from gui.shared.formatters import text_styles
 
+import debug_utils
+from gui.Scaleform.framework.entities.abstract.TextManagerMeta import TextManagerMeta  # Importing the base class for the TextManager.
+from gui.Scaleform.genConsts.TEXT_MANAGER_STYLES import TEXT_MANAGER_STYLES as _TMS  # Importing the predefined text styles.
+from gui.shared.formatters import text_styles  # Importing the module that contains text style formatting functions.
+
+# Subclassing the base TextManagerMeta class to create a custom text manager.
 class TextManager(TextManagerMeta):
 
     def __init__(self):
         super(TextManager, self).__init__()
-        self.__styles = text_styles.getRawStyles([ v for k, v in _TMS.__dict__.iteritems() if not k.startswith('_') ])
+        self.__styles = text_styles.getRawStyles([v for k, v in _TMS.__dict__.iteritems() if not k.startswith('_')])  # Initializing the styles dictionary with the predefined text styles.
 
+    # A method to retrieve a text style by its name.
     def getTextStyle(self, style):
         if style in self.__styles:
             result = self.__styles[style]
         else:
-            LOG_ERROR('Style is not found', style)
+            debug_utils.LOG_ERROR('Style is not found', style)  # Logging an error if the requested style is not found.
             result = ''
         return result
 
     def _dispose(self):
         self.__styles.clear()
-        super(TextManager, self)._dispose()
+        super(TextManager, self)._dispose()  # Disposing of the base TextManagerMeta instance.
 
 
+# Defining a class for text icons.
 class TextIcons(object):
     CHECKMARK_ICON = 'checkmark'
     NUT_ICON = 'nut'
@@ -51,17 +55,4 @@ class TextIcons(object):
      INFO_ICON,
      PREMIUM_IGR_SMALL,
      PREMIUM_IGR_BIG,
-     ORDER_IN_PROGRESS_ICON,
-     CLOCK_ICON,
-     CHECKMARK_ICON,
-     NOT_AVAILABLE,
-     LEVEL_5,
-     LEVEL_10,
-     SWORDS,
-     HUMANS,
-     CREDITS,
-     GOLD,
-     XP,
-     FREE_XP,
-     ARROW_BUTTON,
-     NO_SEASON)
+    

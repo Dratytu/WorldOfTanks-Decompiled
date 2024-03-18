7a18ca96fa737fbe4c7d1c70fe18c49c132eeaf5
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:20 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/carousel/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/carousel/__init__.py
@@ -1,3 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/carousel/__init__.py
-pass
+
+# This script defines the initialization of the Battle Royale carousel view in the hangar lobby.
+# The carousel view is responsible for displaying a rotating set of customizable vehicle options.
+# However, this particular script does not contain any code and only imports the necessary modules.

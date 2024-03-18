e85eb02ee71c4596f70f2e116b155d2d41f0ffed
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:34 +0300

    improve

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/status_notifications/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/status_notifications/__init__.py
@@ -1,3 +1,2 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/status_notifications/__init__.py
-pass
+# Python 2.7
+#

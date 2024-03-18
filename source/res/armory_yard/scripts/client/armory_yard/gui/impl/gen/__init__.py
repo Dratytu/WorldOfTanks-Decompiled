ed1f549a28e0e1f0e0163fea9c3ce61b29b528a2
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:11 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/__init__.py
@@ -1,3 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/gen/__init__.py
-pass
+
+# This script is an empty file that serves as a placeholder for generated code
+# in the 'armory_yard.gui.impl.gen' package.
+
+# Generated code, such as UI scripts, will be added to this file automatically
+# during the build process.
+
+# It is not recommended to modify this file manually, as any changes made here
+# may be overwritten during the build process.
+
+# If you need to customize the generated code, consider creating a new file
+# in a different location and importing the necessary modules from the
+# 'armory_yard.gui.impl.gen' package.
+

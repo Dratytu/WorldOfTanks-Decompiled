b83c13696b2cc086c99ce9d39c1ecef239f088d9
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:17 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/comp7/markers2d/settings.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/comp7/markers2d/settings.py
@@ -1,6 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/comp7/markers2d/settings.py
 
-
 class Comp7markersSymbolsNames(object):
+    """
+    A class containing the symbol names for Comp7 markers used in 2D battles.
+
+    This class is used to define the symbol names for various Comp7 markers used in 2D battles.
+    These symbol names are used in the Scaleform UI to display the markers on the battle screen.
+    """
     COMP7_VEHICLE_MARKER = 'Comp7VehicleMarkerUI'
+    """
+    The symbol name for the Comp7 vehicle marker.
+
+    This symbol name is used to display the vehicle marker for players in the Comp7 battle mode.
+    """

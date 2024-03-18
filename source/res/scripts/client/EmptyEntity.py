a2792c7d7bc4c7875ff2aec11f9ecfb763ad5c8c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:19 +0300

    comment

--- source/res/scripts/client/EmptyEntity.py
+++ source/res/scripts/client/EmptyEntity.py
@@ -1,12 +1,21 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/EmptyEntity.py
+
 import BigWorld
 
+# Define a class 'EmptyEntity' that inherits from 'BigWorld.Entity'
 class EmptyEntity(BigWorld.Entity):
 
+    # Constructor method for the 'EmptyEntity' class
     def __init__(self):
+        # Call the constructor of the parent class 'BigWorld.Entity'
         super(EmptyEntity, self).__init__()
+
+        # Initialize an 'AvatarFilter' object and assign it to 'self.filter'
         self.filter = BigWorld.AvatarFilter()
 
+    # Define a method 'onLeaveWorld' for the 'EmptyEntity' class
     def onLeaveWorld(self):
+        # A placeholder for any code that needs to be executed when the entity leaves the world
         pass
+

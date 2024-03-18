ae82da694780750207b19fe98f06ccf8dabc0e57
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:34 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/trade_in/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/trade_in/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/trade_in/__init__.py
-pass
+
+# This is an empty __init__.py file, which is used to mark this directory as a package
+# in Python. It does not contain any code and does not have any functionality on its own.
+
+# The package name is "scripts/client/gui/Scaleform/daapi/view/lobby/trade_in".
+# This suggests that it is part of a larger application related to the game client,
+# and that it contains views related to the lobby and a feature called "trade_in".

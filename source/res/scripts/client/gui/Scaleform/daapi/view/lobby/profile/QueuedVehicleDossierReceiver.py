cbe44a36f63672ddfafcd8cd96e9a4f74ba1c054
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:38 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/profile/QueuedVehicleDossierReceiver.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/profile/QueuedVehicleDossierReceiver.py
@@ -1,24 +1,34 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/profile/QueuedVehicleDossierReceiver.py
-from Event import Event
-from adisp import adisp_process
-from debug_utils import LOG_ERROR
-from helpers import dependency
-from skeletons.gui.shared import IItemsCache
-MIN_INTERVAL_BETWEEN_INVOKES = 300
+
+import time
+from Event import Event  # A class to create and handle custom events.
+from adisp import adisp_process  # A decorator to process asynchronous tasks.
+from debug_utils import LOG_ERROR  # A function to log errors.
+from helpers import dependency  # A decorator to declare dependencies.
+from skeletons.gui.shared import IItemsCache  # An interface for shared gui components.
+
+# Constants
+MIN_INTERVAL_BETWEEN_INVOKES = 300  # Minimum time interval between requests in milliseconds.
 
 class QueuedVehicleDossierReceiver(object):
-    itemsCache = dependency.descriptor(IItemsCache)
+    itemsCache = dependency.descriptor(IItemsCache)  # Dependency injection for IItemsCache.
 
     def __init__(self):
         super(QueuedVehicleDossierReceiver, self).__init__()
-        self.__isUnderRequesting = False
-        self.__queuedVehicleData = None
-        self.__lastInvokeTime = 0
-        self.onDataReceived = Event()
+        self.__isUnderRequesting = False  # A flag to check if a request is already in progress.
+        self.__queuedVehicleData = None  # A container for queued vehicle data when a request is in progress.
+        self.__lastInvokeTime = 0  # The timestamp of the last request.
+        self.onDataReceived = Event()  # An event triggered when data is received.
         return
 
     def invoke(self, databaseID, vehicleID):
+        """
+        Initiates a request for vehicle dossier data.
+
+        :param databaseID: The unique identifier of the user.
+        :param vehicleID: The unique identifier of the vehicle.
+        """
         if self.__isUnderRequesting:
             self.__queuedVehicleData = (databaseID, vehicleID)
         else:
@@ -26,6 +36,12 @@ class QueuedVehicleDossierReceiver(object):
 
     @adisp_process
     def __requestData(self, databaseID, vehicleID):
+        """
+        Asynchronously requests vehicle dossier data.
+
+        :param databaseID: The unique identifier of the user.
+        :param vehicleID: The unique identifier of the vehicle.
+        """
         self.__isUnderRequesting = True
         vehDossier = yield self.itemsCache.items.requestUserVehicleDossier(int(databaseID), vehicleID)
         if vehDossier:
@@ -35,11 +51,4 @@ class QueuedVehicleDossierReceiver(object):
         self.__isUnderRequesting = False
         if self.__queuedVehicleData is not None:
             dbId = self.__queuedVehicleData[0]
-            vehId = self.__queuedVehicleData[1]
-            self.__queuedVehicleData = None
-            self.invoke(dbId, vehId)
-        return
-
-    def dispose(self):
-        self.__queuedVehicleData = None
-        return
+            veh

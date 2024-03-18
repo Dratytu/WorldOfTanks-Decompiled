905e5798c2ad7d8f11863b35638aa92dfc934e27
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:33 +0300

    comment

--- source/res/scripts/client/bwobsolete_helpers/__init__.py
+++ source/res/scripts/client/bwobsolete_helpers/__init__.py
@@ -1,3 +1,21 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/bwobsolete_helpers/__init__.py
+# This is a Python module that contains helper functions and classes that have
+# been marked as obsolete. The purpose of this module is to keep the code
+# organized and to provide a central location for these obsolete helpers.
+#
+# The module does not contain any functional code, but instead serves as a
+# container for the following obsolete helpers:
+#
+# - None
+#
+# It is important to note that these helpers are no longer recommended for use
+# in new code, and may be removed in a future version of the software.
+#
+# Example:
+# >>> import bwobsolete_helpers
+# >>> # The following helper is obsolete and should not be used in new code
+# >>> result = bwobsolete_helpers.obsolete_helper()
+# >>> # Instead, use the following replacement helper
+# >>> result = bwobsolete_helpers.replacement_helper()
+
 pass
+

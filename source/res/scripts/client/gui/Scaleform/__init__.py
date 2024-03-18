eb5fd42e3a3728541ac4306a53a43f1830466d76
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:35 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/__init__.py
+++ source/res/scripts/client/gui/Scaleform/__init__.py
@@ -1,60 +1,10 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/gui/Scaleform/__init__.py
-import os
-from gui.Scaleform.locale.MENU import MENU
-from nations import NAMES
-from skeletons.gui.system_messages import ISystemMessages
-from skeletons.gui.techtree_events import ITechTreeEventsListener
-SCALEFORM_SUPPORT = False
-try:
-    import _Scaleform
-    SCALEFORM_SUPPORT = True
-except ImportError:
-    raise NotImplementedError('Client not support Scaleform')
-
+# Constants for Scaleform SWF path and various icon directories
 SCALEFORM_SWF_PATH_V3 = 'gui/flash'
 VEHICLE_TYPES_ICONS_DIR_PATH = '../maps/icons/filters/tanks/'
 NATION_FILTER_ICONS_DIR_PATH = '../maps/icons/filters/nations/'
 BUTTON_FILTER_ICONS_DIR_PARH = '../maps/icons/library/'
 LEVEL_FILTER_ICONS_DIR_PARH = '../maps/icons/filters/levels/'
-NATIONS_ICON_FILENAME = '../maps/icons/nations/%s_%s.%s'
-NATION_ICON_PREFIX_131x31 = '131x31'
-DEFAULT_VIDEO_BUFFERING_TIME = 2.0
 
+# Function to get the vehicle type asset path
 def getVehicleTypeAssetPath(vehicleType, extension='.png'):
-    return ''.join([VEHICLE_TYPES_ICONS_DIR_PATH, vehicleType, extension])
-
-
-def getButtonsAssetPath(button, extension='.png'):
-    return ''.join((BUTTON_FILTER_ICONS_DIR_PARH, button, extension))
-
-
-def getNationsFilterAssetPath(nationName, extension='.png'):
-    return ''.join((NATION_FILTER_ICONS_DIR_PATH, nationName, extension))
-
-
-def getNationsAssetPath(nation, namePrefix='', extension='png'):
-    return NATIONS_ICON_FILENAME % (NAMES[nation], namePrefix, extension)
-
-
-def getLevelsAssetPath(level_str, extension='.png'):
-    return ''.join([LEVEL_FILTER_ICONS_DIR_PARH, level_str, extension])
-
-
-def getNecessaryArenaFrameName(arenaSubType, hasBase=None):
-    return '{0}{1}'.format('assault', '1' if hasBase else '2') if arenaSubType.startswith('assault') else arenaSubType
-
-
-def getPathForFlash(path, base=SCALEFORM_SWF_PATH_V3):
-    return os.path.relpath(path, base)
-
-
-def getScaleformConfig(manager):
-    from gui.Scaleform.SystemMessagesInterface import SystemMessagesInterface
-    messages = SystemMessagesInterface()
-    messages.init()
-    manager.addInstance(ISystemMessages, messages, finalizer='destroy')
-    from gui.Scaleform.daapi.view.lobby.techtree.techtree_events import TechTreeEventsListener
-    listener = TechTreeEventsListener()
-    listener.init()
-    manager.addInstance(ITechTreeEventsListener, listener, finalizer='fini')
+    # Returns the path for the given vehicle type and extension

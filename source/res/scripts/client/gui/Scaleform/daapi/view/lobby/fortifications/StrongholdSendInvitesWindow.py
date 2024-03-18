aa4e9e729d712f19de72f9ba94d790bd49d3711e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:14 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/fortifications/StrongholdSendInvitesWindow.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/fortifications/StrongholdSendInvitesWindow.py
@@ -1,5 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/fortifications/StrongholdSendInvitesWindow.py
+
+# Import necessary modules
 from functools import partial
 from gui.prb_control.entities.base.external_battle_unit.base_external_battle_ctx import SendInvitesExternalBattleUnitCtx
 from gui.Scaleform.daapi.view.lobby.SendInvitesWindow import SendInvitesWindow
@@ -9,13 +11,21 @@ from client_request_lib.exceptions import ResponseCodes
 from gui import SystemMessages
 from gui.Scaleform.locale.INVITES import INVITES
 
+# Define the StrongholdSendInvitesWindow class, which inherits from SendInvitesWindow and UsersInfoHelper
 class StrongholdSendInvitesWindow(SendInvitesWindow, UsersInfoHelper):
 
+    # Implement the sendInvites method, which takes accountsToInvite and comment as arguments
     def sendInvites(self, accountsToInvite, comment):
+        # Check if accountsToInvite is not empty
         if accountsToInvite:
+            # Request the send invites action using the prbEntity
             self.prbEntity.request(SendInvitesExternalBattleUnitCtx(accountsToInvite, comment), partial(self.__sendInvitesCallback, accountsToInvite))
 
+    # Implement the __sendInvitesCallback method, which takes accountsToInvite and response as arguments
     def __sendInvitesCallback(self, accountsToInvite, response):
+        # Check if response is an instance of Response and its code is NO_ERRORS
         if isinstance(response, Response) and response.getCode() == ResponseCodes.NO_ERRORS:
+            # Iterate through the accountsToInvite list
             for userId in accountsToInvite:
-                SystemMessages.pushI18nMessage(INVITES.STRONGHOLD_INVITE_SENDINVITETOUSERNAME, type=SystemMessages.SM_TYPE.Information, name=self.getUserName(userId))
+                # Push a system message indicating the invite has been sent to the user
+                SystemMessages.pushI18nMessage(INVITES.STRONGHOLD_INVITE_SENDINVITETOUSERNAME, type

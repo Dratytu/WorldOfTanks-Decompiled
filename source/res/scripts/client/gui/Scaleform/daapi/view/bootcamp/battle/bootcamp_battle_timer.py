679b92839cbb8b8d2b92e2a756d3d30299ed511f
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:18 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/bootcamp/battle/bootcamp_battle_timer.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/bootcamp/battle/bootcamp_battle_timer.py
@@ -1,6 +1,20 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/bootcamp/battle/bootcamp_battle_timer.py
+
+# The BootcampBattleTimer class is a subclass of the BattleTimer class,
+# specifically designed for the bootcamp battle scenario.
+# This class is part of the Scaleform daapi view for the bootcamp battle.
+#
+# The purpose of this class is to manage and display timers related to the
+# bootcamp battle, such as match timer, respawn timer, etc.
+# However, the given code doesn't provide any specific methods or attributes
+# for the BootcampBattleTimer class, making it a pass-through class.
+#
+# To enhance the functionality of this class, you may want to consider
+# adding methods and attributes specific to bootcamp battle timers.
+
 from gui.Scaleform.daapi.view.battle.shared.battle_timers import BattleTimer
 
 class BootcampBattleTimer(BattleTimer):
     pass
+

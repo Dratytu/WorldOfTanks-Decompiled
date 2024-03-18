41e3dec7477bfcaaf550b155b9d3032b8b536642
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:07 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/genConsts/MENU_CONSTANTS.py
+++ source/res/scripts/client/gui/Scaleform/genConsts/MENU_CONSTANTS.py
@@ -1,9 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/genConsts/MENU_CONSTANTS.py
 
-
 class MENU_CONSTANTS(object):
+    # A constant representing the state of showing all menu items.
     STATE_SHOW_ALL = 'showAll'
+
+    # A constant representing the state of showing the server name in the menu.
     STATE_SHOW_SERVER_NAME = 'show_server_name'
+
+    # A constant representing the state of hiding the server stats item in the menu.
     STATE_HIDE_SERVER_STATS_ITEM = 'hide_server_stats_item'
+
+    # A constant representing the state of hiding all menu items.
     STATE_HIDE_ALL = 'hide_all'

5c7786368796a9c7edef0fb8b0af993ca9eef1e6
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:28 +0300

    comment

--- source/res/battle_modifiers/scripts/common/BattleModifiersPersonalityCommon.py
+++ source/res/battle_modifiers/scripts/common/BattleModifiersPersonalityCommon.py
@@ -1,8 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/common/BattleModifiersPersonalityCommon.py
+
+# Import necessary modules for the script
 import extension_rules
 import battle_modifiers_ext
 
+# Define a function for pre-initialization
 def preInit():
+    # Call the init() function from the extension_rules module
     extension_rules.init()
+    
+    # Call the init() function from the battle_modifiers_ext module
     battle_modifiers_ext.init()

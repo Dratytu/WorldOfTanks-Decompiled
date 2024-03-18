593fe974a4d74a350be32de1ece6f7eb2ac08052
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:14 +0300

    comment

--- source/res/scripts/client/cgf_components/trigger_vse_component.py
+++ source/res/scripts/client/cgf_components/trigger_vse_component.py
@@ -1,27 +1,32 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/cgf_components/trigger_vse_component.py
+
+# Import necessary modules
 import CGF
 import Event
 from GenericComponents import VSEComponent
 from cgf_script.component_meta_class import ComponentProperty, CGFMetaTypes, registerComponent
 from cgf_script.managers_registrator import onAddedQuery, onRemovedQuery
 
-@registerComponent
+# Define the TriggerVSEComponent class
+@registerComponent  # Register the component with CGF
 class TriggerVSEComponent(object):
-    domain = CGF.DomainOption.DomainClient | CGF.DomainOption.DomainEditor
-    eventName = ComponentProperty(type=CGFMetaTypes.STRING, editorName='event name', value='event')
+    domain = CGF.DomainOption.DomainClient | CGF.DomainOption.DomainEditor  # Define the domain for the component
+    eventName = ComponentProperty(type=CGFMetaTypes.STRING, editorName='event name', value='event')  # Define a property for the event name
 
     def __init__(self):
-        super(TriggerVSEComponent, self).__init__()
-        self.triggerEvent = Event.Event()
+        super(TriggerVSEComponent, self).__init__()  # Initialize the superclass
+        self.triggerEvent = Event.Event()  # Create an Event object
 
 
+# Define the TriggerVSEComponentsManager class
 class TriggerVSEComponentsManager(CGF.ComponentManager):
 
+    # Define a method to handle component added
     @onAddedQuery(TriggerVSEComponent, VSEComponent)
     def handleComponentAdded(self, triggerVseComponent, vseComponent):
-        triggerVseComponent.triggerEvent += vseComponent.context.onTriggerEvent
+        triggerVseComponent.triggerEvent += vseComponent.context.onTriggerEvent  # Connect the triggerEvent to the onTriggerEvent of the VSEComponent
 
+    # Define a method to handle component removed
     @onRemovedQuery(TriggerVSEComponent, VSEComponent)
     def handleComponentRemoved(self, triggerVseComponent, vseComponent):
-        triggerVseComponent.triggerEvent -= vseComponent.context.onTriggerEvent
+        triggerVseComponent.triggerEvent -= vseComponent.context.onTriggerEvent  # Disconnect the triggerEvent from the onTriggerEvent of the VSEComponent

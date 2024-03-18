0924c85e17e63cbb97c6426a0a37e6450555e836
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:45 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/clans/profile/ClanProfileGlobalMapPromoView.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/clans/profile/ClanProfileGlobalMapPromoView.py
@@ -1,24 +1,35 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/clans/profile/ClanProfileGlobalMapPromoView.py
-from helpers.i18n import makeString as _ms
-from gui.Scaleform.locale.CLANS import CLANS
-from gui.Scaleform.locale.RES_ICONS import RES_ICONS
-from gui.shared.formatters import text_styles
-from gui.shared.events import OpenLinkEvent
-from gui.Scaleform.daapi.view.meta.ClanProfileGlobalMapPromoViewMeta import ClanProfileGlobalMapPromoViewMeta
+
+# Import necessary helpers and libraries
+from helpers.i18n import makeString as _ms # Helper function for localization
+from gui.Scaleform.locale.CLANS import CLANS # Localization constants for clan-related text
+from gui.Scaleform.locale.RES_ICONS import RES_ICONS # Localization constants for icons
+from gui.shared.formatters import text_styles # Helper functions for formatting text
+from gui.shared.events import OpenLinkEvent # Event for opening links
+from gui.Scaleform.daapi.view.meta.ClanProfileGlobalMapPromoViewMeta import ClanProfileGlobalMapPromoViewMeta # Base class for the view
 
 class ClanProfileGlobalMapPromoView(ClanProfileGlobalMapPromoViewMeta):
 
+    # Method to show info link
     def showInfo(self):
         self.fireEvent(OpenLinkEvent(OpenLinkEvent.GLOBAL_MAP_PROMO))
 
+    # Method to show map link
     def showMap(self):
         self.fireEvent(OpenLinkEvent(OpenLinkEvent.GLOBAL_MAP_CAP))
 
+    # Override the _populate method to set view data
     def _populate(self):
         super(ClanProfileGlobalMapPromoView, self)._populate()
-        self.as_setDataS({'header': text_styles.promoSubTitle(_ms(CLANS.GLOBALMAPVIEW_PROMO_HEADER)),
-         'description': text_styles.main(_ms(CLANS.GLOBALMAPVIEW_PROMO_DESCRIPTION)),
-         'infoLinkLabel': _ms(CLANS.GLOBALMAPVIEW_PROMO_INFOLINK),
-         'mapLinkLabel': _ms(CLANS.GLOBALMAPVIEW_PROMO_MAPLINK),
-         'background': RES_ICONS.MAPS_ICONS_CLANS_GLOBAL_MAP_PROMO})
+
+        # Set data for the view
+        self.as_setDataS({
+            # Header text
+            'header': text_styles.promoSubTitle(_ms(CLANS.GLOBALMAPVIEW_PROMO_HEADER)),
+            # Description text
+            'description': text_styles.main(_ms(CLANS.GLOBALMAPVIEW_PROMO_DESCRIPTION)),
+            # Info link label
+            'infoLinkLabel': _ms(CLANS.GLOBALMAPVIEW_PROMO_INFOLINK),
+            # Map link label
+            'mapLinkLabel': _ms

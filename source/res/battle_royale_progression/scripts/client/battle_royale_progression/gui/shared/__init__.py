61a7c90bbce20c108df0d97cece91aa498b5db3e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:44 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/shared/__init__.py
+++ source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/shared/__init__.py
@@ -1,3 +1,8 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale_progression/scripts/client/battle_royale_progression/gui/shared/__init__.py
-pass
+# battle_royale_progression/scripts/client/battle_royale_progression/gui/shared/__init__.py
+#
+# This is an empty __init__.py file for the battle_royale_progression/gui/shared module.
+# Its purpose is to mark the directory as a package and allow for the importing of its
+# submodules using a dot notation.
+#
+# The creation of this file is a common practice in Python to ensure that a directory
+# is recognized as a package, enabling the import of its modules.

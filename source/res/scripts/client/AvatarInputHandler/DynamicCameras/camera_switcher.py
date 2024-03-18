945c8e10225646361b093f7fb9f093b9b9251d85
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:26 +0300

    comment

--- source/res/scripts/client/AvatarInputHandler/DynamicCameras/camera_switcher.py
+++ source/res/scripts/client/AvatarInputHandler/DynamicCameras/camera_switcher.py
@@ -1,32 +1,37 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
 # Embedded file name: scripts/client/AvatarInputHandler/DynamicCameras/camera_switcher.py
+
 import time
 import typing
 from enum import Enum
 import BigWorld
 import math_utils
 
+# Enum for different switch types
 class SwitchTypes(Enum):
     FROM_MAX_DIST = 0
     FROM_MIN_DIST = 1
     FROM_TRANSITION_DIST_AS_MAX = 2
     FROM_TRANSITION_DIST_AS_MIN = 3
 
-
+# Enum for switch to places
 class SwitchToPlaces(Enum):
     TO_RELATIVE_POS = 0
     TO_TRANSITION_DIST = 1
     TO_NEAR_POS = 2
 
-
+# Transition distance hysteresis value
 TRANSITION_DIST_HYSTERESIS = 0.01
 
+# Camera switcher class
 class CameraSwitcher(object):
-    _TIMEOUT_TIME = 0.35
-    _INTERVAL_TO_SWITCH = 0.2
     __slots__ = ('__switchType', '__switchToName', '__switchToPos', '__lastDist', '__intervalToSwitch', '__lastScrollTime', '__hasTimeout', '__timeoutCallback', '__timeoutTime', '__transitionValue', '__switchToPlaceType')
 
     def __init__(self, switchType, switchToName, switchToPos, intervalToSwitch=_INTERVAL_TO_SWITCH, timeoutTime=_TIMEOUT_TIME):
+        """
+        Initialize the camera switcher object with switch type, switch to name, switch to position, interval to switch, and timeout time.
+        """
         self.__switchType = switchType
         self.__switchToName = switchToName
         self.__switchToPos = switchToPos
@@ -40,17 +45,24 @@ class CameraSwitcher(object):
             self.__switchToPlaceType = SwitchToPlaces.TO_TRANSITION_DIST
         else:
             self.__switchToPlaceType = SwitchToPlaces.TO_RELATIVE_POS
-        return
 
     def clear(self):
+        """
+        Clear the switcher's state.
+        """
         self.__lastDist = None
         self.__clearTimeout()
-        return
 
     def getSwitchParams(self):
+        """
+        Get the switch parameters.
+        """
         return (self.__switchToName, self.__switchToPos, self.__switchToPlaceType)
 
     def needToSwitch(self, zValue, dist, minValue, maxValue, transitionValue=None):
+        """
+        Check if the switcher needs to switch.
+        """
         if transitionValue is not None:
             transMin, transMax = self.getDistLimitationForSwitch(minValue, maxValue, transitionValue)
         else:
@@ -68,6 +80,9 @@ class CameraSwitcher(object):
         return result
 
     def getDistLimitationForSwitch(self, minValue, maxValue, transitionValue):
+        """
+        Get the distance limitation for switch.
+        """
         resultMin = minValue
         resultMax = maxValue
         if self.__switchType == SwitchTypes.FROM_TRANSITION_DIST_AS_MIN:
@@ -77,18 +92,25 @@ class CameraSwitcher(object):
         return (resultMin, resultMax)
 
     def __clearTimeout(self):
+        """
+        Clear the timeout.
+        """
         if self.__timeoutCallback is not None:
             BigWorld.cancelCallback(self.__timeoutCallback)
             self.__timeoutCallback = None
         self.__hasTimeout = None
-        return
 
     def __setTimeout(self):
+        """
+        Set the timeout.
+        """
         self.__hasTimeout = False
         self.__timeoutCallback = None
-        return
 
     def __switchFromDist(self, zValue, dist, value):
+        """
+        Switch from distance.
+        """
         currentScrollInterval = None
         if zValue != 0:
             currentTime = time.time()
@@ -98,53 +120,4 @@ class CameraSwitcher(object):
             isEqual = math_utils.almostZero(value - dist)
             if isEqual and self.__hasTimeout is None and self.__timeoutTime:
                 self.__hasTimeout = True
-                self.__timeoutCallback = BigWorld.callback(self.__timeoutTime, self.__setTimeout)
-            if isEqual and currentScrollInterval is not None:
-                return not self.__hasTimeout or currentScrollInterval > self.__intervalToSwitch
-        else:
-            self.__clearTimeout()
-        return False
-
-
-class CameraSwitcherCollection(object):
-
-    def __init__(self, cameraSwitchers, isEnabled=True):
-        self.__cameraSwitchers = cameraSwitchers
-        self.__isEnabled = isEnabled
-        self.__switchParams = None
-        return
-
-    def clear(self):
-        for switcher in self.__cameraSwitchers:
-            switcher.clear()
-
-    def setIsEnabled(self, isEnabled):
-        self.__isEnabled = isEnabled
-
-    def getSwitchParams(self):
-        return self.__switchParams
-
-    def isEnabled(self):
-        return self.__isEnabled
-
-    def needToSwitch(self, zValue, dist, minValue, maxValue, transitionValue=None):
-        if not self.__isEnabled:
-            return False
-        for switcher in self.__cameraSwitchers:
-            if switcher.needToSwitch(zValue, dist, minValue, maxValue, transitionValue):
-                self.__switchParams = switcher.getSwitchParams()
-                return True
-
-        return False
-
-    def getDistLimitationForSwitch(self, minValue, maxValue, transitionValue):
-        if not self.__isEnabled:
-            return (minValue, maxValue)
-        resMinValue = minValue
-        resMaxValue = maxValue
-        for switcher in self.__cameraSwitchers:
-            curMinValue, curMaxValue = switcher.getDistLimitationForSwitch(minValue, maxValue, transitionValue)
-            resMinValue = max(curMinValue, resMinValue)
-            resMaxValue = min(curMaxValue, resMaxValue)
-
-        return (resMinValue, resMaxValue)
+              ````

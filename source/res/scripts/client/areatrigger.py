409e2af0e6746f08505e5d251d3800f8a23380fb
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:00 +0300

    comment

--- source/res/scripts/client/areatrigger.py
+++ source/res/scripts/client/areatrigger.py
@@ -1,6 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/AreaTrigger.py
 
-import BigWorld
-import TriggersManager
+# Importing necessary modules
+import BigWorld # The BigWorld module is used for creating and managing game worlds in the MMO game.
+import TriggersManager # The TriggersManager module is used for managing triggers in the game.
 
+
+# No functions or classes defined in this code snippet.

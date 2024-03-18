72e7ab4720c57a8c4db81f11455b1a6fad34d26b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:42 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/entities/tournament/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/entities/tournament/__init__.py
@@ -1,3 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/prb_control/entities/tournament/__init__.py
-pass
+
+# This is an empty __init__.py file for the 'tournament' package within the
+# 'battle_royale/gui/prb_control/entities' directory.
+# 
+# In Python, __init__.py files are used to initialize a package. In this case,
+# this file doesn't contain any code, indicating that the 'tournament' package
+# doesn't have any specific initialization logic.
+# 
+# The package likely contains various modules related to tournament functionality
+# for the battle royale game mode in the context of the larger project.

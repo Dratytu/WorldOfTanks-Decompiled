1214fab7b22b702c1aaf52789966e13ca87b8a99
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:08 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/server_events/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/server_events/__init__.py
@@ -1,3 +1,42 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/server_events/__init__.py
-pass
+
+# This is an empty __init__.py file for the fun_random.gui.Scaleform.daapi.view.lobby.server_events package.
+# It serves to prevent the package from being recognized as a namespace package when it is imported.
+
+# An empty __init__.py file is necessary for any Python package, and it can be used to include any
+# package-level logic that needs to be executed upon import. However, in this case, there is no such
+# logic required.
+
+# The fun_random.gui.Scaleform.daapi.view.lobby.server_events package contains classes and functions
+# related to displaying server events in the lobby of the Fun Random game mode.
+
+# The package is organized as follows:
+# 
+#     fun_random/
+#         scripts/
+#             client/
+#                 fun_random/
+#                     gui/
+#                         Scaleform/
+#                             daapi/
+#                                 view/
+#                                     lobby/
+#                                         server_events/
+#                                             __init__.py
+#                                             some_module.py
+#                                             another_module.py
+#                                             ...
+# 
+# Each module in this package contains code related to a specific aspect of displaying server events
+# in the lobby.
+
+# For example, the `some_module.py` file might contain a class that handles displaying a particular
+# type of server event, while the `another_module.py` file might contain functions that retrieve
+# data about server events from the game's backend API.
+
+# The code in this package is written in Python 2.7 and uses the Scaleform framework for creating
+# user interfaces in the Fun Random game.
+
+# Note: This file does not contain any actual code or functionality, and is included only for
+

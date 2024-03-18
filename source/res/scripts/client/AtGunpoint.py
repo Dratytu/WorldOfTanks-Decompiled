6547ff4a4fe49e4e801bd8a184b43660daa6edc6
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:20 +0300

    comment

--- source/res/scripts/client/AtGunpoint.py
+++ source/res/scripts/client/AtGunpoint.py
@@ -1,19 +1,23 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/AtGunpoint.py
+
 import BigWorld
 from helpers import dependency
 from math_utils import almostZero
-from skeletons.gui.battle_session import IBattleSessionProvider
+from skeletons.gui.battle_session import IBattleSessionProvider  # Import the IBattleSessionProvider interface from the skeletons module
+
+# The epsilon value for yaw comparison
 ARTY_HIT_PREDICTION_EPSILON_YAW = 1e-05
 
 class AtGunpoint(BigWorld.DynamicScriptComponent):
-    __guiSessionProvider = dependency.descriptor(IBattleSessionProvider)
+    __guiSessionProvider = dependency.descriptor(IBattleSessionProvider)  # Create a descriptor for IBattleSessionProvider
 
     def __init__(self):
         BigWorld.DynamicScriptComponent.__init__(self)
 
     def _avatar(self):
         avatar = BigWorld.player()
+        # If the player is an observer, check if they are attached to the entity this component is attached to
         if avatar.isObserver():
             attachedVehicle = avatar.getVehicleAttached()
             if not attachedVehicle or attachedVehicle.id != self.entity.id:
@@ -32,12 +36,14 @@ class AtGunpoint(BigWorld.DynamicScriptComponent):
         if self.entity.isMyVehicle:
             if self.attackers is not None and self.entity.isAlive():
                 newAttackers = self.attackers[path[-1][0]:path[-1][1]]
+                # Iterate through new attackers and add arty hit predictions for new yaw values
                 for hitYaw in newAttackers:
                     if not prev or all((not self.__isSameYaw(prevHitYaw, hitYaw) for prevHitYaw in prev)):
                         if self._avatar() is not None:
                             self.__guiSessionProvider.shared.hitDirection.addArtyHitPrediction(hitYaw)
 
             if prev is not None:
+                # Iterate through previous attackers and remove arty hit predictions for yaw values not present in new attackers
                 for prevHitYaw in prev:
                     if not self.attackers or all((not self.__isSameYaw(prevHitYaw, hitYaw) for hitYaw in self.attackers)):
                         self.__guiSessionProvider.shared.hitDirection.removeArtyHitPrediction(prevHitYaw)
@@ -45,4 +51,4 @@ class AtGunpoint(BigWorld.DynamicScriptComponent):
         return
 
     def __isSameYaw(self, a, b):
-        return almostZero(a - b, epsilon=ARTY_HIT_PREDICTION_EPSILON_YAW)
+        return almostZero(a - b, epsilon=ARTY_HIT_PREDICTION_EPSILON_YAW)  # Compare yaw values with a small epsilon

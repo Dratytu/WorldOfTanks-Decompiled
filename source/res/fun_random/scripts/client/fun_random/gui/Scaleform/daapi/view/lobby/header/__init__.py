12111920da01d2195c3e1382ff09fef2d3942d07
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:59 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/header/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/header/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/header/__init__.py
-pass
+
+# This script defines the initialization of the header module for the lobby view
+# in the fun_random project. The header module is responsible for rendering
+# the header section of the lobby view.
+
+# However, this particular script is empty and does not contain any code.
+# It only serves as a placeholder for potential future modifications.

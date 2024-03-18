0ea86750a50d374495afdcbb635b4aa31faabc71
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:59 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/comp7/tooltips/comp7_calendar_day_extended_tooltip.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/comp7/tooltips/comp7_calendar_day_extended_tooltip.py
@@ -1,5 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/comp7/tooltips/comp7_calendar_day_extended_tooltip.py
+
+# Import necessary modules and libraries
 from datetime import datetime
 from gui.impl import backport
 from gui.shared.formatters import text_styles
@@ -8,11 +10,17 @@ from gui.Scaleform.daapi.view.lobby.comp7.tooltips.comp7_calendar_day_tooltip im
 from gui.Scaleform.daapi.view.lobby.formatters.tooltips import packCalendarBlock
 from helpers import time_utils
 from gui.prb_control.settings import SELECTOR_BATTLE_TYPES
+
+# Define a constant for the minimum tooltip width
 _TOOLTIP_MIN_WIDTH = 210
 
+# Subclass Comp7CalendarDayTooltip to create Comp7CalendarDayExtendedTooltip
 class Comp7CalendarDayExtendedTooltip(Comp7CalendarDayTooltip):
+    
+    # Set the tooltip type
     _TOOLTIP_TYPE = TOOLTIP_TYPE.COMP7_CALENDAR_DAY_EXTENDED_INFO
-
+    
+    # Override the packBlocks method to add new blocks
     def _packBlocks(self, selectedTime):
         items = []
         if self.__isSeasonEnded(selectedTime):
@@ -26,10 +34,12 @@ class Comp7CalendarDayExtendedTooltip(Comp7CalendarDayTooltip):
         blocks.extend(self.__packCalendarBlock(selectedTime))
         items.append(formatters.packBuildUpBlockData(blocks, 13))
         return items
-
+    
+    # Pack the time left block
     def __packTimeLeftBlock(self, daysLeft):
         return formatters.packTextBlockData(text=text_styles.stats(backport.ntext(self._RES_ROOT.timeLeft(), daysLeft, days=daysLeft)), blockWidth=_TOOLTIP_MIN_WIDTH)
-
+    
+    # Check if the season has ended
     def __isSeasonEnded(self, selectedTime):
         if selectedTime is None:
             selectedTime = time_utils.getCurrentLocalServerTimestamp()
@@ -38,6 +48,8 @@ class Comp7CalendarDayExtendedTooltip(Comp7CalendarDayTooltip):
             seasonEnd = self._controller.getCurrentSeason().getEndDate()
             seasonEnd = datetime.fromtimestamp(seasonEnd).date()
         return datetime.fromtimestamp(selectedTime).date() > seasonEnd
-
+    
+    # Pack the calendar block
     def __packCalendarBlock(self, selectedTime):
         return packCalendarBlock(self._controller, selectedTime, SELECTOR_BATTLE_TYPES.COMP7)
+

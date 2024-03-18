a4360f1619601a6e38ebdd9d63f875dfb8ddf1a7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:20 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/MapBoxEntryPointMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/MapBoxEntryPointMeta.py
@@ -1,6 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/MapBoxEntryPointMeta.py
-from gui.Scaleform.framework.entities.inject_component_adaptor import InjectComponentAdaptor
+
+# This script defines a single class 'MapBoxEntryPointMeta' which is a subclass of 'InjectComponentAdaptor'.
+# The purpose of this class is to act as a meta class for a Scaleform view in the context of a game
+# client, specifically for the MapBox component.
+#
+# Inheriting from 'InjectComponentAdaptor' allows this meta class to automatically inject
+# dependencies and components required by the Scaleform view.
+#
+# The class is currently empty, suggesting that additional methods and properties will be
+# defined in a derived class to implement the desired functionality for the MapBox component.
 
 class MapBoxEntryPointMeta(InjectComponentAdaptor):
+    # The class inherits from 'InjectComponentAdaptor' and does not provide any additional
+    # methods or properties, making it a minimal meta class for the MapBox component.
     pass
+

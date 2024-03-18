b3c73ed52d57ff8eb1157d50b44d179fdaa71d22
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:12 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/epic/status_notifications/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/epic/status_notifications/__init__.py
@@ -1,3 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/epic/status_notifications/__init__.py
+
+# This is an empty __init__.py file, which is used to mark this directory as a package
+# in Python. It contains no functional code and exists solely for organizational purposes.
+
+# The scripts/client/gui/Scaleform/daapi/view/battle/epic/status_notifications/ directory
+# contains modules related to status notifications in the Epic Battles game mode.
+
+# The purpose of this specific file is to initialize the package, ensuring that it can
+# be imported and used in other parts of the codebase.
+
+# Since this file contains no functional code, there are no additional comments to add.
+
 pass
+

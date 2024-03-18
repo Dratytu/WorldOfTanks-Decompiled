55cef62e12f4254f24568804844b2daae6688a83
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:05 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/shared/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/shared/__init__.py
@@ -1,4 +1,9 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/gui/shared/__init__.py
+# This is a Python script for the fun_random module.
+
+# The script imports the EventBus class from the gui.shared.event_bus module.
 from gui.shared.event_bus import EventBus
+
+# The EventBus instance for fun_random is created and assigned to the variable g_funEventBus.
+# This will be used to handle and dispatch events within the fun_random module.
 g_funEventBus = EventBus()
+

8b9cdc2ca2d878e3cc82b4327e184739861c3aad
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:18 +0300

    comment

--- source/res/scripts/client/PolygonalDeathZoneAreaTrigger.py
+++ source/res/scripts/client/PolygonalDeathZoneAreaTrigger.py
@@ -1,8 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/PolygonalDeathZoneAreaTrigger.py
+
 import BigWorld
 
+# Define a class named PolygonalDeathZoneAreaTrigger that inherits from BigWorld.UserDataObject
 class PolygonalDeathZoneAreaTrigger(BigWorld.UserDataObject):
 
+    # Constructor method for the PolygonalDeathZoneAreaTrigger class
     def __init__(self):
+        # Call the constructor of the parent class BigWorld.UserDataObject
         BigWorld.UserDataObject.__init__(self)
+

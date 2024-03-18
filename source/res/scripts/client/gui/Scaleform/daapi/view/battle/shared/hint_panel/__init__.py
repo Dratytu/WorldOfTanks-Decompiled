ab066b458c455052774255a5baf30979b8697975
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:23 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/shared/hint_panel/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/shared/hint_panel/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/shared/hint_panel/__init__.py
-pass
+
+# This is an empty __init__.py file, which is used to indicate that this directory
+# should be treated as a package in Python. In this case, the package is
+# 'scripts/client/gui/Scaleform/daapi/view/battle/shared/hint_panel'.
+# An __init__.py file can also contain initialization code for the package,
+# but in this case there is none.

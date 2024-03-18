02fa1742365f2499002ffbfbdb2f80482989b1bf
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:21 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/__init__.py
@@ -1,3 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/__init__.py
-pass
+
+# This script defines the base module for all Scaleform battle views in the
+# gui.daapi package. Currently, it doesn't contain any code, but it serves
+# as a convenient place to import all battle views.
+#
+# By keeping all battle views under a single module, we can easily
+# organize and manage them. This can be especially useful in large
+# projects with many different views and components.
+#
+# Note that this module does not contain any view-related logic or
+# functionality. Instead, it simply provides a centralized location
+# for importing and organizing battle views.

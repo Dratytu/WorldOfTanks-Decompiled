5679e891945a8ba66999446ce05e00db726636ba
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:22 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/PaginationMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/PaginationMeta.py
@@ -1,14 +1,22 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/PaginationMeta.py
-from gui.Scaleform.framework.entities.BaseDAAPIComponent import BaseDAAPIComponent
+
+import gui.Scaleform.framework.entities.BaseDAAPIComponent as BaseDAAPIComponent # Importing BaseDAAPIComponent from the Scaleform framework.
 
 class PaginationMeta(BaseDAAPIComponent):
 
+    # showPage method is overridden, but the override is not implemented.
+    # It is recommended to either implement the method or remove the override.
     def showPage(self, dir):
         self._printOverrideError('showPage')
 
+    # as_setPageS method is used to set the current page value in the UI.
     def as_setPageS(self, value):
+        # If the DAAPI is initialized, return the result of as_setPage method called on the flashObject.
         return self.flashObject.as_setPage(value) if self._isDAAPIInited() else None
 
+    # as_setEnabledS method is used to set the enabled/disabled state of left and right buttons in the UI.
     def as_setEnabledS(self, leftEnabled, rightEnabled):
+        # If the DAAPI is initialized, return the result of as_setEnabled method called on the flashObject.
         return self.flashObject.as_setEnabled(leftEnabled, rightEnabled) if self._isDAAPIInited() else None
+

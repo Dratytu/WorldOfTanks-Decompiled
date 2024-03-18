e3fb9917391d61ef6ddc6475f9ab6fcf78062308
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:22 +0300

    comment

--- source/res/scripts/client/VehicleRespawnComponent.py
+++ source/res/scripts/client/VehicleRespawnComponent.py
@@ -1,13 +1,22 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/VehicleRespawnComponent.py
-import Event
-from script_component.DynamicScriptComponent import DynamicScriptComponent
 
-class VehicleRespawnComponent(DynamicScriptComponent):
-    onSetSpawnTime = Event.Event()
+import Event # Importing Event module for creating and handling events
+from script_component.DynamicScriptComponent import DynamicScriptComponent # Importing DynamicScriptComponent for inheritance
+
+class VehicleRespawnComponent(DynamicScriptComponent): # Defining a class named VehicleRespawnComponent that inherits from DynamicScriptComponent
+    onSetSpawnTime = Event.Event() # Creating an event named onSetSpawnTime
 
     def chooseSpawnGroup(self, groupName):
-        self.cell.chooseSpawnGroup(groupName)
+        """
+        A method to choose a spawn group by specifying its name.
+        :param groupName: The name of the spawn group
+        """
+        self.cell.chooseSpawnGroup(groupName) # Calling the chooseSpawnGroup method of the cell object
 
     def set_spawnTime(self, prev):
-        self.onSetSpawnTime(self.entity.id, self.spawnTime)
+        """
+        A method to set the spawn time for the entity.
+        :param prev: Previous spawn time
+        """
+        self.onSetSpawnTime(self.entity.id, self.spawnTime) # Triggering the onSetSpawnTime event with the entity id and spawn time

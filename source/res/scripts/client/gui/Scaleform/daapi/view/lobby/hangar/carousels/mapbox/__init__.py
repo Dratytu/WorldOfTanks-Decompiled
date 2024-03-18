cc2f3ca013b94aadac75a9b102fff09bdd12b9b8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:14 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/mapbox/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/mapbox/__init__.py
@@ -1,3 +1,10 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/mapbox/__init__.py
-pass
+
+# This is an empty __init__.py file, which is used to mark this directory as a package
+# in Python. It does not contain any code and serves only to ensure that the directory
+# is recognized as a package by Python's import system.
+
+# The gui.Scaleform.daapi.view.lobby.hangar.carousels.mapbox package contains code
+# related to the Mapbox carousel in the game's hangar lobby. However, there does not
+# appear to be any code in this particular file.

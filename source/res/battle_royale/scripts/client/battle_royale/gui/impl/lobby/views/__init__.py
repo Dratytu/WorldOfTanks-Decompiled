58fb2f708ee8a35edacd89cef0ff18db78154d36
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:26 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/impl/lobby/views/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/impl/lobby/views/__init__.py
@@ -1,3 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/impl/lobby/views/__init__.py
-pass
+
+# This is an empty __init__.py file for the 'battle_royale/gui/impl/lobby/views' package.
+# It serves the purpose of preventing namespace collisions when the package is imported.
+
+# An empty __init__.py file is a common convention used in Python packages to make them importable as a single entity.
+# In this case, it allows the 'battle_royale/gui/impl/lobby/views' package to be imported using a single line,
+# such as 'from battle_royale.gui.impl.lobby.views import SomeClass'.
+
+# Without this file, importing classes or modules from this package would require specifying the full path,
+# such as 'from battle_royale.scripts.client.battle_royale.gui.impl.lobby.views.some_module import SomeClass'.

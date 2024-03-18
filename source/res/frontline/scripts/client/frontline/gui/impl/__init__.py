f4b7c7fe5503b43750366c0d9ebe041221514e63
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:56 +0300

    comment

--- source/res/frontline/scripts/client/frontline/gui/impl/__init__.py
+++ source/res/frontline/scripts/client/frontline/gui/impl/__init__.py
@@ -1,3 +1,15 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: frontline/scripts/client/frontline/gui/impl/__init__.py
-pass
+# frontline/scripts/client/frontline/gui/impl/__init__.py
+#
+# This is an empty Python file that serves as the initialization point for
+# the frontline.gui.impl module in the client scripts of the Frontline game.
+#
+# The purpose of this file is to provide a location for importing and
+# organizing all of the various implementation files that make up the
+# graphical user interface (GUI) of the Frontline game. By importing all
+# of these files into a single module, it becomes easier to manage and
+# access the various GUI components that make up the game.
+#
+# Because this file is currently empty, there is no functionality or code
+# to document at this time. However, as the frontline.gui.impl module grows
+// and develops, this file will serve as an important entry point for
+// understanding and navigating the various GUI components of the game.

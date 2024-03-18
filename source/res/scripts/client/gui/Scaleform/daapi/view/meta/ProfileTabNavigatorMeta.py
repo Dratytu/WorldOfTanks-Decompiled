dfed18f689c78a9bad44736e35662672b8115ef7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:03 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/ProfileTabNavigatorMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/ProfileTabNavigatorMeta.py
@@ -1,14 +1,34 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/ProfileTabNavigatorMeta.py
-from gui.Scaleform.framework.entities.BaseDAAPIComponent import BaseDAAPIComponent
+
+import gui.Scaleform.framework.entities.BaseDAAPIComponent # Importing BaseDAAPIComponent class from the Scaleform framework.
 
 class ProfileTabNavigatorMeta(BaseDAAPIComponent):
+    """
+    Meta class for the ProfileTabNavigator component.
+    This class is responsible for handling communication between ActionScript and Python.
+    """
 
     def onTabChange(self, tabId):
-        self._printOverrideError('onTabChange')
+        """
+        Method called when a tab is changed in the user interface.
+        :param tabId: The ID of the tab that was changed.
+        """
+        self._printOverrideError('onTabChange') # Print an error message if this method is overridden.
 
     def as_setInitDataS(self, data):
-        return self.flashObject.as_setInitData(data) if self._isDAAPIInited() else None
+        """
+        Method called to set the initial data for the component.
+        :param data: A dictionary containing the initial data.
+        :return: An ActionScript message containing the initial data.
+        """
+        return self.flashObject.as_setInitData(data) if self._isDAAPIInited() else None # Return the message if the DAAPI is initialized, otherwise return None.
 
     def as_setBtnTabCountersS(self, counters):
-        return self.flashObject.as_setBtnTabCounters(counters) if self._isDAAPIInited() else None
+        """
+        Method called to set the button tab counters for the component.
+        :param counters: A dictionary containing the button tab counters.
+        :return: An ActionScript message containing the button tab counters.
+        """
+        return self.flashObject.as_setBtnTabCounters(counters) if self._isDAAPIInited() else None # Return the message if the DAAPI is initialized, otherwise return None.
+

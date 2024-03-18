f4d81c3494f95f459a57a79e54210847504eda2e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:18 +0300

    comment

--- source/res/battle_royale/scripts/client/VehicleDeathZoneEffect.py
+++ source/res/battle_royale/scripts/client/VehicleDeathZoneEffect.py
@@ -1,5 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/VehicleDeathZoneEffect.py
+
+# Import necessary modules and libraries
 import BigWorld
 from helpers import dependency
 from skeletons.gui.battle_session import IBattleSessionProvider
@@ -9,24 +11,34 @@ from gui.battle_control.battle_constants import VEHICLE_VIEW_STATE, DeathZoneTim
 from battle_royale.gui.battle_control.battle_constants import BrTimerViewState
 from script_component.DynamicScriptComponent import DynamicScriptComponent
 
+# Define the VehicleDeathZoneEffect class, which inherits from DynamicScriptComponent
 class VehicleDeathZoneEffect(DynamicScriptComponent):
+    # Define a dependency for IBattleSessionProvider using dependency.descriptor
     guiSessionProvider = dependency.descriptor(IBattleSessionProvider)
 
+    # Define a method called _onAvatarReady
     def _onAvatarReady(self):
+        # Call the set_state method when the avatar is ready
         self.set_state()
 
+    # Define the set_state method
     def set_state(self, _=None):
+        # Calculate the time left in the death zone
         timeLeft = self.timeLeft()
+        # Create a DeathZoneTimerViewState object with the appropriate parameters
         value = DeathZoneTimerViewState(DEATH_ZONES.STATIC, False, timeLeft, BrTimerViewState.fromZone(self.state), timeLeft + BigWorld.serverTime())
+        # Invalidate the vehicle state for DEATHZONE_TIMER with the new value
         self.guiSessionProvider.invalidateVehicleState(VEHICLE_VIEW_STATE.DEATHZONE_TIMER, value)
 
+    # Define the timeLeft method
     def timeLeft(self):
+        # If the state is CRITICAL, calculate the time left based on the timeToDamage and damageTime variables
         if self.state == ZONE_STATE.CRITICAL:
             if self.timeToDamage > 0:
                 return self.timeToDamage
             return max(self.damageTime - BigWorld.serverTime(), 0.0)
 
+    # Define the onDamaged method
     def onDamaged(self):
-        value = DeathZoneTimerViewState(DEATH_ZONES.STATIC, True, -1.0, None, -1.0)
-        self.guiSessionProvider.invalidateVehicleState(VEHICLE_VIEW_STATE.DEATHZONE_TIMER, value)
-        return
+        # Create a DeathZoneTimerViewState object with the appropriate parameters
+        value = DeathZoneTimerView

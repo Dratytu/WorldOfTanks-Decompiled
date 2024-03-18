ffdcd4d887d983b3a938c56ba67adf423a4d1b7d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:18 +0300

    comment

--- source/res/scripts/client/CameraNode.py
+++ source/res/scripts/client/CameraNode.py
@@ -1,8 +1,17 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/CameraNode.py
-import BigWorld
+
+import BigWorld # Importing the BigWorld module for creating a user data object
 
 class CameraNode(BigWorld.UserDataObject):
+    """
+    A class representing a camera node in the game world.
+    Inherits from BigWorld.UserDataObject to enable the use of user data properties.
+    """
 
     def __init__(self):
-        BigWorld.UserDataObject.__init__(self)
+        """
+        Initializes the CameraNode instance.
+        Calls the constructor of the superclass to set up the user data object.
+        """
+        BigWorld.UserDataObject.__init__(self) # Calling the constructor of the superclass to set up the user data object

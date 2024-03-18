49f078e8672b48d6488c10f2e071aacc622a5af0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:32 +0300

    improve

--- source/res/battle_modifiers/scripts/common/battle_modifiers_ext/modification_cache/modification_cache.py
+++ source/res/battle_modifiers/scripts/common/battle_modifiers_ext/modification_cache/modification_cache.py
@@ -1,26 +1,25 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_modifiers/scripts/common/battle_modifiers_ext/modification_cache/modification_cache.py
-from collections import deque
+import collections
 from typing import Optional, Any
 
-class ModificationCache(object):
-    DEFAULT_LAYER_COUNT = 0
-    __slots__ = ('_modifications', '_maxLayerCount', '_layerIdQueue')
+class ModificationCache:
+    DEFAULT_LAYER_COUNT = 10
+    __slots__ = ('_modifications', '_max_layer_count', '_layer_id_queue')
 
-    def __init__(self, layerCount=None):
-        if layerCount is None:
-            layerCount = self.DEFAULT_LAYER_COUNT
+    def __init__(self, layer_count: Optional[int] = None):
+        if layer_count is None:
+            layer_count = self.DEFAULT_LAYER_COUNT
         self._modifications = {}
-        self._maxLayerCount = layerCount
-        self._layerIdQueue = deque()
-        return
+        self._max_layer_count = layer_count
+        self._layer_id_queue: collections.deque[int] = collections.deque()
 
     def clear(self):
         self._modifications.clear()
-        self._layerIdQueue.clear()
+        self._layer_id_queue.clear()
 
-    def _addLayer(self, layerId, initValue=None):
-        if len(self._layerIdQueue) == self._maxLayerCount:
-            del self._modifications[self._layerIdQueue.popleft()]
-        self._layerIdQueue.append(layerId)
-        self._modifications[layerId] = initValue
+    def _add_layer(self, layer_id: int, init_value: Optional[Any] = None):
+        if len(self._layer_id_queue) == self._max_layer_count:
+            layer_id_to_remove = self._layer_id_queue.popleft()
+            del self._modifications[layer_id_to_remove]
+        self._layer_id_queue.append(layer_id)
+        self._modifications[layer_id] = init_value
+

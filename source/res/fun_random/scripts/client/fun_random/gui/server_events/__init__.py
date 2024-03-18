b400594fc5fbab0483da4474f2654256bd94961a
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:06 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/server_events/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/server_events/__init__.py
@@ -1,7 +1,14 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/gui/server_events/__init__.py
+# This script contains definitions and registrations for server events related to the "fun random" feature.
+
+# Import the FunProgressionTriggerQuestBuilder class, which is used to build quests related to the "fun random" feature.
 from fun_random.gui.server_events.event_items import FunProgressionTriggerQuestBuilder
+
+# Use the registerQuestBuilder function from the gui.shared.system_factory module to register the FunProgressionTriggerQuestBuilder.
+# This allows the game to use the FunProgressionTriggerQuestBuilder to build quests related to the "fun random" feature.
 from gui.shared.system_factory import registerQuestBuilder
 
+# Define a function to register all "fun random" quests with the game.
 def registerFunRandomQuests():
+    # Register the FunProgressionTriggerQuestBuilder with the game using the registerQuestBuilder function.
+    # This allows the game to use the FunProgressionTriggerQuestBuilder to build quests related to the "fun random" feature.
     registerQuestBuilder(FunProgressionTriggerQuestBuilder)

aefee279d21ea960901b4c7078817c6c74a50a97
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:41 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/battle_control/controllers/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/battle_control/controllers/__init__.py
@@ -1,3 +1,10 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale/scripts/client/battle_royale/gui/battle_control/controllers/__init__.py
-pass
+# This is an empty __init__.py file for the battle_control package within
+# the battle_royale/gui/battle_control/controllers directory.
+#
+# In Python, __init__.py files are used to mark a directory as a package,
+# and can contain initialization code for the package. In this case, this
+# file does not contain any initialization code, but is required for the
+# directory to be recognized as a package.
+#
+# Any modules or sub-packages within this package should be imported here
+# if they are intended to be part of the public API for this package.

ae60032b47ffaf6a8d323b76762d4f3428ac99c4
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:29 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/shared/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/shared/__init__.py
@@ -1,3 +1,9 @@
 # This is an empty Python file that serves as a package initialization module for the 'battle_royale.gui.shared' package.
-# Its presence is required to treat the 'battle_royale/gui/shared' directory as a package in Python.
-# This file does not contain any code, but any modules or sub-packages contained within this package will be imported when this file is loaded.
+# 
+# In Python, a directory is considered a package if it contains an `__init__.py` file. This file can be empty, but its presence is required to treat the directory as a package.
+# 
+# In this case, the `__init__.py` file is located in the 'battle_royale/gui/shared' directory, which is a package that contains various modules and sub-packages related to the graphical user interface (GUI) of a battle royale game.
+# 
+# When this file is imported or loaded, any modules or sub-packages contained within the 'battle_royale.gui.shared' package will also be imported and available for use.
+# 
+# This file does not contain any code or functionality itself, but it plays a crucial role in organizing and structuring the codebase of the 'battle_royale.gui.shared' package.

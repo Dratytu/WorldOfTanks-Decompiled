8dfe9b3feca64d8af9dd37c94bc73cca19d406b7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:24 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/ConfirmDialogMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/ConfirmDialogMeta.py
@@ -1,11 +1,19 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/ConfirmDialogMeta.py
+
+import weakref
 from gui.Scaleform.framework.entities.abstract.AbstractWindowView import AbstractWindowView
 
 class ConfirmDialogMeta(AbstractWindowView):
+    """
+    Meta class for ConfirmDialog view.
+    Inherits from AbstractWindowView, providing basic window functionality.
+    """
 
-    def submit(self, selected):
-        self._printOverrideError('submit')
+    def __init__(self):
+        """
+        Initialize the ConfirmDialogMeta class.
+        """
+        super(ConfirmDialogMeta, self).__init__()
+        self._weakRefs = {}
 
-    def as_setSettingsS(self, data):
-        return self.flashObject.as_setSettings(data) if self._isDAAPIInited() else None

2db98cab4c5fcd75f3f90d017c1bc37ab61817ce
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:09 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/lobby/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/lobby/__init__.py
@@ -1,12 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/lobby/__init__.py
+# Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/lobby.py
 
-# This is an empty __init__.py file for the armory_yard.gui.impl.lobby package.
-# In Python, __init__.py files are used to initialize a package, and can contain
-# valid Python code. However, in this case, the file is empty, which means that
+# This file is an empty __init__.py file for the armory_yard.gui.impl.lobby package.
+# In Python, __init__.py files are used to initialize a package and can contain
+# valid Python code. However, this particular file is empty, which means that
 # the package does not contain any initializer code.
-#
-# The armory_yard.gui.impl.lobby package likely contains modules related to
-# the lobby of an armory yard game or application. However, without additional
-# context or information, it is difficult to provide a more specific description
-# of the package's purpose or functionality.
+
+# The armory_yard.gui.impl.lobby package likely contains modules related to the
+# lobby of an armory yard game or application. Without additional context or
+# information, it is difficult to provide a more specific description of the
+# package's purpose or functionality.

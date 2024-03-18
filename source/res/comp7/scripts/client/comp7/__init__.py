f6463b904af12ba32a5620d0cc037022ab66ebd9
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:50 +0300

    comment

--- source/res/comp7/scripts/client/comp7/__init__.py
+++ source/res/comp7/scripts/client/comp7/__init__.py
@@ -1,3 +1,22 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: comp7/scripts/client/comp7/__init__.py
-pass
+# comp7/scripts/client/comp7/__init__.py
+
+# This is an empty __init__.py file for the comp7 package.
+# In Python, __init__.py files are used to mark a directory as a package,
+# and can contain initialization code for the package.
+# However, in this case, the file is empty, indicating that there is no
+# initialization code needed for the comp7 package.
+
+# The comp7 package may contain various modules and sub-packages,
+# which can be imported and used in other Python scripts.
+# By placing an empty __init__.py file in this directory, we have
+# told Python that this directory should be treated as a package,
+# and any modules or sub-packages contained within it can be imported
+# using the standard dot notation.
+
+# For example, if there was a module named 'mymodule' contained within
+# the comp7 package, we could import it in another Python script like this:
+#
+#     from comp7.mymodule import some_function
+#
+# This would import the 'some_function' function from the 'mymodule' module,
+# which is contained within the comp7 package.

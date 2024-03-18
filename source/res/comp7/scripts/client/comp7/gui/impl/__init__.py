3ea24326d4e840fa494f2882d82a3862ccd143c1
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:48 +0300

    comment

--- source/res/comp7/scripts/client/comp7/gui/impl/__init__.py
+++ source/res/comp7/scripts/client/comp7/gui/impl/__init__.py
@@ -1,3 +1,8 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
+# This script is part of the comp7 project and contains the initialization
+# for the gui module.
+
 # Embedded file name: comp7/scripts/client/comp7/gui/impl/__init__.py
 pass
+

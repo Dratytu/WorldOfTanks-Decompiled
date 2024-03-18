c30fadb802eefe2717bb056f6ec7a9e6dc204ed0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:36 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/wot_plus/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/wot_plus/__init__.py
@@ -1,3 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
+# This is the initializer script for a set of views related to WotPlus in the lobby.
+# It does not contain any functionality itself, but rather imports and initializes
+# various modules and components related to WotPlus.
+#
+# These views are likely part of the user interface for the World of Tanks game,
+# and are built using the Scaleform framework.
+#
+# The script only includes a single pass statement, which indicates that there
+# is no functionality or logic contained within this script.
+
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/wot_plus/__init__.py
 pass
+

551ceb5e5b78a05d8b7bc954eb6736c02a6e5c23
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:56 +0300

    comment

--- source/res/frontline/scripts/common/frontline_common/__init__.py
+++ source/res/frontline/scripts/common/frontline_common/__init__.py
@@ -1,3 +1,9 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: frontline/scripts/common/frontline_common/__init__.py
-pass
+# frontline/scripts/common/frontline_common/__init__.py
+# 
+# This is an empty Python file that serves as the entry point for the 'frontline\_common' module.
+# It does not contain any code itself, but rather provides a location for importing and organizing
+# other modules and functions that are used in common across various parts of the Frontline application.
+# By keeping this code in a separate module, we can reduce duplication and improve maintainability.
+#
+# As this file is currently empty, there is no functionality to document. However, any functions or
+# modules imported and used here should be documented in their own files.

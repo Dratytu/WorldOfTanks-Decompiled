bb3987ccee41fa40a1cd16c81ec445a8555ef3b2
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:03 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/feature/models/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/feature/models/__init__.py
@@ -1,3 +1,13 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/gui/feature/models/__init__.py
-pass
+# This is the __init__.py file for the fun_random.gui.feature.models module.
+# This file is empty, indicating that this module does not contain any code of its own,
+# but is instead intended to serve as a container for related classes or functions.
+# Any Python file placed in the same directory as this file will be considered part
+# of this module, and can be imported using the standard Python import syntax.
+#
+# For example, to import a class called MyClass from a file called my_class.py,
+# you would use the following syntax:
+#
+#     from fun_random.gui.feature.models.my_class import MyClass
+#
+# This will make the MyClass available for use in your code, and any other code
+# that imports this module.

f0e69d26b29c8366c12658108904b78a842db9e3
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:32 +0300

    improve

--- Remove-GuiAndVScript.bat
+++ Remove-GuiAndVScript.bat
@@ -10,25 +10,27 @@ set "source=%~dp0source\res"
 
 REM Sets the source directory to the "res" directory inside the directory where the batch script is located
 
-for /f "tokens=* delims=" %%d in ('dir /ad/b/ogne "%source%"') do (
+for /f "tokens=* delims=" %%d in ('dir /ad/b/ON "%source%" 2^>nul') do (
     REM Loops through all directories in the source directory
 
     for %%x in (gui vscript) do (
         REM Loops through the "gui" and "vscript" directories inside each directory in the source directory
 
-        if exist "%source%\%%d\%%x\" (
+        set "directory=%source%\%%d\%%x"
+
+        if exist "!directory!" (
             REM Checks if the "gui" or "vscript" directory exists inside the current directory
 
-            del "%source%\%%d\%%x\*.*" /s/f/q >nul
+            del "!directory!\*.*" /s/f/q >nul 2^>nul
             REM Deletes all files inside the "gui" or "vscript" directory and its subdirectories quietly (without prompts)
 
-            cd "%source%\%%d"
+            cd /d "!source!\%%d"
             REM Changes the current directory to the parent directory of the "gui" or "vscript" directory
 
-            rd "%source%\%%d\%%x" /s/q >nul
+            rd "!directory!" /s/q >nul 2^>nul
             REM Deletes the "gui" or "vscript" directory and its contents quietly (without prompts)
 
-            echo     %source%\%%d\%%x\)
+            echo     !directory!
             REM Echos the path of the deleted "gui" or "vscript" directory for the user to see
 
         )

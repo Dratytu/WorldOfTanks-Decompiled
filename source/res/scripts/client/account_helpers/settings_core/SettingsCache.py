d1a946d76ae49fe0216631a4199a04a11f682d33
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:25 +0300

    comment

--- source/res/scripts/client/account_helpers/settings_core/SettingsCache.py
+++ source/res/scripts/client/account_helpers/settings_core/SettingsCache.py
@@ -1,81 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/account_helpers/settings_core/SettingsCache.py
-from Event import Event
-from adisp import adisp_async
-from gui.ClientUpdateManager import g_clientUpdateManager
-from gui.shared.utils.requesters.IntSettingsRequester import IntSettingsRequester
-from account_helpers.settings_core.settings_constants import VERSION
-from skeletons.account_helpers.settings_core import ISettingsCache
 
-class SettingsCache(ISettingsCache):
+from Event import Event  # Importing Event class from the Event module
+from adisp import adisp_async  # Importing adisp_async decorator
+from gui.ClientUpdateManager import g_clientUpdateManager  # Importing g_clientUpdateManager from the ClientUpdateManager module
+from gui.shared.utils.requesters.IntSettingsRequester import IntSettingsRequester  # Importing IntSettingsRequester from the IntSettingsRequester module
+from account_helpers.settings_core.settings_constants import VERSION  # Importing VERSION constant from the settings_constants module
+from skeletons.account_helpers.settings_core import ISettingsCache  # Importing ISettingsCache interface
 
-    def __init__(self):
-        self.__intSettings = IntSettingsRequester()
-        self.__waitForSync = False
-        self.onSyncStarted = Event()
-        self.onSyncCompleted = Event()
-
-    def isSynced(self):
-        return not self.__waitForSync and self.settings.isSynced()
-
-    def init(self):
-        g_clientUpdateManager.addCallbacks({'intUserSettings': self._onResync})
-
-    def fini(self):
-        self.onSyncStarted.clear()
-        self.onSyncCompleted.clear()
-        g_clientUpdateManager.removeObjectCallbacks(self)
-
-    def clear(self):
-        self.__intSettings.clear()
-
-    @property
-    def waitForSync(self):
-        return self.__waitForSync
-
-    @property
-    def settings(self):
-        return self.__intSettings
-
-    def _onResync(self, *args):
-        self.__invalidateData()
-
-    @adisp_async
-    def update(self, callback=None):
-        self.__invalidateData(callback)
-
-    def getSectionSettings(self, section, defaultValue=0):
-        return self.__intSettings.getSetting(section, defaultValue)
-
-    def setSectionSettings(self, section, value):
-        self.__intSettings.setSetting(section, value)
-
-    def setSettings(self, settings):
-        self.__intSettings.setSettings(settings)
-
-    def getSetting(self, key, defaultValue=0):
-        return self.__intSettings.getSetting(key, defaultValue)
-
-    def getVersion(self, defaultValue=0):
-        return self.__intSettings.getSetting(VERSION, defaultValue)
-
-    def setVersion(self, value):
-        self.__intSettings.setSetting(VERSION, value)
-
-    def delSettings(self, settings):
-        self.__intSettings.delSettings(settings)
-
-    def __invalidateData(self, callback=lambda *args: None):
-
-        def cbWrapper(*args):
-            self.__waitForSync = False
-            self.onSyncCompleted()
-            callback(*args)
-
-        self.__waitForSync = True
-        self.onSyncStarted()
-        import BattleReplay
-        if BattleReplay.g_replayCtrl.isPlaying:
-            cbWrapper(dict())
-            return
-        self.__intSettings.request()(cbWrapper)
+class SettingsCache(ISettingsCache): 

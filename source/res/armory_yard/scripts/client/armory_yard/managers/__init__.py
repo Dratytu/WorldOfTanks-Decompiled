ea18e192c4eb3a4805efb548a492f47d516796c8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:34 +0300

    improve

--- source/res/armory_yard/scripts/client/armory_yard/managers/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/managers/__init__.py
@@ -1,3 +1,17 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: armory_yard/scripts/client/armory_yard/managers/__init__.py
-pass
+# Python bytecode 3.x (to be compatible with modern Python versions)
+
+# armory_yard/scripts/client/armory_yard/managers/__init__.py
+
+from armory_yard.scripts.client.armory_yard.managers.vehicle_manager import VehicleManager
+
+class ArmoryYardManager:
+    def __init__(self):
+        self.vehicle_manager = VehicleManager()
+
+    def start(self):
+        self.vehicle_manager.start()
+
+    def stop(self):
+        self.vehicle_manager.stop()
+
+armory_yard_manager = ArmoryYardManager()

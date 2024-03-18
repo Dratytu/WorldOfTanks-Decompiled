4f19f7f97403249311c6fdfb09d95cb05b5be294
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:42 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/bootcamp/BCBattleSelector.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/bootcamp/BCBattleSelector.py
@@ -1,15 +1,25 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/bootcamp/BCBattleSelector.py
+
 import logging
 from gui.Scaleform.daapi.view.lobby.header.BattleTypeSelectPopover import BattleTypeSelectPopover
+
+# Initialize the logger for this module
 _logger = logging.getLogger(__name__)
 
+# Inherit from BattleTypeSelectPopover to customize the behavior of the base class
 class BCBattleSelector(BattleTypeSelectPopover):
 
+    # Override the as_updateS method to implement custom functionality
     def as_updateS(self, items, extraItems, isShowDemonstrator, demonstratorEnabled):
+        # Log the received items for debugging purposes
         _logger.debug('BCBattleSelector, %s', items)
+
+        # Iterate through the battle type items
         for battleTypeItem in items:
+            # If the data is not 'random', set the disabled attribute to True
             if battleTypeItem['data'] != 'random':
                 battleTypeItem['disabled'] = True
 
+        # Call the superclass method with the modified items, extraItems, isShowDemonstrator, and demonstratorEnabled
         super(BCBattleSelector, self).as_updateS(items, extraItems, isShowDemonstrator, demonstratorEnabled)

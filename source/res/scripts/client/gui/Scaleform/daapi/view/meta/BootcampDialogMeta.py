25cf4a151c15086def8c8b469c9b32cde8466de9
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:38 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/BootcampDialogMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/BootcampDialogMeta.py
@@ -1,8 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/BootcampDialogMeta.py
-from gui.Scaleform.daapi.view.dialogs.SimpleDialog import SimpleDialog
 
-class BootcampDialogMeta(SimpleDialog):
+import sys
+if sys.version_info[0] < 3:
+    from gui.Scaleform.daapi.view.dialogs.SimpleDialog import SimpleDialog  # noqa
 
-    def as_setDataS(self, path, label, showAward, awardText):
-        return self.flashObject.as_setData(path, label, showAward, awardText) if self._isDAAPIInited() else None

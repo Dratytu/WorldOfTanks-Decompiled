a9ad5bc9cc5d6bf5936847e5e4824060ebde9e26
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:33 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/gen/view_models/views/lobby/views/__init__.py
+++ source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/gen/view_models/views/lobby/views/__init__.py
@@ -1,3 +1,21 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/gen/view_models/views/lobby/views/__init__.py
+
+# This script defines the view models for the lobby views in the Battle Royale progression system.
+# It does not contain any concrete implementation, but rather serves as a blueprint for generating
+# the view models at runtime.
+#
+# All view models in this script are defined within the 'battle_royale_progression.gui.impl.gen.view_models.views.lobby.views'
+# namespace, and are implemented as Python classes.
+#
+# Each view model class is responsible for defining the properties and methods that correspond
+# to the elements of the user interface that the view model represents.
+#
+# Example usage:
+#   from battle_royale_progression.gui.impl.gen.view_models.views.lobby.views import SomeViewModel
+#
+#   view_model = SomeViewModel()
+#   # The view model can now be used to populate the view with data, or to respond to user input.
+
 pass
+

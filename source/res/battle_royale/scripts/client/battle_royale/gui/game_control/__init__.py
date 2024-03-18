ba2d8351bbe58541e5809a738d9f99789dbbe589
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:22 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/game_control/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/game_control/__init__.py
@@ -1,10 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/game_control/__init__.py
+
+# Import necessary modules and classes
 from battle_royale.gui.game_control.battle_royale_controller import BattleRoyaleController as _BattleRoyale
 from battle_royale.gui.game_control.battle_royale_tournament_controller import BattleRoyaleTournamentController as _BRTournament
 from battle_royale.gui.game_control.battle_royale_rent_vehicles_controller import BattleRoyaleRentVehiclesController as _BRRentController
-import skeletons.gui.game_control as _interface
-from gui.shared.system_factory import registerGameControllers
+import skeletons.gui.game_control as _interface  # Skeleton interface for game control
+from gui.shared.system_factory import registerGameControllers  # Function to register game controllers
 
+# Function to register Battle Royale game controllers
 def registerBRGameControllers():
-    registerGameControllers([(_interface.IBattleRoyaleController, _BattleRoyale, False), (_interface.IBattleRoyaleTournamentController, _BRTournament, False), (_interface.IBattleRoyaleRentVehiclesController, _BRRentController, False)])
+    # Register the following game controllers using the 'registerGameControllers' function:
+    # 1. IBattleRoyaleController: BattleRoyaleController
+    # 

3228b0be6e9a00dcd12eaf47b3a14e4dd674a90b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:19 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/mapbox/tooltips/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/mapbox/tooltips/__init__.py
@@ -1,3 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/mapbox/tooltips/__init__.py
-pass
+
+# This is an empty __init__.py file, which is used to mark this directory as a package
+# in Python. It does not contain any code itself, but it allows other modules and packages
+# to import modules from this package using a dot-notation.

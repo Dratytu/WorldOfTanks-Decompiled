4bb7280e84ba332ede332d4a25479e989d6af3eb
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:43 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/common/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/common/__init__.py
@@ -1,14 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/common/__init__.py
 
-# This is an empty __init__.py file, which is used to indicate that this directory
+# This is an empty __init__.py file, used to indicate that this directory
 # should be treated as a package in Python.
 #
-# In this particular case, the package is 'fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/common'.
-# There are no functions or classes defined in this file.
-#
-# However, the package may contain other modules or sub-packages, which could define
-# functions, classes, or variables that are used by other parts of the application.
+# The package name is 'fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/common'.
+# This package does not contain any functions, classes, or variables
+# directly, but it may contain other modules or sub-packages
+# that define functions, classes, or variables used by other parts of the application.
 #
 # By creating an __init__.py file, we ensure that the directory is recognized as a
 # package, and that its contents can be imported using the standard Python import syntax.

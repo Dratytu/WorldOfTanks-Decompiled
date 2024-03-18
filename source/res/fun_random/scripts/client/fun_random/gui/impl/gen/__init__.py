db890ec8c30480c3e26427b28bad0aa6de73fa07
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:49 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/impl/gen/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/impl/gen/__init__.py
@@ -1,3 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/impl/gen/__init__.py
-pass
+
+# This is an empty Python file that serves as a placeholder for generated code.
+# It is likely that this file was created by a code generator tool,
+# such as the ones used to create boilerplate code for user interfaces.
+
+# The file contains a single pass statement, which is used as a placeholder
+# and has no effect on the execution of the program.
+
+# If you are modifying this file manually, it is important to be aware
+# that any changes you make will be overwritten the next time the code is generated.
+
+# Instead, you should make any necessary modifications in the code generator
+# configuration files or in the code that calls the generator.
+
+# For more information on code generation and how it is used in this project,
+# please consult the relevant documentation or contact the project maintainers.

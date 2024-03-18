ffbfd516a0ba337ccf2aba8ec4011896b34fae42
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:56 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/ingame_help/fun_random_pages.py
+++ source/res/fun_random/scripts/client/fun_random/gui/ingame_help/fun_random_pages.py
@@ -1,34 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/ingame_help/fun_random_pages.py
-from constants import ARENA_GUI_TYPE
-from fun_random.gui.ingame_help import HelpPagePriority
-from fun_random.gui.feature.util.fun_mixins import FunSubModesWatcher
-from fun_random.gui.feature.util.fun_wrappers import hasBattleSubMode
-from fun_random.gui.Scaleform.daapi.view.battle.hint_panel.hint_panel_plugin import HelpHintContext
-from gui.impl import backport
-from gui.ingame_help.detailed_help_pages import addPage, DetailedHelpPagesBuilder
-from gui.shared.formatters import text_styles
+
+from constants import ARENA_GUI_TYPE # Importing ARENA_GUI_TYPE constant from constants module
+from fun_random.gui.ingame_help import HelpPagePriority # Importing HelpPagePriority enum from fun_random.gui.ingame_help module
+from fun_random.gui.feature.util.fun_mixins import FunSubModesWatcher # Importing FunSubModesWatcher mixin from fun_random.gui.feature.util.fun_mixins module
+from fun_random.gui.feature.util.fun_wrappers import hasBattleSubMode # Importing hasBattleSubMode decorator from fun_random.gui.feature.util.fun_wrappers module
+from fun_random.gui.Scaleform.daapi.view.battle.hint_panel.hint_panel_plugin import HelpHintContext # Importing HelpHintContext enum from fun_random.gui.Scaleform.daapi.view.battle.hint_panel.hint_panel_plugin module
+from gui.impl import backport # Importing backport module
+from gui.ingame_help.detailed_help_pages import addPage, DetailedHelpPagesBuilder # Importing addPage function and DetailedHelpPagesBuilder class from gui.ingame_help.detailed_help_pages module
+from gui.shared.formatters import text_styles # Importing text_styles module from gui.shared.formatters module
 
 class FunRandomHelpPagesBuilder(DetailedHelpPagesBuilder, FunSubModesWatcher):
-    _SUITABLE_CTX_KEYS = ('isFunRandom',)
-
-    @classmethod
-    def priority(cls):
-        return HelpPagePriority.FUN_RANDOM
-
-    @classmethod
-    @hasBattleSubMode(defReturn=())
-    def buildPages(cls, _):
-        pages = []
-        battleSubMode = cls.getBattleSubMode()
-        iconsRoot = battleSubMode.getIconsResRoot()
-        localsRoot = battleSubMode.getLocalsResRoot()
-        commonTitle = backport.text(localsRoot.detailsHelpTitle())
-        for pageID, pageRes in sorted(localsRoot.detailsHelp.items()):
-            addPage(pages, commonTitle, backport.text(pageRes.title()), text_styles.mainBig(backport.text(pageRes.description())), [], [], backport.image(iconsRoot.battle_help.dyn(pageID)()), hintCtx=HelpHintContext.FUN_RANDOM)
-
-        return pages
-
-    @classmethod
-    def _collectHelpCtx(cls, ctx, arenaVisitor, vehicle):
-        ctx['isFunRandom'] = arenaVisitor.getArenaGuiType() == ARENA_GUI_TYPE.FUN_RANDOM
+    # Class inheriting from DetailedHelpPagesBuilder and FunSub

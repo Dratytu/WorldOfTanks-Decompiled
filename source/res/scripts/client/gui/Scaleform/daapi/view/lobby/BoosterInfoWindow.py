2bcd3bc4c5de6a14c5207373e237ece2131cf51c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:43 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/BoosterInfoWindow.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/BoosterInfoWindow.py
@@ -1,5 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/BoosterInfoWindow.py
+
+# Import necessary modules and classes
 from gui.Scaleform import MENU
 from gui.Scaleform.daapi.view.meta.BoosterInfoMeta import BoosterInfoMeta
 from gui.Scaleform.framework.entities.View import View
@@ -7,25 +9,3 @@ from helpers import dependency
 from helpers.i18n import makeString as _ms
 from skeletons.gui.goodies import IGoodiesCache
 
-class BoosterInfoWindow(BoosterInfoMeta):
-    goodiesCache = dependency.descriptor(IGoodiesCache)
-
-    def __init__(self, ctx=None):
-        super(BoosterInfoWindow, self).__init__()
-        self.boosterID = ctx.get('boosterID')
-
-    def onCancelClick(self):
-        self.destroy()
-
-    def onWindowClose(self):
-        self.destroy()
-
-    def _populate(self):
-        super(View, self)._populate()
-        booster = self.goodiesCache.getBooster(self.boosterID)
-        self.as_setBoosterInfoS({'windowTitle': _ms(MENU.BOOSTERS_COMMON_NAME),
-         'name': booster.userName,
-         'icon': booster.icon,
-         'parameters': [{'value': '{}\n'.format(booster.getFormattedValue()),
-                         'type': '{}\n'.format(_ms(MENU.BOOSTERS_COMMON_EFFECT_VALUE))}, {'value': booster.getEffectTimeStr(hoursOnly=True),
-                         'type': _ms(MENU.BOOSTERS_COMMON_EFFECT_TIME)}]})

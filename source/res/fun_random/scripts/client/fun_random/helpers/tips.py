71bbfcfe07ef83dc7bf67c6bd278a1d656a994f9
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:49 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/helpers/tips.py
+++ source/res/fun_random/scripts/client/fun_random/helpers/tips.py
@@ -1,10 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/helpers/tips.py
+
+# This script is part of the fun_random package, located in the scripts/client directory.
+# It contains helper functions for generating tips.
 
 import random
-from cgf_components.marker_component import IBattleSessionProvider
-from fun_random.gui.feature.util.fun_mixins import FunSubModesWatcher
-from fun_random.gui.feature.util.fun_wrappers import hasBattleSubMode
-from gui.impl.gen import R  # R is a helper class for accessing resource strings
-from helpers import dependency
-from helpers.tips import TipsCriteria, TipData 
+from cgf_components.marker_component import IBattleSessionProvider  # Interface for battle session providers
+from fun_random.gui.feature.util.fun_mixins import FunSubModesWatcher  # Mixin for monitoring fun submodes
+from fun_random.gui.feature.util.fun_wrappers import hasBattleSubMode  # Helper function for checking battle submode
+from gui.impl.gen import R  # Helper class for accessing resource strings
+from helpers import dependency  # Helper module for managing dependencies
+from helpers.tips import TipsCriteria, TipData  # Classes for managing tips criteria and data
+

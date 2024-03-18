53050152897f148c8203932dd67b298ca0a6b12c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:51 +0300

    comment

--- source/res/event_platform/scripts/client/event_platform/gui/impl/gen/__init__.py
+++ source/res/event_platform/scripts/client/event_platform/gui/impl/gen/__init__.py
@@ -1,3 +1,8 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: event_platform/scripts/client/event_platform/gui/impl/gen/__init__.py
-pass
+# This script is a part of the event_platform project, located in the client directory.
+# It is specifically located in event_platform/gui/impl/gen.
+
+# The script is empty and does not contain any code or functionality.
+# It only contains a single pass statement, which is a null operation and does nothing when executed.
+
+# It is unclear why this script exists or what its intended purpose may be.
+# If there is a reason for its existence, it would be helpful to include a comment explaining that reason.

1234c8907659718944d3f7b97e6451ab3f71680c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:50 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/PersonalReservesTabMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/PersonalReservesTabMeta.py
@@ -1,6 +1,18 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/PersonalReservesTabMeta.py
-from gui.Scaleform.framework.entities.inject_component_adaptor import InjectComponentAdaptor
-
-class PersonalReservesTabMeta(InjectComponentAdaptor):
-    pass
+# PersonalReservesTabMeta.py
+# This file is a part of the gui.Scaleform.framework.entities package.
+# It contains the PersonalReservesTabMeta class, which is an adaptation of
+# InjectComponentAdaptor for personal reserves tab related functionality.
+#
+# The class does not contain any implemented methods, as it is intended to
+# be inherited and extended by other classes to provide specific personal
+# reserves tab features.
+#
+# InjectComponentAdaptor is a base class that provides an easy way to
+# inject components into the class. This is useful when working with
+# the Starfall framework, which relies on dependency injection to
+# manage components and their interactions.
+#
+# The PersonalReservesTabMeta class is designed to be a part of the
+# scaleform-based user interface, and it does not contain any business
+# logic. Instead, it is focused on managing and organizing UI components
+# related to personal reserves in the game.

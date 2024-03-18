10aa57fb038680e38f2c30566f13f15e17314377
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:31 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/__init__.py
+++ source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/__init__.py
@@ -1,3 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/__init__.py
-pass
+
+# This is an empty __init__.py file for the 'battle_royale_progression/gui/impl' package.
+# In Python, __init__.py files are used to initialize a package and can contain valid Python code.
+# However, in this case, the file is empty, which means that the package is not adding any
+# additional functionality or behavior when it is imported.
+
+# The purpose of this file is simply to mark the directory as a package, allowing it to be
+# imported using a dot notation. For example, if this file is located at
+# 'battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/__init__.py',
+# then the package can be imported using the following code:
+#
+#     from battle_royale_progression.gui.impl import some_module
+#
+# Where 'some_module' is a module contained within the 'battle_royale_progression/gui/impl'
+# package.
+

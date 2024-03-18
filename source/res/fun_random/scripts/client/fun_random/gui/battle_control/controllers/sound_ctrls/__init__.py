67e080892a4ff07143d2df0eb04a48d59466a9a3
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:43 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/battle_control/controllers/sound_ctrls/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/battle_control/controllers/sound_ctrls/__init__.py
@@ -1,3 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/battle_control/controllers/sound_ctrls/__init__.py
-pass
+
+# This is an empty __init__.py file for the fun\_random.gui.battle_control.controllers.sound_ctrls package.
+# It serves to prevent errors when importing modules from this package.
+
+# The fun\_random.gui.battle_control.controllers.sound_ctrls package contains sound control controllers
+# for the fun\_random game mode. These controllers handle the playback of sound effects and music
+# during battles.
+
+# Author: [Your Name]
+# Date: [Date]
+# Version: [Version Number]

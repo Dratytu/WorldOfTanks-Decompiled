466d77bac7ad8103a12fc749b15cb7c93d7ae284
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:06 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/helpers/server_settings.py
+++ source/res/fun_random/scripts/client/fun_random/helpers/server_settings.py
@@ -1,39 +1,62 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/helpers/server_settings.py
+
 from collections import namedtuple
 from fun_random_common.fun_constants import DEFAULT_ASSETS_PACK, DEFAULT_SETTINGS_KEY, DEFAULT_PRIORITY, UNKNOWN_WWISE_REMAPPING, UNKNOWN_EVENT_ID, FunSubModeImpl
 from shared_utils import makeTupleByDict
 
+# Define a namedtuple for FunSubModeClientConfig with the following fields:
+# subModeImpl: The implementation of the subscription mode
+# assetsPointer: The pointer to the assets
+# settingsKey: The key for settings
+# priority: The priority level
+# wwiseRemapping: The Wwise remapping
+# battleModifiersDescr: The battle modifiers description
+# infoPageUrl: The URL for the info page
 class FunSubModeClientConfig(namedtuple('_FunSubModeClientConfig', ('subModeImpl', 'assetsPointer', 'settingsKey', 'priority', 'wwiseRemapping', 'battleModifiersDescr', 'infoPageUrl'))):
     __slots__ = ()
 
     def __new__(cls, **kwargs):
+        # Set default values for the fields and update with the provided keyword arguments
         defaults = dict(subModeImpl=FunSubModeImpl.DEFAULT, assetsPointer=DEFAULT_ASSETS_PACK, settingsKey=DEFAULT_SETTINGS_KEY, priority=DEFAULT_PRIORITY, wwiseRemapping=UNKNOWN_WWISE_REMAPPING, battleModifiersDescr=(), infoPageUrl='')
         defaults.update(kwargs)
         return super(FunSubModeClientConfig, cls).__new__(cls, **defaults)
 
     @classmethod
     def defaults(cls):
+        # Return a new instance of the namedtuple with default values
         return cls(FunSubModeImpl.DEFAULT, DEFAULT_ASSETS_PACK, DEFAULT_SETTINGS_KEY, DEFAULT_PRIORITY, UNKNOWN_WWISE_REMAPPING, (), '')
 
-
+# Define a namedtuple for FunSubModeFiltrationConfig with the following fields:
+# levels: The levels
+# forbiddenClassTags: The set of forbidden class tags
+# forbiddenVehTypes: The set of forbidden vehicle types
+# allowedVehTypes: The set of allowed vehicle types
 class FunSubModeFiltrationConfig(namedtuple('FunSubModeFiltrationConfig', ('levels', 'forbiddenClassTags', 'forbiddenVehTypes', 'allowedVehTypes'))):
     __slots__ = ()
 
     def __new__(cls, **kwargs):
+        # Set default values for the fields and update with the provided keyword arguments
         defaults = dict(levels=(), forbiddenClassTags=set(), forbiddenVehTypes=set(), allowedVehTypes=set())
         defaults.update(kwargs)
         return super(FunSubModeFiltrationConfig, cls).__new__(cls, **defaults)
 
     @classmethod
     def defaults(cls):
+        # Return a new instance of the namedtuple with default values
         return cls((), set(), set(), set())
 
-
+# Define a namedtuple for FunSubModeSeasonalityConfig with the following fields:
+# isEnabled: A boolean indicating whether the seasonality is enabled
+# peripheryIDs: The set of periphery IDs
+# seasons: A dictionary of seasons with integer keys and values
+# primeTimes: A dictionary of prime times with integer keys and values
+# cycleTimes: A tuple of cycle times
 class FunSubModeSeasonalityConfig(namedtuple('FunSubModeSeasonalityConfig', ('isEnabled', 'peripheryIDs', 'seasons', 'primeTimes', 'cycleTimes'))):
     __slots__ = ()
 
     def __new__(cls, **kwargs):
+        # Set default values for the fields and update with the provided keyword arguments
         defaults = dict(isEnabled=False, peripheryIDs=set(), seasons={}, primeTimes={}, cycleTimes=())
         defaults.update(kwargs)
         cls.__packSeasonalityConfig(defaults)
@@ -41,21 +64,30 @@ class FunSubModeSeasonalityConfig(namedtuple('FunSubModeSeasonalityConfig', ('is
 
     @classmethod
     def defaults(cls):
+        # Return a new instance of the namedtuple with default values
         return cls(False, set(), {}, {}, ())
 
     def asDict(self):
+        # Return a dictionary representation of the namedtuple
         return self._asdict()
 
     @classmethod
     def __packSeasonalityConfig(cls, data):
+        # Pack the seasonality config data
         data['primeTimes'] = {int(peripheryID):value for peripheryID, value in data['primeTimes'].iteritems()}
         data['seasons'] = {int(seasonID):value for seasonID, value in data['seasons'].iteritems()}
 
-
+# Define a namedtuple for FunSubModeConfig with the following fields:
+# eventID: The ID of the event
+# isEnabled: A boolean indicating whether the subscription mode is enabled
+# seasonality: An instance of FunSubModeSeasonalityConfig
+# filtration: An instance of FunSubModeFiltrationConfig
+# client: An instance of FunSubModeClientConfig
 class FunSubModeConfig(namedtuple('_FunSubModeConfig', ('eventID', 'isEnabled', 'seasonality', 'filtration', 'client'))):
     __slots__ = ()
 
     def __new__(cls, **kwargs):
+        # Set default values for the fields and update with the provided keyword arguments
         defaults = dict(eventID=UNKNOWN_EVENT_ID, isEnabled=False, seasonality={}, filtration={}, client={})
         allowedFields = defaults.keys()
         defaults.update(kwargs)
@@ -67,82 +99,8 @@ class FunSubModeConfig(namedtuple('_FunSubModeConfig', ('eventID', 'isEnabled', 
 
     @classmethod
     def defaults(cls):
+        # Return a new instance of the namedtuple with default values
         return cls(UNKNOWN_EVENT_ID, False, {}, {}, {})
 
     @classmethod
-    def __filterAllowedFields(cls, data, allowedFields):
-        return dict(((k, v) for k, v in data.iteritems() if k in allowedFields))
-
-    @classmethod
-    def __packConfigPart(cls, configPartCls, configPartName, data):
-        data[configPartName] = makeTupleByDict(configPartCls, data)
-
-
-class FunProgressionConfig(namedtuple('_FunProgressionConfig', ('name', 'executors', 'conditions'))):
-    __slots__ = ()
-
-    def __new__(cls, **kwargs):
-        defaults = dict(name='', executors=(), conditions=())
-        defaults.update(kwargs)
-        return super(FunProgressionConfig, cls).__new__(cls, **defaults)
-
-    @classmethod
-    def defaults(cls):
-        return cls('', (), ())
-
-
-class FunMetaProgressionConfig(namedtuple('_FunMetaProgressionConfig', ('isEnabled', 'progressions'))):
-    __slots__ = ()
-
-    def __new__(cls, **kwargs):
-        defaults = dict(isEnabled=False, progressions=())
-        defaults.update(kwargs)
-        cls.__packProgressionsConfigs(defaults)
-        return super(FunMetaProgressionConfig, cls).__new__(cls, **defaults)
-
-    @classmethod
-    def defaults(cls):
-        return cls(False, ())
-
-    @classmethod
-    def __packProgressionsConfigs(cls, data):
-        data['progressions'] = tuple((makeTupleByDict(FunProgressionConfig, p) for p in data['progressions']))
-
-
-class FunRandomConfig(namedtuple('_FunRandomConfig', ('isEnabled', 'subModes', 'metaProgression', 'assetsPointer', 'settingsKey', 'infoPageUrl'))):
-    __slots__ = ()
-
-    def __new__(cls, **kwargs):
-        defaults = dict(isEnabled=False, subModes={}, metaProgression={}, assetsPointer=DEFAULT_ASSETS_PACK, settingsKey=DEFAULT_SETTINGS_KEY, infoPageUrl='')
-        allowedFields = defaults.keys()
-        defaults.update(kwargs)
-        cls.__packSubModesConfigs(defaults)
-        cls.__packMetaProgressionConfig(defaults)
-        defaults = cls.__filterAllowedFields(defaults, allowedFields)
-        return super(FunRandomConfig, cls).__new__(cls, **defaults)
-
-    @classmethod
-    def defaults(cls):
-        return cls(False, {}, {}, DEFAULT_ASSETS_PACK, DEFAULT_SETTINGS_KEY, '')
-
-    def replace(self, data):
-        data = self.__packSubModesConfigs(data)
-        data = self.__packMetaProgressionConfig(data)
-        dataToUpdate = self.__filterAllowedFields(data, self._fields)
-        return self._replace(**dataToUpdate)
-
-    @classmethod
-    def __filterAllowedFields(cls, data, allowedFields):
-        return dict(((k, v) for k, v in data.iteritems() if k in allowedFields))
-
-    @classmethod
-    def __packMetaProgressionConfig(cls, data):
-        progression = data['metaProgression'] if data['isEnabled'] else {}
-        data['metaProgression'] = makeTupleByDict(FunMetaProgressionConfig, progression)
-        return data
-
-    @classmethod
-    def __packSubModesConfigs(cls, data):
-        events = data['events'] if data['isEnabled'] else {}
-        data['subModes'] = {int(eID):FunSubModeConfig(**eData) for eID, eData in events.iteritems() if eData.get('isEnabled', False) and eData.get('eventID', UNKNOWN_EVENT_ID)}
-        return data
+    def __

3f27904e03aaaba4eb040e3aab9e5ed2db5dacfa
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:30 +0300

    comment

--- source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/view_models/__init__.py
+++ source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/view_models/__init__.py
@@ -1,3 +1,7 @@
+# This is a Python script that contains import statements for view models
+# in the battle modifiers module of a client application.
+
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/view_models/__init__.py
 pass
+

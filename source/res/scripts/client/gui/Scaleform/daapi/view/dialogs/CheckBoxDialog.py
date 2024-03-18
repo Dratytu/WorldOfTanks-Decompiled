d8d57277b5b2c1d738fb88e2528a0790026b93cb
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:50 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/dialogs/CheckBoxDialog.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/dialogs/CheckBoxDialog.py
@@ -1,22 +1,36 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/dialogs/CheckBoxDialog.py
+
+# Import the ConfirmDialogMeta class from the gui.Scaleform.daapi.view.meta package.
+# This class is the meta for the CheckBoxDialog, which is a dialog with a checkbox.
 from gui.Scaleform.daapi.view.meta.ConfirmDialogMeta import ConfirmDialogMeta
 
+# Define the CheckBoxDialog class, which inherits from ConfirmDialogMeta.
 class CheckBoxDialog(ConfirmDialogMeta):
 
+    # Initialize the class with the meta and handler arguments.
     def __init__(self, meta, handler):
+        # Call the constructor of the superclass (ConfirmDialogMeta).
         super(CheckBoxDialog, self).__init__()
+        # Store the meta and handler arguments as instance variables.
         self.meta = meta
         self.handler = handler
 
+    # This method is called when the user interacts with the dialog.
     def _callHandler(self, success, selected):
+        # If the handler instance variable is not None, call the handler with the success and selected arguments.
         if self.handler is not None:
             self.handler((success, selected))
+        # Return None.
         return
 
+    # This method is called when the dialog is displayed.
     def _populate(self):
+        # Call the _populate method of the superclass (ConfirmDialogMeta).
         super(CheckBoxDialog, self)._populate()
+        # Get the button labels from the meta instance variable.
         buttonLabels = self.meta.getButtonsSubmitCancel()
+        # Call the as_setSettingsS method of the dialog with the settings dictionary as an argument.
         self.as_setSettingsS({'title': self.meta.getTitle(),
          'description': self.meta.getMessage(),
          'submitBtnLabel': buttonLabels['submit'],
@@ -24,17 +38,6 @@ class CheckBoxDialog(ConfirmDialogMeta):
          'checkBoxLabel': self.meta.getCheckBoxButtonLabel(),
          'checkBoxSelected': self.meta.getCheckBoxSelected()})
 
+    # This method is called when the dialog is closed.
     def _dispose(self):
-        if self.meta is not None:
-            self.meta = None
-        self.handler = self._data = None
-        super(CheckBoxDialog, self)._dispose()
-        return
-
-    def onWindowClose(self):
-        self._callHandler(False, self.meta.getCheckBoxSelected())
-        self.destroy()
-
-    def submit(self, selected):
-        self._callHandler(True, selected)
-        self.destroy()
+        # Set

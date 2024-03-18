96554af2476ba917b5377a344083a23620647df5
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:34 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/__init__.py
@@ -1,3 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/__init__.py
+
+# This script is a part of the battle_royale module and is located in the
+# scripts/client/battle_royale/gui/Scaleform/daapi/view/ directory.
+
+# The purpose of this script is not immediately clear from the code itself,
+# as it only contains a single "pass" statement. However, based on the file
+# name and location, it is likely that this script is intended to be a
+`init`.py file for a package of view-related code in the battle_royale game mode.
+
+# The "pass" statement is a placeholder and does not affect the behavior of
+# the script. It is often used as a reminder that additional code is needed in
+# the future.
+
 pass
+

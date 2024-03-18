d04e5aa4bcd9d7f1ebce9954701b2b2054b8ad5c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:18 +0300

    comment

--- source/res/scripts/client/cgf_components/on_shot_components.py
+++ source/res/scripts/client/cgf_components/on_shot_components.py
@@ -11,11 +11,15 @@ class EffectOnShotComponent(object):
     """
     A class representing a component that adds an effect on shot event.
 
+    This component is used in the 'Shooting' category and has the title 'Effect On Shot Component'
+    in the editor. It can be used in both the DomainEditor and DomainClient domains.
+
     Attributes:
         category (str): The category of the component.
         editorTitle (str): The title of the component in the editor.
         domain (CGF.DomainOption): The domain of the component.
-        effectPath (str): The path to the effect prefab.
+        effectPath (str): The path to the effect prefab. This attribute is of type string and
+        can be edited in the editor with the name 'Effect Prefab'. It supports the file format '*.prefab'.
     """
     category = 'Shooting'
     editorTitle = 'Effect On Shot Component'
@@ -28,11 +32,15 @@ class SoundOnShotComponent(object):
     """
     A class representing a component that adds a sound on shot event.
 
+    This component is used in the 'Shooting' category and has the title 'Sound On Shot Component'
+    in the editor. It can be used in both the DomainEditor and DomainClient domains.
+
     Attributes:
         category (str): The category of the component.
         editorTitle (str): The title of the component in the editor.
         domain (CGF.DomainOption): The domain of the component.
-        soundPath (str): The path to the sound prefab.
+        soundPath (str): The path to the sound prefab. This attribute is of type string and
+        can be edited in the editor with the name 'Sound Prefab'. It supports the file format '*.prefab'.
     """
     category = 'Shooting'
     editorTitle = 'Sound On Shot Component'
@@ -41,4 +49,12 @@ class SoundOnShotComponent(object):
 
 # A class representing a component that controls the ejection of shell casings
 @registerComponent
+class ShellCasingEjectionComponent(object):
+    """
+    A class representing a component that controls the ejection of shell casings.
 
+    This component is used in the 'Shooting' category and can be used in both the DomainEditor and
+    DomainClient domains.
+
+    Attributes:
+        category (str): The category

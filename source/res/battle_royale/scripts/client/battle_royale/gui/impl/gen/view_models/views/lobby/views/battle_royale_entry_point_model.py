12d4dba41b0b076b44fd16ec57e40afc78f5fbf7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:40 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/views/battle_royale_entry_point_model.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/views/battle_royale_entry_point_model.py
@@ -1,41 +1,50 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/views/battle_royale_entry_point_model.py
-from enum import Enum
+
+import enum
 from frameworks.wulf import ViewModel
 
-class State(Enum):
+# Define an Enum class 'State' with three possible states
+class State(enum.Enum):
     ACTIVE = 'active'
     DISABLED = 'disabled'
     ANNOUNCE = 'announce'
 
 
+# Define the ViewModel class 'BattleRoyaleEntryPointModel' with properties and commands
 class BattleRoyaleEntryPointModel(ViewModel):
     __slots__ = ('onClick',)
 
+    # Initialize the class with properties and commands
     def __init__(self, properties=3, commands=1):
         super(BattleRoyaleEntryPointModel, self).__init__(properties=properties, commands=commands)
 
+    # Getter method for the state property
     def getState(self):
         return State(self._getString(0))
 
+    # Setter method for the state property
     def setState(self, value):
         self._setString(0, value.value)
 
+    # Getter method for the timestamp property
     def getTimestamp(self):
         return self._getNumber(1)
 
+    # Setter method for the timestamp property
     def setTimestamp(self, value):
         self._setNumber(1, value)
 
+    # Getter method for the isSingle property
     def getIsSingle(self):
         return self._getBool(2)
 
+    # Setter method for the isSingle property
     def setIsSingle(self, value):
         self._setBool(2, value)
 
+    # Initialize the properties
     def _initialize(self):
         super(BattleRoyaleEntryPointModel, self)._initialize()
-        self._addStringProperty('state')
-        self._addNumberProperty('timestamp', 0)
-        self._addBoolProperty('isSingle', True)
-        self.onClick = self._addCommand('onClick')
+        self._addStringProperty('state')  # Add a string property 'state'
+        self._addNumberProperty('timestamp', 0)  # Add a number

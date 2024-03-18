ae0ee62a07e0e68f978d1640919b3d0d86a46d57
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:14 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/RankedBattlesEntryPointMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/RankedBattlesEntryPointMeta.py
@@ -1,6 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/RankedBattlesEntryPointMeta.py
-from gui.Scaleform.framework.entities.inject_component_adaptor import InjectComponentAdaptor
 
+# The 'InjectComponentAdaptor' class is imported from the 'gui.Scaleform.framework.entities' module.
+# This base class is used to inject components into the view.
+from gui.Scaleform.framework.entities import InjectComponentAdaptor
+
+# The 'RankedBattlesEntryPointMeta' class is defined, which inherits from the 'InjectComponentAdaptor' class.
+# This class represents the meta-class for the RankedBattlesEntryPoint view.
 class RankedBattlesEntryPointMeta(InjectComponentAdaptor):
+    # The class does not contain any methods or attributes, so there is no additional functionality to document.
     pass
+

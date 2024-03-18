d613f11f7cfa2f6b3ebc2b85c37a08bbfc3fb6d5
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:13 +0300

    comment

--- source/res/armory_yard/scripts/client/__init__.py
+++ source/res/armory_yard/scripts/client/__init__.py
@@ -1,3 +1,11 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: armory_yard/scripts/client/__init__.py
-pass
+# armory_yard/scripts/client/__init__.py
+# 
+# This is an empty Python script that serves as the entry point for the 'client' module of the 'armory_yard' package.
+# It does not contain any executable code, but it is required to be present for the package to be properly structured.
+# 
+# The 'client' module may contain various scripts and modules that are used to implement the client-side logic of the application.
+# 
+# The use of the 'pass' statement in this script indicates that there is no code to execute in this module.
+# It is a placeholder that allows the script to run without raising a syntax error.
+# 
+# For more information on Python packages and modules, please refer to the official Python documentation.

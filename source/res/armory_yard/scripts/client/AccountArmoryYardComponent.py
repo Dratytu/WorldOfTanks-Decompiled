4b0424ef581ac9dfba0581b3ddd18cb379264d3a
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:23 +0300

    comment

--- source/res/armory_yard/scripts/client/AccountArmoryYardComponent.py
+++ source/res/armory_yard/scripts/client/AccountArmoryYardComponent.py
@@ -1,24 +1,47 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: armory_yard/scripts/client/AccountArmoryYardComponent.py
+
 import BigWorld
 import armory_yard_constants
 
 class AccountArmoryYardComponent(BigWorld.StaticScriptComponent):
+    """
+    A component for handling Account Armory Yard related functionality.
+    """
 
     def collectAllRewards(self, callback=None):
+        """
+        Collect all rewards available in the Armory Yard.
+
+        :param callback: A function to be called when the command is processed.
+        """
         self.entity._doCmdInt(armory_yard_constants.CMD_COLLECT_REWARDS, 0, callback)
 
     def buyStepTokens(self, currency, count, callback=None):
+        """
+        Buy step tokens using the specified currency.
+
+        :param currency: The currency to be used for buying step tokens.
+        :param count: The number of step tokens to be bought.
+        :param callback: A function to be called when the command is processed.
+        """
         self.entity._doCmdIntStr(armory_yard_constants.CMD_BUY_STEP_TOKENS, count, currency, callback)
 
     def devAddToken(self, count, callback=None):
+        """
+        Add a specified number of tokens for development purposes.
+
+        :param count: The number of tokens to be added.
+        :param callback: A function to be called when the command is processed.
+        """
         self.entity._doCmdInt(armory_yard_constants.DEV_CMD_ADD_TOKEN_S, count, callback)
 
     def devCompleteQuest(self, cycle, number, callback=None):
-        self.entity._doCmdInt2(armory_yard_constants.DEV_CMD_SET_QUEST, cycle, number, callback)
+        """
+        Complete a specific quest for development purposes.
 
-    def devCompleteCycle(self, cycle, callback=None):
-        self.entity._doCmdInt(armory_yard_constants.DEV_CMD_SET_CYCLE, cycle, callback)
-
-    def claimFinalReward(self, callback=None):
-        self.entity._doCmdInt(armory_yard_constants.CMD_CLAIM_FINAL_REWARD, 0, callback)
+        :param cycle: The cycle of the quest to be completed.
+        :param number: The number of the quest to be completed.
+        :param callback: A function to be called when the command is processed.
+        """
+        self.entity._doCmdInt2(armory_yard_constants.DEV

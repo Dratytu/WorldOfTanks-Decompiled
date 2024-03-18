deb2079ceb743785d769c3061dac519d5c0f3c4f
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:38 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/__init__.py
@@ -1,3 +1,10 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/__init__.py
-pass
+# fun\_random/scripts/client/fun_random/__init__.py
+# 
+# This is an empty Python file serving as the entry point for the 'fun\_random' package.
+# It does not contain any code, but it is required for the package to be importable.
+# 
+# The 'fun\_random' package contains various scripts and modules related to generating
+# random values and applying randomness to different aspects of a software application.
+# 
+# For more information on package structure and importing in Python, please refer to:
+# https://docs.python.org/3/tutorial/modules.html#packages

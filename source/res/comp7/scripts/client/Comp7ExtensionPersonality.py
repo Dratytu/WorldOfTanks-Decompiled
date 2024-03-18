1332bce74bb5da42569c02d749bf227104f2e56a
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:47 +0300

    comment

--- source/res/comp7/scripts/client/Comp7ExtensionPersonality.py
+++ source/res/comp7/scripts/client/Comp7ExtensionPersonality.py
@@ -1,18 +1,26 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: comp7/scripts/client/Comp7ExtensionPersonality.py
+
+# Importing the 'registerComp7Scaleform' function from the 'comp7.gui.Scaleform' module
 from comp7.gui.Scaleform import registerComp7Scaleform
 
+# The 'preInit' function is defined as follows:
 def preInit():
+    # The 'registerComp7Scaleform' function is called to register the Comp7Scaleform
     registerComp7Scaleform()
 
-
+# The 'init' function is defined as follows:
 def init():
+    # The function does not contain any code, so it effectively does nothing
     pass
 
-
+# The 'start' function is defined as follows:
 def start():
+    # The function does not contain any code, so it effectively does nothing
     pass
 
-
+# The 'fini' function is defined as follows:
 def fini():
+    # The function does not contain any code, so it effectively does nothing
     pass
+

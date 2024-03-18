e13e2137395ba16ecd131a8a9f7b403827e31525
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:28 +0300

    comment

--- source/res/comp7/scripts/client/AccountComp7Component.py
+++ source/res/comp7/scripts/client/AccountComp7Component.py
@@ -1,8 +1,8 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: comp7/scripts/client/AccountComp7Component.py
 
-# This is a Python script for the AccountComp7Component, a component of the BigWorld
-# game engine. The component inherits from BigWorld.StaticScriptComponent, which
+# This script defines the AccountComp7Component, which is a component of the BigWorld
+# game engine. This component inherits from BigWorld.StaticScriptComponent, which
 # provides basic functionality for static script components.
 class AccountComp7Component(BigWorld.StaticScriptComponent):
     # The class definition is empty, indicating that this component does not have any

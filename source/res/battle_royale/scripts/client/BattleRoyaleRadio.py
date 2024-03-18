4374c19f5df8390b3d60e02d1ff4e10594238a52
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:14 +0300

    comment

--- source/res/battle_royale/scripts/client/BattleRoyaleRadio.py
+++ source/res/battle_royale/scripts/client/BattleRoyaleRadio.py
@@ -1,6 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/BattleRoyaleRadio.py
+
 import BigWorld
 
+# This is the main class for the Battle Royale Radio entity
 class BattleRoyaleRadio(BigWorld.Entity):
+    """
+    This class represents the Battle Royale Radio entity in the game world.
+    It inherits from BigWorld.Entity, which provides basic functionality for game entities.
+    """
     pass
+

ee0eec038ab11cd739510a85533598b536ca2cdc
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:08 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/ProfileStatisticsMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/ProfileStatisticsMeta.py
@@ -2,5 +2,16 @@
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/ProfileStatisticsMeta.py
 
 # Import the ProfileSection class from the gui.Scaleform.daapi.view.lobby.profile package
+# This line is importing the ProfileSection class, which is likely a base class or a mixin for the ProfileStatisticsMeta class
 from gui.Scaleform.daapi.view.lobby.profile.ProfileSection import ProfileSection
+
+# The ProfileStatisticsMeta class is defined here
+class ProfileStatisticsMeta(ProfileSection):
+    # This class inherits from the ProfileSection class, and likely adds or overrides methods to provide functionality specific to statistics in a user's profile
+
+    # The __init__ method is the constructor for the class, and it takes a single argument, 'args'.
+    # The 'args' argument is likely a dictionary or other collection of keyword arguments that are used to initialize the object.
+    def __init__(self, args=None):
+        # The constructor calls the constructor of the superclass (ProfileSection) and passes the 'args' argument to it.
+        super(ProfileStatisticsMeta, self).__init__(args)
 

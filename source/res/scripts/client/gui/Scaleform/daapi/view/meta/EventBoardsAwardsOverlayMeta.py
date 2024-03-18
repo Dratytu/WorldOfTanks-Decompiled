a531eed27a7f38eeacd338621e3b52d82b19151b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:18 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/EventBoardsAwardsOverlayMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/EventBoardsAwardsOverlayMeta.py
@@ -1,17 +1,23 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/EventBoardsAwardsOverlayMeta.py
-from gui.Scaleform.framework.entities.BaseDAAPIComponent import BaseDAAPIComponent
+
+import gui.Scaleform.framework.entities.BaseDAAPIComponent as BaseDAAPIComponent # Importing BaseDAAPIComponent to inherit its properties and methods
 
 class EventBoardsAwardsOverlayMeta(BaseDAAPIComponent):
 
+    # Method to change the filter based on the given id
     def changeFilter(self, id):
-        self._printOverrideError('changeFilter')
+        self._printOverrideError('changeFilter') # Print an error message if this method is overridden
 
+    # Set the header data on the client side
     def as_setHeaderS(self, data):
-        return self.flashObject.as_setHeader(data) if self._isDAAPIInited() else None
+        return self.flashObject.as_setHeader(data) if self._isDAAPIInited() else None # Return None if DAAPI is not initialized, otherwise call the method on the flashObject
 
+    # Set the vehicle data on the client side
     def as_setVehicleS(self, data):
-        return self.flashObject.as_setVehicle(data) if self._isDAAPIInited() else None
+        return self.flashObject.as_setVehicle(data) if self._isDAAPIInited() else None # Return None if DAAPI is not initialized, otherwise call the method on the flashObject
 
+    # Set the data on the client side
     def as_setDataS(self, data):
-        return self.flashObject.as_setData(data) if self._isDAAPIInited() else None
+        return self.flashObject.as_setData(data) if self._isDAAPIInited() else None # Return None if DAAPI is not initialized, otherwise call the method on the flashObject
+

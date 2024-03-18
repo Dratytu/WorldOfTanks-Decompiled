dcf84a20c1d6bd8bbfd4c811f90f2fa4c05300ba
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:48 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/battle_pass/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/battle_pass/__init__.py
@@ -1,8 +1,23 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/battle_pass/__init__.py
 
-
 class BattlePassFilterConsts(object):
+    """
+    A class containing constants used for filtering battle pass data.
+    """
+
     FILTER_KEY_COMMON = 'isCommonProgression'
+    """
+    A constant key for filtering battle pass data by common progression.
+    """
+
     FILTER_KEY_SEASON = 'battlePassSeason'
+    """
+    A constant key for filtering battle pass data by season.
+    """
+
     FILTERS_KEYS = [FILTER_KEY_COMMON]
+    """
+    A list of constant keys used for filtering battle pass data.
+    Currently, only common progression is supported.
+    """

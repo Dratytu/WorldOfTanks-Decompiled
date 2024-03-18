1296c4206b0cd0fd6c47fd202056321219e92530
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:23 +0300

    comment

--- source/res/scripts/client/festivity/dummy/__init__.py
+++ source/res/scripts/client/festivity/dummy/__init__.py
@@ -1,3 +1,8 @@
+# This is a Python script for a dummy module in the festivity package.
+# It does not contain any executable code, and is intended solely for
+# documentation or placeholder purposes.
+
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/festivity/dummy/__init__.py
 pass
+

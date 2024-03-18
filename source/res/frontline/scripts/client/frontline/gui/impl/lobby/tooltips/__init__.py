2b093dc20c195dc36d8ed0b5e4328b836d7e2fd7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:41 +0300

    comment

--- source/res/frontline/scripts/client/frontline/gui/impl/lobby/tooltips/__init__.py
+++ source/res/frontline/scripts/client/frontline/gui/impl/lobby/tooltips/__init__.py
@@ -1,3 +1,8 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: frontline/scripts/client/frontline/gui/impl/lobby/tooltips/__init__.py
+# frontline/scripts/client/frontline/gui/impl/lobby/tooltips/__init__.py
+
+# This is an empty Python file that serves as the initialization point
+# for the frontline.gui.impl.lobby.tooltips module in the Frontline game.
+# No functions or classes are defined in this file.
+
 pass
+

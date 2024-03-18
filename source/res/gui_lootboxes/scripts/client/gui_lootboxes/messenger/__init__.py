3bd9a323597229ec3da35b31883b896c76372e2e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:55 +0300

    comment

--- source/res/gui_lootboxes/scripts/client/gui_lootboxes/messenger/__init__.py
+++ source/res/gui_lootboxes/scripts/client/gui_lootboxes/messenger/__init__.py
@@ -1,3 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: gui_lootboxes/scripts/client/gui_lootboxes/messenger/__init__.py
-pass
+
+# This is an empty __init__.py file for the gui_lootboxes.messenger package.
+# It serves to prevent errors that may arise from attempting to import modules
+# from a package that has not been initialized.
+
+# An example of how to use this package might look like:
+# from gui_lootboxes.messenger import some_module
+# result = some_module.some_function()
+
+# For more information on package initialization in Python, see:
+# https://docs.python.org/2/tutorial/modules.html#packages

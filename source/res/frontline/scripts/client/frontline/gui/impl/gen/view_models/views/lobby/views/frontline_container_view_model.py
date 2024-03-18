9c57c05a764fa63ff1ed43e48969f3e165673f86
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:00 +0300

    comment

--- source/res/frontline/scripts/client/frontline/gui/impl/gen/view_models/views/lobby/views/frontline_container_view_model.py
+++ source/res/frontline/scripts/client/frontline/gui/impl/gen/view_models/views/lobby/views/frontline_container_view_model.py
@@ -1,34 +1,47 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: frontline/scripts/client/frontline/gui/impl/gen/view_models/views/lobby/views/frontline_container_view_model.py
-from frameworks.wulf import Array
-from frameworks.wulf import ViewModel
+
+import frameworks.wulf as wulf
 from frontline.gui.impl.gen.view_models.views.lobby.views.frontline_container_tab_model import FrontlineContainerTabModel
 
-class FrontlineContainerViewModel(ViewModel):
-    __slots__ = ('onTabChange', 'onClose')
+# Define a ViewModel class for the FrontlineContainerView
+class FrontlineContainerViewModel(wulf.ViewModel):
+    # Define the names of properties and commands
+    __slots__ = ('on_tab_change', 'on_close')
 
     def __init__(self, properties=2, commands=2):
+        # Initialize the ViewModel with the given properties and commands
         super(FrontlineContainerViewModel, self).__init__(properties=properties, commands=commands)
 
-    def getCurrentTabId(self):
-        return self._getNumber(0)
+    def get_current_tab_id(self):
+        # Get the current tab ID
+        return self._get_number(0)
 
-    def setCurrentTabId(self, value):
-        self._setNumber(0, value)
+    def set_current_tab_id(self, value):
+        # Set the current tab ID
+        self._set_number(0, value)
 
-    def getTabs(self):
-        return self._getArray(1)
+    def get_tabs(self):
+        # Get the list of tabs
+        return self._get_array(1)
 
-    def setTabs(self, value):
-        self._setArray(1, value)
+    def set_tabs(self, value):
+        # Set the list of tabs
+        self._set_array(1, value)
 
+    # Static method to get the type of elements in the 'tabs' Array
     @staticmethod
-    def getTabsType():
+    def get_tabs_type():
         return FrontlineContainerTabModel
 
     def _initialize(self):
+        # Initialize the ViewModel
         super(FrontlineContainerViewModel, self)._initialize()
-        self._addNumberProperty('currentTabId', 0)
-        self._addArrayProperty('tabs', Array())
-        self.onTabChange = self._addCommand('onTabChange')
-        self.onClose = self._addCommand('onClose')
+
+        # Add 'currentTabId' Number property with a default value of 0
+        self._add_number_property('currentTabId', 0)
+
+        # Add 'tabs' Array property
+        self._add_array_property('tabs', wulf.Array())
+
+        # Add '

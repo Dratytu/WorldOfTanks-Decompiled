a095daaa4e748685536256bb4f25719f26c6b68f
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:28 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_quest_model.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_quest_model.py
@@ -1,26 +1,23 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_quest_model.py
-from gui.impl.gen.view_models.common.missions.quest_model import QuestModel
 
+import super
+from gui.impl.gen.view_models.common.missions.quest_model import QuestModel  # Importing QuestModel class from a common missions package
+
+# Defining ArmoryYardQuestModel class, which inherits from QuestModel class
 class ArmoryYardQuestModel(QuestModel):
+    # Defining a list of properties and commands for the class
     __slots__ = ()
 
+    # Initializing the class with default properties and commands
     def __init__(self, properties=13, commands=0):
+        # Calling the constructor of the parent class with the given properties and commands
         super(ArmoryYardQuestModel, self).__init__(properties=properties, commands=commands)
 
+    # A getter method for the 'chapterId' property
     def getChapterId(self):
         return self._getNumber(11)
 
+    # A setter method for the 'chapterId' property
     def setChapterId(self, value):
-        self._setNumber(11, value)
-
-    def getVehicleType(self):
-        return self._getString(12)
-
-    def setVehicleType(self, value):
-        self._setString(12, value)
-
-    def _initialize(self):
-        super(ArmoryYardQuestModel, self)._initialize()
-        self._addNumberProperty('chapterId', 0)
-        self._addStringProperty('vehicleType', '')
+        self._setNumber(1

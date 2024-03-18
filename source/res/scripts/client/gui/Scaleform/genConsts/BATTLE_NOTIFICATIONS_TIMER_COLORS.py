f02249c76126be6259f28e556eec375b562475cc
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:05 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/genConsts/BATTLE_NOTIFICATIONS_TIMER_COLORS.py
+++ source/res/scripts/client/gui/Scaleform/genConsts/BATTLE_NOTIFICATIONS_TIMER_COLORS.py
@@ -1,11 +1,24 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/gui/Scaleform/genConsts/BATTLE_NOTIFICATIONS_TIMER_COLORS.py
-
+# This module defines a class named BATTLE_NOTIFICATIONS_TIMER_COLORS, which contains a set of predefined color constants
+# for use in battle notifications timers.
 
 class BATTLE_NOTIFICATIONS_TIMER_COLORS(object):
+    # The ORANGE constant represents the orange color
     ORANGE = 'orange'
+
+    # The GREEN constant represents the green color
     GREEN = 'green'
+
+    # The RED constant represents the red color
     RED = 'red'
+
+    # The GREEN_DISABLED constant represents the disabled green color
     GREEN_DISABLED = 'green_disabled'
+
+    # The YELLOW constant represents the yellow color
     YELLOW = 'yellow'
+
+    # The GRAY constant represents the gray color
     GRAY = 'gray'
+
+    # These color constants can be used to set the color of battle notifications timers
+    # depending on their status or importance.

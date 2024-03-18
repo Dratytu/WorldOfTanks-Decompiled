b0372d12b8d88458054a2370ceb7817e0757525d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:36 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/impl/dialogs/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/impl/dialogs/__init__.py
@@ -1,11 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/impl/dialogs/__init__.py
-from enum import Enum
 
+from enum import Enum  # Importing Enum class from Python's standard library
+
+# Defining a custom Enum class named CurrencyTypeExtended
 class CurrencyTypeExtended(Enum):
-    CREDITS = 'credits'
-    GOLD = 'gold'
-    CRYSTAL = 'crystal'
-    XP = 'xp'
-    FREEXP = 'freeXP'
-    BR_COIN = 'brcoin'
+    # Defining constants for different types of in-game currencies
+    CREDITS = 'credits'  # Represents in-game credits
+    GOLD = 'gold'  # Represents in-game gold
+    CRYSTAL = 'crystal'  # Represents in-game crystals
+    XP = 'xp'  # Represents player experience points
+    FREEXP = 'freeXP'  # Represents free experience points
+    BR_COIN = 'brcoin'  # Represents Battle Royale coins

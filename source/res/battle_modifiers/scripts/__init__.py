a9cbd55dda0f23ac28890aa28a4574b88e98db9b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:26 +0300

    comment

--- source/res/battle_modifiers/scripts/__init__.py
+++ source/res/battle_modifiers/scripts/__init__.py
@@ -1,3 +1,14 @@
+# This is the entry point for the battle modifiers module.
+# The purpose of this module is to contain all code related to battle modifiers.
+
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/__init__.py
+
+# This empty script serves as a placeholder for the battle modifiers module.
+# As the module grows in complexity, this file will serve as a central location
+# for importing all necessary scripts related to battle modifiers.
+
+# For now, this file contains only a single "pass" statement, which serves
+# as a placeholder and does nothing when executed.
 pass
+

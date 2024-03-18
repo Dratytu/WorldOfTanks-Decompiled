c570a1eb95a8aaea6f045e1dfa11bfc34f3717d8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:10 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/__init__.py
@@ -1,3 +1,10 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
+# This script defines the module-level namespace for the gui.Scaleform.daapi.view package.
+# The package contains view-related code for the game's user interface.
+
+# There are no functions or classes defined in this script, only an empty module-level namespace.
+
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/__init__.py
 pass
+

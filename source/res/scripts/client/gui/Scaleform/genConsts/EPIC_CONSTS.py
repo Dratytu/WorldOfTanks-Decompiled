54db7a9a93d3fcfab3980a1f3a216a7d022cc688
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:06 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/genConsts/EPIC_CONSTS.py
+++ source/res/scripts/client/gui/Scaleform/genConsts/EPIC_CONSTS.py
@@ -1,21 +1,29 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/genConsts/EPIC_CONSTS.py
 
-
 class EPIC_CONSTS(object):
+    # Constants for UI modes
     SELECTED_UI_MODE = 1
     UI_MODE_DEFENDER = 2
     UI_MODE_ATTACKER = 1
+
+    # Constants for teams
     TEAM_ATTACKER = 1
     TEAM_DEFENDER = 2
+
+    # Constants for sector states
     SECTOR_CLOSED = 0
     SECTOR_FIGHTING = 1
     SECTOR_CLOSING = 2
     SECTOR_CAPTURED = 3
     SECTOR_BOMBING = 4
+
+    # Constants for lanes
     LANE_1 = 1
     LANE_2 = 2
     LANE_3 = 3
+
+    # Constants for zone transitions
     ZONE_TRANSITION_ATK = 0
     ZONE_TRANSITION_DEF = 1
     HQ_DESTROYED_ATK = 2
@@ -44,32 +52,22 @@ class EPIC_CONSTS(object):
     MSG_ZONE_CONTESTED_DEF = 27
     MSG_HQ_ATTACK_ATK = 28
     MSG_HQ_ATTACK_DEF = 29
+
+    # Constants for zone death timer delay
     ZONE_DEATH_TIMER_DELAY = 4000
+
+    # Constants for resupply states
     RESUPPLY_READY = 0
     RESUPPLY_BLOCKED = 1
     RESUPPLY_FULL = 2
+
+    # Constants for primary mission types
     PRIMARY_BASE_MISSION = 1
     PRIMARY_HQ_MISSION = 2
     PRIMARY_REGROUP_MISSION = 3
     PRIMARY_WAYPOINT_MISSION = 4
     PRIMARY_EMPTY_MISSION = 5
+
+    # Constants for spectator mode
     SPECTATOR_MODE_FREECAM = 1
-    SPECTATOR_MODE_FOLLOW = 2
-    SPECTATOR_MODE_DEATHCAM = 3
-    TOP_RANK = 5
-    BASE_NEUTRAL_STATE = 1
-    BASE_POSITIVE_STATE = 2
-    BASE_NEGATIVE_STATE = 3
-    RECOVERY_UNAVAILABLE = -1
-    RECOVERY_AVAILABLE = 0
-    RECOVERY_ACTIVE = 2
-    RECOVERY_CANCELED = 3
-    RECOVERY_BLOCKED = 4
-    TARGET_HQ = 1
-    TARGET_BASE = 2
-    TARGET_NONE = -1
-    SPG_LIMIT_REACHED = 1
-    PLAYER_LANE_LIMIT_REACHED = 2
-    PROTECTION_ZONE_WARNING = 'artillery'
-    BOMBING_WARNING = 'airstrike'
-    SPECTATOR_VIEW_STATES = [SPECTATOR_MODE_FREECAM, SPECTATOR_MODE_FOLLOW, SPECTATOR_MODE_DEATHCAM]
+    SPECT

13911424413c23d27ce8a1d4d4bb639b4a738c3e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:14 +0300

    comment

--- source/res/battle_modifiers/scripts/client/__init__.py
+++ source/res/battle_modifiers/scripts/client/__init__.py
@@ -1,3 +1,9 @@
+# This is the entry point for the battle modifiers module.
+# The purpose of this module is to provide any necessary modifications to the game's battle mechanics.
+
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/client/__init__.py
-pass
+
+# The passages of code that follow are currently empty, but they may be used in the future
+# to import and initialize specific battle modifiers.
+

e0be77a5d8706cf1d9f74ff9675daca4a06085c0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:58 +0300

    comment

--- source/res/scripts/client/BotPoint.py
+++ source/res/scripts/client/BotPoint.py
@@ -1,10 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/BotPoint.py
-import BigWorld
-from debug_utils import LOG_DEBUG
 
+import BigWorld # Importing BigWorld for creating a UserDataObject
+from debug_utils import LOG_DEBUG # Importing LOG_DEBUG for logging debug information
+
+# Defining the BotPoint class that inherits from BigWorld.UserDataObject
 class BotPoint(BigWorld.UserDataObject):
 
     def __init__(self):
+        # Calling the constructor of the parent class
         BigWorld.UserDataObject.__init__(self)
+        
+        # Logging the debug information with the position of the BotPoint
         LOG_DEBUG('BotPoint ', self.position)

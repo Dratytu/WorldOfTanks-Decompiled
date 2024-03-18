5067e61e764b990bde24023866880e3cc0c2e645
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:20 +0300

    comment

--- source/res/abc_tester/scripts/client/AccountAbcTesterComponent.py
+++ source/res/abc_tester/scripts/client/AccountAbcTesterComponent.py
@@ -1,6 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: abc_tester/scripts/client/AccountAbcTesterComponent.py
+
+# Importing the BaseAccountExtensionComponent class from the BaseAccountExtensionComponent module
 from BaseAccountExtensionComponent import BaseAccountExtensionComponent
 
+# Defining the AccountAbcTesterComponent class which inherits from the BaseAccountExtensionComponent class
 class AccountAbcTesterComponent(BaseAccountExtensionComponent):
+    # The AccountAbcTesterComponent class doesn't contain any additional code beyond what's inherited from the BaseAccountExtensionComponent class
     pass
+

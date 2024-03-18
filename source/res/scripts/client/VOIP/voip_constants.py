9148c757b073de6a215f771f200bf3c0e07beddb
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:20 +0300

    comment

--- source/res/scripts/client/VOIP/voip_constants.py
+++ source/res/scripts/client/VOIP/voip_constants.py
@@ -1,7 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/VOIP/voip_constants.py
 
-
 class VOIP_SUPPORTED_API(object):
+    """
+    A class representing the different APIs that are supported for Voice Over IP (VOIP) functionality.
+    """
     VIVOX = 'VIVOX'
+    """
+    A constant representing the Vivox API for VOIP functionality.
+    """
     YY = 'YY'
+    """
+

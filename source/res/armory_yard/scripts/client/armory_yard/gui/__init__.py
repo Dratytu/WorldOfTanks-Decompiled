d31056993ca6705fc192ccbd8e9e644fa7612c28
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:23 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/__init__.py
@@ -1,3 +1,10 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/__init__.py
+
+# This is an empty __init__.py file for the armory_yard.gui module.
+# It serves to initialize the module and make it importable as a package.
+
+# There are no functions or classes defined in this file.
+
 pass
+

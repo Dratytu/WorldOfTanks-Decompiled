5d45737b4f1e3e1aa29699881df9e28898dfaf89
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:30 +0300

    improve

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/lobby/feature/tooltips/entry_point_not_active_tooltip_view.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/lobby/feature/tooltips/entry_point_not_active_tooltip_view.py
@@ -1,18 +1,20 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/lobby/feature/tooltips/entry_point_not_active_tooltip_view.py
 from frameworks.wulf import ViewFlags, ViewSettings
-import armory_yard.gui.impl.gen.view_models.views.lobby.feature.tooltips.entry_point_not_active_tooltip_view_model as model
+import armory_yard.gui.impl.gen.view_models.views.lobby.feature.tooltips.entry_point_not_active_tooltip_view_model as entry_point_not_active_tooltip_view_model
 from gui.impl.pub import ViewImpl
+
 
 class EntryPointNotActiveTooltipView(ViewImpl):
     __slots__ = ()
 
-    def __init__(self, layoutID):
-        settings = ViewSettings(layoutID)
+    def __init__(self, layout_id: int):
+        settings = ViewSettings(layout_id)
         settings.flags = ViewFlags.LOBBY_SUB_VIEW
-        settings.model = model.EntryPointNotActiveTooltipViewModel()
-        super(EntryPointNotActiveTooltipView, self).__init__(settings)
+        settings.model = entry_point_not_active_tooltip_view_model.EntryPointNotActiveTooltipViewModel()
+        super().__init__(settings)
 
     @property
-    def viewModel(self):
-        return super(EntryPointNotActiveTooltipView, self).getViewModel()
+    def view_model(self) -> entry_point_not_active_tooltip_view_model.EntryPointNotActiveTooltipViewModel:
+        return super().getViewModel()
+

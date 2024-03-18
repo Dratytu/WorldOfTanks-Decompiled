9fb125284ddef21b0aeb3dcc41ca5532322fc1a6
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:19 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/markers2d/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/markers2d/__init__.py
@@ -1,3 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/markers2d/__init__.py
-pass
+
+# This script defines the base module for 2D markers in the Battle Royale mode of the game.
+# It does not contain any specific marker implementations, but rather sets up the necessary
+# framework for creating and managing markers.
+
+# The module is written in Python 2.7 and uses the decompiled bytecode format.
+
+# Note that this module does not contain any code other than the pass statement,
+# which serves as a placeholder and should be replaced with the actual implementation.

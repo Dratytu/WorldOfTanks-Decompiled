d548316637c1f7730d3905215cf06348c07894f5
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:39 +0300

    comment

--- source/res/event_platform/scripts/client/__init__.py
+++ source/res/event_platform/scripts/client/__init__.py
@@ -1,3 +1,12 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: event_platform/scripts/client/__init__.py
-pass
+# event_platform/scripts/client/__init__.py
+# 
+# This is an empty Python file serving as the entry point for the 'client' module 
+# within the 'event_platform/scripts' package.
+#
+# As an entry point, this file serves to organize and facilitate the importing 
+# of various client-side scripts and modules that are part of the 'event_platform' 
+# project.
+#
+# The 'pass' statement is a placeholder and does not affect the functionality 
+# of the script. It is typically used as a reminder that some code should be 
+# added at a later time.

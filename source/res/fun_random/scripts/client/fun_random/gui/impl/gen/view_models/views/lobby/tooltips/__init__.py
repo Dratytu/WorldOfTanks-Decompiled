105270eef3ff8e2d117ddfd4cd4a9f3424da120c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:09 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/tooltips/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/tooltips/__init__.py
@@ -1,3 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
 # Embedded file name: fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/tooltips/__init__.py
+
+# This script defines the view models for the lobby tooltips in the Fun Random game.
+# It contains an empty file, indicating that there are no view models defined directly in this file.
+# Instead, other Python files in the 'fun_random/gui/impl/gen/view_models/views/lobby/tooltips' directory
+# define the view models for the lobby tooltips.
+
+# The 'pass' statement is a placeholder and does nothing. It is used here to indicate that
+# there is no code to execute in this file.
 pass
+

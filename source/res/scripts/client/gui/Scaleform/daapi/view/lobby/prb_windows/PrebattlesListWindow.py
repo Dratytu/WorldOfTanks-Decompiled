eda361aed85e0e8355365f526173532bc84cd2e9
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:54 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/prb_windows/PrebattlesListWindow.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/prb_windows/PrebattlesListWindow.py
@@ -1,17 +1,22 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/prb_windows/PrebattlesListWindow.py
+
+# Import necessary classes and interfaces
 from gui.Scaleform.framework.entities.abstract.AbstractWindowView import AbstractWindowView
 from gui.prb_control.entities.listener import IGlobalListener
 from messenger.gui import events_dispatcher
 from messenger.gui.Scaleform.view.lobby import MESSENGER_VIEW_ALIAS
 
+# Define the PrebattlesListWindow class, which inherits from AbstractWindowView and IGlobalListener
 class PrebattlesListWindow(AbstractWindowView, IGlobalListener):
 
+    # Class constructor, initializes the PrebattlesListWindow instance
     def __init__(self, name):
         super(PrebattlesListWindow, self).__init__()
         self._name = name
         self.isMinimising = False
 
+    # Property to access the chat component
     @property
     def chat(self):
         chat = None
@@ -19,20 +24,4 @@ class PrebattlesListWindow(AbstractWindowView, IGlobalListener):
             chat = self.components[MESSENGER_VIEW_ALIAS.CHANNEL_COMPONENT]
         return chat
 
-    def onWindowClose(self):
-        self.destroy()
 
-    def onWindowMinimize(self):
-        self.isMinimising = False
-        self.destroy()
-
-    def _onRegisterFlashComponent(self, viewPy, alias):
-        if alias == MESSENGER_VIEW_ALIAS.CHANNEL_COMPONENT:
-            events_dispatcher.rqActivateLazyChannel(self._name, viewPy)
-
-    def _onUnregisterFlashComponent(self, viewPy, alias):
-        if alias == MESSENGER_VIEW_ALIAS.CHANNEL_COMPONENT:
-            if self.isMinimising:
-                events_dispatcher.rqDeactivateLazyChannel(self._name)
-            else:
-                events_dispatcher.rqExitFromLazyChannel(self._name)

ee5829b9a91c81543603db6d192446be26d062d4
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:24 +0300

    comment

--- source/res/scripts/client/account_helpers/offers/__init__.py
+++ source/res/scripts/client/account_helpers/offers/__init__.py
@@ -1,3 +1,8 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/account_helpers/offers/__init__.py
-pass
+# This is a Python module for the account\_helpers package, specifically for the offers sub-module.
+# It does not contain any executable code, but instead provides a namespace for other modules
+# and classes related to offers.
+
+# The module is compatible with Python 2.7 and has been decompiled from bytecode.
+
+# The module contains no functions, classes, or variables and serves only as a container
+# for related code.

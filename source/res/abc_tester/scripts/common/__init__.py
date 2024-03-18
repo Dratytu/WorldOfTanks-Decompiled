652e0182b74ea7a7e0418f754e2ac92635e76181
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:13 +0300

    comment

--- source/res/abc_tester/scripts/common/__init__.py
+++ source/res/abc_tester/scripts/common/__init__.py
@@ -1,3 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: abc_tester/scripts/common/__init__.py
+
+# This is an empty __init__.py file, which is used to mark this directory as a
+# package in Python. When a directory contains an __init__.py file, it is
+# considered a package and can be imported as a module.
+#
+# In this particular case, this __init__.py file is located in the
+# abc_tester/scripts/common directory, which means that the common directory
+# is a package within the abc_tester/scripts module.
+#
+# By leaving this file empty, we are indicating that there are no specific
+# initialization steps required for this package. However, we could also use
+# this file to import specific modules or functions from this package, making
+# them available for import when the package is imported as a whole.
+
 pass
+

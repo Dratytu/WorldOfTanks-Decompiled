d4ebb8b4751bb2283091837039da2e714c2bdd94
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:27 +0300

    comment

--- source/res/battle_modifiers/scripts/client/battle_modifiers/gui/__init__.py
+++ source/res/battle_modifiers/scripts/client/battle_modifiers/gui/__init__.py
@@ -1,3 +1,12 @@
+# This is the initialization file for the battle modifiers GUI.
+# It does not contain any functional code, but rather serves
+# to import any necessary modules and set up the package structure.
+
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/client/battle_modifiers/gui/__init__.py
+
+# The pass statement is used as a placeholder, and no action is taken when it is executed.
+# It is used here to indicate that this file does not contain any code that needs to be executed
+# at the package level.
 pass
+

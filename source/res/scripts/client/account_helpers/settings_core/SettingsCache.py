84ed0a8cc834d9e9651c3196d6d0ab36a0c66df2
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:05 +0300

    comment

--- source/res/scripts/client/account_helpers/settings_core/SettingsCache.py
+++ source/res/scripts/client/account_helpers/settings_core/SettingsCache.py
@@ -1,11 +1,23 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/account_helpers/settings_core/SettingsCache.py
 
-from Event import Event  # Importing Event class from the Event module
-from adisp import adisp_async  # Importing adisp_async decorator
-from gui.ClientUpdateManager import g_clientUpdateManager  # Importing g_clientUpdateManager from the ClientUpdateManager module
-from gui.shared.utils.requesters.IntSettingsRequester import IntSettingsRequester  # Importing IntSettingsRequester from the IntSettingsRequester module
-from account_helpers.settings_core.settings_constants import VERSION  # Importing VERSION constant from the settings_constants module
-from skeletons.account_helpers.settings_core import ISettingsCache  # Importing ISettingsCache interface
+# Importing Event class from the Event module
+from Event import Event 
 
-class SettingsCache(ISettingsCache): 
+# Importing adisp_async decorator
+from adisp import adisp_async 
+
+# Importing g_clientUpdateManager from the ClientUpdateManager module
+from gui.ClientUpdateManager import g_clientUpdateManager 
+
+# Importing IntSettingsRequester from the IntSettingsRequester module
+from gui.shared.utils.requesters.IntSettingsRequester import IntSettingsRequester 
+
+# Importing VERSION constant from the settings_constants module
+from account_helpers.settings_core.settings_constants import VERSION 
+
+# Importing ISettingsCache interface
+from skeletons.account_helpers.settings_core import ISettingsCache 
+
+# Defining the SettingsCache class that implements the ISettingsCache interface
+class SettingsCache(ISettingsCache):

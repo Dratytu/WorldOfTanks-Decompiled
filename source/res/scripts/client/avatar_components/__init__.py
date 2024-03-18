41bcabb73483a016e97d073cade8f87b60025a34
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:30 +0300

    comment

--- source/res/scripts/client/avatar_components/__init__.py
+++ source/res/scripts/client/avatar_components/__init__.py
@@ -1,3 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/avatar_components/__init__.py
-__author__ = 'g_hebart'
+# 
+# This file serves as the initialization module for the avatar_components package.
+# It is authored by g_hebart.
+
+# avatar_components is a package that contains various components related to the
+# in-game avatar.
+
+# This module does not contain any executable code, but rather serves to organize
+# and provide a namespace for the various components.

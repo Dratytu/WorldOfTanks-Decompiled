96ae38a5b60d807f55a0bbeb3f82dd0a02397e64
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:33 +0300

    comment

--- source/res/scripts/client/frameworks/wulf/view/command.py
+++ source/res/scripts/client/frameworks/wulf/view/command.py
@@ -1,18 +1,28 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/frameworks/wulf/view/command.py
+
 import typing
-from Event import Event
+from Event import Event  # Import Event class from the Event module
 from ..py_object_binder import PyObjectEntity
 from ..py_object_wrappers import PyObjectCommand
 
 class Command(PyObjectEntity):
+    """
+    A Command class that represents a command object.
+    """
     __slots__ = ('__event',)
 
     def __init__(self):
+        """
+        Initialize the Command object.
+        """
         super(Command, self).__init__(PyObjectCommand())
-        self.__event = Event()
+        self.__event = Event()  # Create an Event object
 
     def __call__(self, args=None):
+        """
+        Call the associated event with the given arguments.
+        """
         if args is not None:
             args = (args,)
         else:
@@ -22,9 +32,15 @@ class Command(PyObjectEntity):
 
     @property
     def name(self):
+        """
+        Get the name of the command.
+        """
         return self.proxy.name
 
     def execute(self, args=None):
+        """
+        Execute the command with the given arguments.
+        """
         if args is not None:
             args = (args,)
         else:
@@ -33,10 +49,16 @@ class Command(PyObjectEntity):
         return
 
     def _unbind(self):
+        """
+        Unbind the command from its associated event.
+        """
         self.__event.clear()
         super(Command, self)._unbind()
 
     def _cNotify(self, args=None):
+        """
+        Call the associated event with the given arguments.
+        """
         if args is not None:
             args = (args,)
         else:
@@ -45,9 +67,10 @@ class Command(PyObjectEntity):
         return
 
     def __iadd__(self, delegate):
+        """
+        Add a delegate to the associated event.
+        """
         self.__event += delegate
         return self
 
-    def __isub__(self, delegate):
-        self.__event -= delegate
-        return self
+    def __is

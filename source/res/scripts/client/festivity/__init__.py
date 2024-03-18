8ced8391328f21298d766559508e36c983024856
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:09 +0300

    comment

--- source/res/scripts/client/festivity/__init__.py
+++ source/res/scripts/client/festivity/__init__.py
@@ -1,8 +1,16 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/festivity/__init__.py
-from festivity.dummy.df_factory import DummyFactory
-from skeletons.festivity_factory import IFestivityFactory
+# This script contains the implementation for the festivity module.
+# It sets up a dummy festivity factory for use in the game.
 
+from festivity.dummy.df_factory import DummyFactory  # Importing the DummyFactory class from the festivity.dummy.df_factory module.
+from skeletons.festivity_factory import IFestivityFactory  # Importing the IFestivityFactory interface from the skeletons.festivity_factory module.
+
+# The getFestivityConfig function takes a manager object as an argument.
 def getFestivityConfig(manager):
+    # It creates an instance of the DummyFactory class and assigns it to the festivityFactory variable.
     festivityFactory = DummyFactory()
-    manager.addInstance(IFestivityFactory, festivityFactory)
+    
+    # The addInstance method of the manager object is called with the IFestivityFactory interface and the festivityFactory instance as arguments.
+    # This associates the IFestivityFactory interface with the festivityFactory instance in the manager object.
+    manager.addInstance(IFestityFactory, festivityFactory)
+
+    # The festivityFactory instance is now set up and ready to be used in the game.

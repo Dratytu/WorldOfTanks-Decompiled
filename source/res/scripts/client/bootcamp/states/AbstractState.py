79498c3eb872f991ebe0f09694e52f4f216641b4
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:33 +0300

    comment

--- source/res/scripts/client/bootcamp/states/AbstractState.py
+++ source/res/scripts/client/bootcamp/states/AbstractState.py
@@ -1,49 +1,62 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/bootcamp/states/AbstractState.py
+
 import BigWorld
 from debug_utils_bootcamp import LOG_DEBUG_DEV_BOOTCAMP
 
 class AbstractState(object):
+    """
+    Abstract base class for all bootcamp states.
+    Provides basic functionality for state management.
+    """
 
     def __init__(self, stateId):
+        """
+        Initialize the state object with a unique state identifier.
+        :param stateId: A unique identifier for the state.
+        """
         super(AbstractState, self).__init__()
-        self.__id = stateId
-        self.__isActive = False
+        self.__id = stateId  # The unique identifier for the state.
+        self.__isActive = False  # Flag to track if the state is currently active.
 
     def id(self):
+        """
+        Return the unique identifier for the state.
+        :return: The unique identifier for the state.
+        """
         return self.__id
 
     def handleKeyEvent(self, event):
+        """
+        Override this method in subclasses to handle key events.
+        :param event: The key event to handle.
+        """
         pass
 
     def activate(self):
+        """
+        Activate the state.
+        This method should only be called when the state is not already active.
+        """
         if self.__isActive:
             LOG_DEBUG_DEV_BOOTCAMP('State.activate: state is already active')
             return
         self.__isActive = True
-        self._doActivate()
+        self._doActivate()  # Activate the state by calling the protected method.
 
     def deactivate(self):
+        """
+        Deactivate the state.
+        This method should only be called when the state is currently active.
+        """
         if not self.__isActive:
             LOG_DEBUG_DEV_BOOTCAMP('State.deactivate: state is already not active')
             return
-        self._doDeactivate()
+        self._doDeactivate()  # Deactivate the state by calling the protected method.
         self.__isActive = False
 
     def onSpaceLoadCompleted(self):
-        BigWorld.player().onSpaceLoaded()
-
-    def onAvatarBecomeNonPlayer(self):
-        pass
-
-    def onArenaLoadCompleted(self):
-        pass
-
-    def getIntroVideoData(self):
-        return {}
-
-    def _doActivate(self):
-        raise NotImplementedError('Should be implemented in subclass')
-
-    def _doDeactivate(self):
-        raise NotImplementedError('Should be implemented in subclass')
+        """
+        Callback method called when the space has finished loading.
+        """
+        BigWorld

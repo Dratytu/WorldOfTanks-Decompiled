a6aacbaf7ca04922b400c720a4711de090ab4e8e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:37 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/__init__.py
@@ -1,3 +1,19 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/__init__.py
-pass
+# This is a Python script that is part of the battle_royale module,
+# specifically in the scripts/client/battle_royale/gui/impl/gen directory.
+
+# The script does not contain any executable code, but instead is used for
+# generating view models for the Battle Royale game mode.
+
+# The script contains a single pass statement, which is used as a placeholder
+# and has no effect when the script is executed.
+
+# The purpose of this script is to import all view models for the Battle Royale
+# game mode, which are generated based on .py files located in the
+# battle_royale/gui/impl/gen/view_models directory.
+
+# By importing all view models in this way, they become available for use in
+# other parts of the codebase, without the need to import them individually.
+
+# It is important to note that this script does not contain any business logic,
+# but simply serves as a container for the generated view models.
+

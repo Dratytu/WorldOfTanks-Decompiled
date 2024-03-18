997ffb59010782b61dcef2bc2adff3a548fcc971
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:35 +0300

    comment

--- source/res/scripts/client/festivity/dummy/df_requester.py
+++ source/res/scripts/client/festivity/dummy/df_requester.py
@@ -1,6 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/festivity/dummy/df_requester.py
+
+# Importing the required base class 'BaseFestivityRequester' from the 'festivity.base' module.
 from festivity.base import BaseFestivityRequester
 
-class DummyRequester(BaseFestivityRequester):
-    dataKey = 'dummyFestivity'

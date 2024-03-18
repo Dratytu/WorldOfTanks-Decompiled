2a75018575b167acd483edb1daaaa68d594e93ec
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:44 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/game_control/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/game_control/__init__.py
@@ -1,7 +1,11 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/gui/game_control/__init__.py
-from fun_random.gui.game_control.awards_controller import FunProgressionQuestsHandler
-from gui.shared.system_factory import registerAwardControllerHandler
+# This script contains the initialization code for Fun Random game control modules.
 
+# Import necessary modules and classes.
+from fun_random.gui.game_control.awards_controller import FunProgressionQuestsHandler  # Import the FunProgressionQuestsHandler class.
+from gui.shared.system_factory import registerAwardControllerHandler  # Import the registerAwardControllerHandler function.
+
+# Define the registerFunRandomAwardControllers function.
+# This function registers the FunProgressionQuestsHandler class as an award controller handler.
 def registerFunRandomAwardControllers():
+    # Register the FunProgressionQuestsHandler class using the registerAwardControllerHandler function.
     registerAwardControllerHandler(FunProgressionQuestsHandler)

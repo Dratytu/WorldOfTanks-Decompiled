4b71cae97682ffa532a3c0a52c42d7d3835658c4
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:18 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/epic/battle_carousel_filters.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/epic/battle_carousel_filters.py
@@ -2,8 +2,11 @@
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/epic/battle_carousel_filters.py
 
 # Importing the RoleCriteriesGroup class from the gui.Scaleform.daapi.view.common.vehicle_carousel.carousel_filter module
+# RoleCriteriesGroup is a class that handles a group of criteria for filtering vehicles based on their roles
 from gui.Scaleform.daapi.view.common.vehicle_carousel.carousel_filter import RoleCriteriesGroup
 
 # Importing the RequestCriteria and PredicateCondition classes from the gui.shared.utils.requesters.ItemsRequester module
+# RequestCriteria is a class that represents a set of conditions for filtering items
+# PredicateCondition is a class that represents a single condition for filtering items
 from gui.shared.utils.requesters.ItemsRequester import RequestCriteria, PredicateCondition
 

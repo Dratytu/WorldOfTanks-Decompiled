d23c3b548666ac3eda027442de05197ac1d03f77
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:28 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/AnimatedBackgroundScreenMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/AnimatedBackgroundScreenMeta.py
@@ -1,8 +1,17 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/AnimatedBackgroundScreenMeta.py
+
+# Import the BaseDAAPIComponent class from the gui.Scaleform.framework.entities package
 from gui.Scaleform.framework.entities.BaseDAAPIComponent import BaseDAAPIComponent
 
+# Define the AnimatedBackgroundScreenMeta class, which inherits from BaseDAAPIComponent
 class AnimatedBackgroundScreenMeta(BaseDAAPIComponent):
 
+    # Define the as_setImageDataS method, which takes three parameters:
+    # backgroundImgPath: A string representing the path to the background image
+    # fogImgPath: A string representing the path to the fog image
+    # animate: A boolean indicating whether or not to animate the background
     def as_setImageDataS(self, backgroundImgPath, fogImgPath, animate):
+        # If the DAAPI is initialized, call the as_setImageData method on the flashObject with the provided parameters
         return self.flashObject.as_setImageData(backgroundImgPath, fogImgPath, animate) if self._isDAAPIInited() else None
+

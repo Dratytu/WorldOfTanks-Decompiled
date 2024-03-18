c6cd3a300b1cf90d4961f7fce1a907eaa1947b7d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:59 +0300

    comment

--- source/res/scripts/client/NextClipsReloader.py
+++ source/res/scripts/client/NextClipsReloader.py
@@ -1,6 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/NextClipsReloader.py
+
+# Importing the GunReloadBoost class from GunReloadBoost module
 from GunReloadBoost import GunReloadBoost
 
+# Defining a new class named NextClipsReloader which inherits from GunReloadBoost class
 class NextClipsReloader(GunReloadBoost):
+    # The NextClipsReloader class doesn't have any additional methods or attributes defined here,
+    # so it inherits all the attributes and methods from the GunReloadBoost class.
     pass
+

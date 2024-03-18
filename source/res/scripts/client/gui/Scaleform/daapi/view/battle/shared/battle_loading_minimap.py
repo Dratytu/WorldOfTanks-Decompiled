ed98b0454b49c7ef212905485fd717c5625fb219
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:13 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/shared/battle_loading_minimap.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/shared/battle_loading_minimap.py
@@ -1,6 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/shared/battle_loading_minimap.py
+
+# Importing the MinimapLobby class from the gui.Scaleform.daapi.view.lobby.MinimapLobby module
 from gui.Scaleform.daapi.view.lobby.MinimapLobby import MinimapLobby
 
+# Defining the BattleLoadingMinimapComponent class which is a subclass of MinimapLobby
 class BattleLoadingMinimapComponent(MinimapLobby):
+    # The BattleLoadingMinimapComponent class doesn't contain any additional methods or attributes,
+    # so it inherits all the attributes and methods from the MinimapLobby class.
     pass
+

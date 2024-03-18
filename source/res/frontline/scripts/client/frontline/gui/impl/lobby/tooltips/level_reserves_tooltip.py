585b3dc14c5cf1bc8b4a09a9be2c1d60f5c787d4
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:48 +0300

    comment

--- source/res/frontline/scripts/client/frontline/gui/impl/lobby/tooltips/level_reserves_tooltip.py
+++ source/res/frontline/scripts/client/frontline/gui/impl/lobby/tooltips/level_reserves_tooltip.py
@@ -1,5 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: frontline/scripts/client/frontline/gui/impl/lobby/tooltips/level_reserves_tooltip.py
+
+# Import necessary modules and dependencies
 from frameworks.wulf import ViewFlags, ViewSettings
 from frontline.gui.impl.gen.view_models.views.lobby.tooltips.level_reserves_tooltip_model import LevelReservesTooltipModel
 from gui.impl.pub import ViewImpl
@@ -7,23 +9,19 @@ from gui.impl.gen import R
 from helpers import dependency
 from skeletons.gui.game_control import IEpicBattleMetaGameController
 
+# Define the LevelReservesTooltip class
 class LevelReservesTooltip(ViewImpl):
+    # Inject the IEpicBattleMetaGameController dependency
     __epicController = dependency.descriptor(IEpicBattleMetaGameController)
 
     def __init__(self):
+        # Initialize the view with settings and a LevelReservesTooltipModel
         settings = ViewSettings(R.views.frontline.lobby.tooltips.LevelReservesTooltip(), ViewFlags.VIEW, LevelReservesTooltipModel())
         super(LevelReservesTooltip, self).__init__(settings)
 
     @property
     def viewModel(self):
+        # Return the view model
         return super(LevelReservesTooltip, self).getViewModel()
 
-    def _onLoading(self, *args, **kwargs):
-        super(LevelReservesTooltip, self)._onLoading(*args, **kwargs)
-        with self.viewModel.transaction() as vm:
-            levels = vm.getLevels()
-            levels.clear()
-            for level in self.__epicController.getSkillLevelRanks():
-                levels.addString(level)
-
-            levels.invalidate()
+    def _on

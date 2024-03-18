5e9d6a5b10f9bf365c24c39433d5662cf95ed2a0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:01 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/impl/lobby/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/impl/lobby/__init__.py
@@ -1,3 +1,10 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/gui/impl/lobby/__init__.py
+
+# This is a Python script for the fun_random module, specifically for the lobby component.
+# It is contained within the fun_random/gui/impl/lobby directory.
+
+# The script does not contain any active code, as indicated by the pass statement.
+# This may be a placeholder or an empty script, to be filled with code at a later time.
+
 pass
+

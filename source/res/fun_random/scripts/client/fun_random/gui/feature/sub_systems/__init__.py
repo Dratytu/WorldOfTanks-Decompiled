7c8f7a70226ee3388f3bfb5a1aeef99a31c83137
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:02 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/feature/sub_systems/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/feature/sub_systems/__init__.py
@@ -1,3 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/feature/sub_systems/__init__.py
-pass
+
+# This is an empty __init__.py file for the 'sub_systems' package within the
+# 'fun_random/gui/feature' module.
+#
+# In Python, __init__.py files are used to initialize a package. In this case,
+# this file does not contain any code and serves only to mark the directory as
+# a package.
+#
+# The 'sub_systems' package may contain various sub-systems related to the
+# 'fun_random' feature and its graphical user interface (GUI). However, based
+# on the contents of this file, it is currently empty.

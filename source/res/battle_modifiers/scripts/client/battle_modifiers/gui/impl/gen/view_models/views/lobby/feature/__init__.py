d744571a8a8d151d43e17c456150e3f9f81760f2
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:13 +0300

    comment

--- source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/view_models/views/lobby/feature/__init__.py
+++ source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/view_models/views/lobby/feature/__init__.py
@@ -1,5 +1,4 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/view_models/views/lobby/feature/__init__.py
-
 # This is an empty __init__.py file, which is used to mark this directory as a package
 # in Python. It does not contain any code or functionality itself.
+# The presence of this file allows this directory to be imported as a module,
+# enabling organization and modularity within the project.

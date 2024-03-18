0f9fdb62d54114a4b3147c14bcd1f82284d44fac
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:39 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/__init__.py
@@ -1,3 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/__init__.py
-pass
+
+# This is an empty Python file that serves as a container for view models
+# related to the lobby of the Battle Royale game mode. Currently, there are
+# no view models defined in this file.
+
+# As the project grows and new view models are added, they can be placed
+# here for organization and easy import in other modules.
+
+# By keeping all lobby-related view models in one place, it becomes easier
+# to maintain and update the codebase, as well as to understand the
+# structure of the lobby views.
+
+# Example usage:
+# from battle_royale.gui.impl.gen.view_models.views.lobby.some_view_model import SomeViewModel

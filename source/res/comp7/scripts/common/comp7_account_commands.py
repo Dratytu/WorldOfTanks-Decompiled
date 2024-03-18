4d6660d02f66edcc25c2a3e4f43b2c6788f07aa0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:50 +0300

    comment

--- source/res/comp7/scripts/common/comp7_account_commands.py
+++ source/res/comp7/scripts/common/comp7_account_commands.py
@@ -1,3 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: comp7/scripts/common/comp7_account_commands.py
+
+# This script contains constants for account-related commands in the
+# Comp7 system.
+
+# Command constants are used to identify specific commands within
+# the Comp7 system. They are assigned unique integer values to
+# distinguish them from one another.
+
+# The CMD_QUAL_COMPLETE_PRODUCT_DEV constant is defined here
+# with a value of 30001. This command constant is likely used
+# for completing product development tasks within the Comp7
+# system.
 CMD_QUAL_COMPLETE_PRODUCT_DEV = 30001

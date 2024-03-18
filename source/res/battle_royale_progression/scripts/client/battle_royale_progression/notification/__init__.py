5beccab2fd67f03e3aa4b19a8d9d0df931d7602d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:49 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/battle_royale_progression/notification/__init__.py
+++ source/res/battle_royale_progression/scripts/client/battle_royale_progression/notification/__init__.py
@@ -1,7 +1,13 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale_progression/scripts/client/battle_royale_progression/notification/__init__.py
+# This Python script contains the initialization code for Battle Royale progression notifications.
+
+# Import necessary modules and classes.
 from battle_royale_progression.notification.actions_handlers import ShowBRProgressionActionHandler
 from gui.shared.system_factory import registerNotificationsActionsHandlers
 
+# Define a function to register Battle Royale progression notification handlers.
 def registerClientNotificationHandlers():
+    # Register the ShowBRProgressionActionHandler to handle Battle Royale progression notifications.
     registerNotificationsActionsHandlers((ShowBRProgressionActionHandler,))
+
+# Call the registerClientNotificationHandlers function to initialize the notification handlers.
+registerClientNotificationHandlers()

886c3283eb53b0e23dd1a5a73e9ec1641bb6ba1a
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:05 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/genConsts/TEXT_ALIGN.py
+++ source/res/scripts/client/gui/Scaleform/genConsts/TEXT_ALIGN.py
@@ -1,8 +1,16 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/gui/Scaleform/genConsts/TEXT_ALIGN.py
-
+# This module defines a class for text alignment constants used in GUI scripts.
+# These constants are used to specify how text should be aligned within a given
+# graphical user interface (GUI) element.
 
 class TEXT_ALIGN(object):
+    # The LEFT constant indicates that text should be aligned to the left
+    # margin of the GUI element.
     LEFT = 'left'
+
+    # The RIGHT constant indicates that text should be aligned to the right
+    # margin of the GUI element.
     RIGHT = 'right'
+
+    # The CENTER constant indicates that text should be centered within the
+    # GUI element.
     CENTER = 'center'

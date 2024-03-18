15a61aefb294cfe75c6150b999d684ab184480fa
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:57 +0300

    comment

--- source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/gen/view_models/views/lobby/gui_lootboxes/__init__.py
+++ source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/gen/view_models/views/lobby/gui_lootboxes/__init__.py
@@ -1,3 +1,20 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/gen/view_models/views/lobby/gui_lootboxes/__init__.py
-pass
+from gui.impl.gen.view_models.views.lobby.gui_lootboxes.loot_box_purchase_view_model import LootBoxPurchaseViewModel
+
+class LootBoxPurchaseViewModel(ViewModel):
+
+    def __init__(self, type, price):
+        super(LootBoxPurchaseViewModel, self).__init__()
+        self._type = type
+        self._price = price
+
+    @property
+    def type(self):
+        return self._type
+
+    @property
+    def price(self):
+        return self._price
+
+    def purchase(self):
+        # code to purchase a loot box
+        pass

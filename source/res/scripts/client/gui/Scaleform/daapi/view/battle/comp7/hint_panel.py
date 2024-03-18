b466f813542d1b3d654d48c6c7331646908acd4d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:34 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/comp7/hint_panel.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/comp7/hint_panel.py
@@ -1,8 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/comp7/hint_panel.py
+
+# Importing the BattleHintPanel class from the gui.Scaleform.daapi.view.battle.shared.hint_panel.component module
 from gui.Scaleform.daapi.view.battle.shared.hint_panel.component import BattleHintPanel
 
+# Defining the Comp7BattleHintPanel class that inherits from BattleHintPanel
 class Comp7BattleHintPanel(BattleHintPanel):
 
+    # Overriding the _initPlugins method
     def _initPlugins(self):
+        # This method is currently empty, so there's nothing to comment on it
         pass
+

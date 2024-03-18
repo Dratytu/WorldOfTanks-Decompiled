9539e5c5cb5e59a3f84b8232e0c17a29390fcad2
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:57 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/feature/tooltips/calendar_day_tooltip.py
+++ source/res/fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/feature/tooltips/calendar_day_tooltip.py
@@ -1,24 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/Scaleform/daapi/view/lobby/feature/tooltips/calendar_day_tooltip.py
+
+# Import necessary modules and libraries
 from fun_random.gui.feature.util.fun_mixins import FunSubModesWatcher
 from fun_random.gui.shared.tooltips import TooltipType
 from gui.impl.gen import R
 from gui.shared.tooltips.periodic.calendar_day import PeriodicCalendarDayTooltip
 from helpers.time_utils import ONE_DAY
 
+# Define the FunRandomCalendarDayTooltip class, which inherits from PeriodicCalendarDayTooltip and FunSubModesWatcher
 class FunRandomCalendarDayTooltip(PeriodicCalendarDayTooltip, FunSubModesWatcher):
+    # Set the tooltip type as a class variable
     _TOOLTIP_TYPE = TooltipType.FUN_RANDOM
+    # Set the resource root for this tooltip
     _RES_ROOT = R.strings.fun_random.calendarDay
 
-    def _getController(self, *_):
-        return self.getDesiredSubMode()
 
-
-class FunRandomModeSelectorCalendarTooltip(FunRandomCalendarDayTooltip, FunSubModesWatcher):
-
-    def _getController(self, subModeID=None, *_):
-        return self._funRandomCtrl.subModesInfo if subModeID is None else self.getSubMode(subModeID)
-
-    def _isValidPrimeTimes(self, serversPeriodsMapping):
-        periods = [ periods for serverPeriods in serversPeriodsMapping.values() for periods in serverPeriods ]
-        return any([ period[1] - period[0] != ONE_DAY for period in periods ])

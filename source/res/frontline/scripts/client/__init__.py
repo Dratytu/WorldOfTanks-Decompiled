2835f0b9fa1768106b6b9fffeae990802dea5700
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:37 +0300

    comment

--- source/res/frontline/scripts/client/__init__.py
+++ source/res/frontline/scripts/client/__init__.py
@@ -1,3 +1,13 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: frontline/scripts/client/__init__.py
+# frontline/scripts/client/__init__.py
+
+# This is an empty Python file that serves as the entry point for the 'frontline'
+# package's 'scripts/client' module.
+#
+# Python scripts generally do not require an explicit `__init__.py` file, but
+# having one can be useful for organizing and documenting a module's structure.
+#
+# In this case, the 'scripts/client' module is likely to contain various scripts
+# related to the client-side of the 'frontline' application.
+
 pass
+

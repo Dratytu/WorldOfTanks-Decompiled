0c3788009b5ef20b50aad15b6fde39c52679e684
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:51 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/shared/fitting_select/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/shared/fitting_select/__init__.py
@@ -1,3 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/shared/fitting_select/__init__.py
-pass
+
+# This is an empty __init__.py file, which is used to indicate that this
+# directory should be treated as a package in Python.
+#
+# In this case, the package is named 'fitting\_select' and is located
+# inside the 'shared' directory within the 'gui/Scaleform/daapi/view/lobby'
+# hierarchy of the project.
+#
+# An empty __init__.py file is required for a directory to be recognized
+# as a package in Python, and can contain initialization code for the
+# package if necessary. However, in this case, no such code is needed.

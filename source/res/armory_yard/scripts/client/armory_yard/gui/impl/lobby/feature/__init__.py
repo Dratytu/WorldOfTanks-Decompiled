5a450aad37514fc2e7665b24bdd26b21fe5c3f93
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:09 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/lobby/feature/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/lobby/feature/__init__.py
@@ -1,6 +1,9 @@
 # armory_yard/scripts/client/armory_yard/gui/impl/lobby/feature/__init__.py
 
-# This is an empty __init__.py file for the armory_yard.gui.impl.lobby.feature package.
-# Its purpose is to signal that this directory should be treated as a package in Python.
-# It contains no functionality on its own and is only used to organize related modules
-# and sub-packages within the armory_yard.gui.impl.lobby.feature package.
+# This file is an empty __init__.py file for the armory_yard.gui.impl.lobby.feature package.
+# Its purpose is to inform Python that this directory should be treated as a package,
+# enabling the importing of modules and sub-packages contained within this directory.
+#
+# This particular file does not contain any functionality itself and is only used for
+# organizational purposes, allowing related modules and sub-packages to be grouped
+# together within the armory_yard.gui.impl.lobby.feature package.

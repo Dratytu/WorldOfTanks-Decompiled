8fef3da84c391282a184e49bfc9d3b905b3f6f88
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:17 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/MinimapGridMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/MinimapGridMeta.py
@@ -1,17 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/MinimapGridMeta.py
+
+# Base class for DAAPI components, providing basic functionality for interacting with Scaleform
 from gui.Scaleform.framework.entities.BaseDAAPIComponent import BaseDAAPIComponent
 
-class MinimapGridMeta(BaseDAAPIComponent):
-
-    def setClick(self, x, y):
-        self._printOverrideError('setClick')
-
-    def as_clickEnabledS(self, value):
-        return self.flashObject.as_clickEnabled(value) if self._isDAAPIInited() else None
-
-    def as_addPointS(self, x, y):
-        return self.flashObject.as_addPoint(x, y) if self._isDAAPIInited() else None
-
-    def as_clearPointsS(self):
-        return self.flashObject.as_clearPoints() if self._isDAAPIInited() else None

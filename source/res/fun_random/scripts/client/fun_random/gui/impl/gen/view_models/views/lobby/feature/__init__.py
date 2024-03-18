a2c228446733d1656d166cd6188d49e21f2c0b84
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:08 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/feature/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/feature/__init__.py
@@ -1,3 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/feature/__init__.py
-pass
+
+# This is an empty Python module that serves as a placeholder for
+# organizing view models related to lobby features in the fun_random module.
+# Currently, there are no view models implemented in this module.
+
+# As the project grows and more view models are added, they can be organized
+# into sub-modules within the 'fun_random/gui/impl/gen/view_models/views/lobby/feature/'
+# directory, and then imported and used in this module as needed.
+
+# By keeping related view models organized in this way, it can help make the code
+# easier to navigate and maintain over time.
+
+# Example usage:
+# from fun_random.gui.impl.gen.view_models.views.lobby.feature.some_view_model import SomeViewModel

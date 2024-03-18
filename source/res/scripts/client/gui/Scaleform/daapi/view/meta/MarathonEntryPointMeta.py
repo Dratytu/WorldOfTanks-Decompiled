9392f7a8e9b1e05e33b73991166560792bde8f71
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:09 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/MarathonEntryPointMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/MarathonEntryPointMeta.py
@@ -1,6 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/MarathonEntryPointMeta.py
-from gui.Scaleform.framework.entities.inject_component_adaptor import InjectComponentAdaptor
+
+from gui.Scaleform.framework.entities.inject_component_adaptor import InjectComponentAdaptor  # Importing InjectComponentAdaptor base class
 
 class MarathonEntryPointMeta(InjectComponentAdaptor):
-    pass
+    """
+    A metadata class for the MarathonEntryPoint component.
+    This class is derived from the InjectComponentAdaptor base class.
+    """
+    pass  # The class does not contain any methods or properties at the moment
+

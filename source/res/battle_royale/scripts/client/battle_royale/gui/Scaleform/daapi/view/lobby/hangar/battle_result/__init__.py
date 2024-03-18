8169046af17b9ebe5ab10753f27311c100fbf0a1
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:22 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/battle_result/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/battle_result/__init__.py
@@ -1,3 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
+# This is a Python script for a Battle Royale game, specifically for the part of the
+# code that handles the display of battle results in the hangar screen.
+# The script does not contain any functional code, but rather imports necessary
+# modules and classes for the implementation of the battle result view in the hangar.
+
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/battle_result/__init__.py
-pass
+# This line imports any necessary modules or packages that will be used in the
+# implementation of the battle result view.
+from battle_royale.scripts.client.battle_royale.gui.Scaleform.daapi.view.lobby.hangar.battle_result.battle_result_constants import *
+
+# This line imports a module or class named 'battle_result_constants' which
+# likely contains constants and enumerations used in the implementation of
+# the battle result view.

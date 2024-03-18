c75839c7200150363e715ccb35d8a66ca7537c00
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:31 +0300

    improve

--- source/res/battle_royale/scripts/client/InBattleUpgradeReadiness.py
+++ source/res/battle_royale/scripts/client/InBattleUpgradeReadiness.py
@@ -1,16 +1,3 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
+# Python bytecode 3.7 (decompiled from Python 3.7)
 # Embedded file name: battle_royale/scripts/client/InBattleUpgradeReadiness.py
-
-import BigWorld
-
-# This is a dynamic script component for handling in-battle upgrade readiness
-class InBattleUpgradeReadiness(BigWorld.DynamicScriptComponent):
-
-    # Called when the component enters the world
-    def onEnterWorld(self, *args):
-        pass
-
-    # Called when the component leaves the world
-    def onLeaveWorld(self, *args):
-        pass
 

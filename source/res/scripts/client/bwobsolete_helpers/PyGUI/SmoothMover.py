fdbf9c42f1597a300e47e109babca3dc4e2d6377
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:10 +0300

    comment

--- source/res/scripts/client/bwobsolete_helpers/PyGUI/SmoothMover.py
+++ source/res/scripts/client/bwobsolete_helpers/PyGUI/SmoothMover.py
@@ -1,13 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/bwobsolete_helpers/PyGUI/SmoothMover.py
-import BigWorld, GUI, Math, Keys
-from PyGUIBase import PyGUIBase
 
-class SmoothMover(PyGUIBase):
-    factoryString = 'PyGUI.SmoothMover'
+import BigWorld, GUI, Math, Keys
+from PyGUIBase import PyGUIBase  # Importing the PyGUIBase class from PyGUIBase module
+
+class SmoothMover(PyGUIBase):  # Defining a new class SmoothMover that inherits from PyGUIBase
+    factoryString = 'PyGUI.SmoothMover'  # A string that identifies the factory that creates this object
 
     def __init__(self, component):
-        PyGUIBase.__init__(self, component)
+        PyGUIBase.__init__(self, component)  # Calling the constructor of the parent class
+
+        # Initializing class variables
         self.minScroll = [0, 0]
         self.maxScroll = [0, 0]
         self.scroll = [0, 0]
@@ -16,25 +19,25 @@ class SmoothMover(PyGUIBase):
         self.scrollTransform.setIdentity()
 
     def scrollTo(self, x, y, animate=True):
+        # Setting the scroll position to the new position if it's within the allowed range
         self.scroll[0] = max(x, self.minScroll[0])
         self.scroll[0] = min(self.scroll[0], self.maxScroll[0])
         self.scroll[1] = max(y, self.minScroll[1])
         self.scroll[1] = min(self.scroll[1], self.maxScroll[1])
+
+        # Creating a new scroll transform with the new scroll position
         self.scrollTransform.setTranslate((self.scroll[0], self.scroll[1], 0))
+
+        # Setting the component's transform target to the new scroll transform
         self.component.transform.target = self.scrollTransform
+
+        # Setting the component's transform eta to the scroll speed if animate is True, otherwise setting it to 0
         self.component.transform.eta = self.scrollSpeed if animate else 0.0
 
     def scrollBy(self, x, y):
+        # Calling scrollTo with the current scroll position plus the given offsets
         self.scrollTo(self.scroll[0] + x, self.scroll[1] + y)
 
     def canScrollUp(self):
-        return self.scroll[1] > self.minScroll[1] + 0.0001
+        # Checking if the current scroll position is greater than the minimum scroll position by a small threshold
 
-    def canScrollDown(self):
-        return self.scroll[1] < self.maxScroll[1] - 0.0001
-
-    def canScrollLeft(self):
-        return self.scroll[0] > self.minScroll[0] + 0.0001
-
-    def canScrollRight(self):
-        return self.scroll[0] < self.maxScroll[0] - 0.0001

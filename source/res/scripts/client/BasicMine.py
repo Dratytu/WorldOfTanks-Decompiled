bd73683299718f73eb40eed27c92113e3617ed66
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:57 +0300

    comment

--- source/res/scripts/client/BasicMine.py
+++ source/res/scripts/client/BasicMine.py
@@ -1,34 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/BasicMine.py
-from battleground.mines_object import loadMines
-from entity_game_object import EntityGameObject
 
-class BasicMine(EntityGameObject):
+import from battleground.mines_object import loadMines
+import from entity_game_object import EntityGameObject
 
-    def set_isDetonated(self, prev=None):
-        if self.isDetonated:
-            if self.gameObject is not None:
-                self.gameObject.detonate()
-        return
-
-    def _loadGameObject(self):
-        return loadMines(self.ownerVehicleID, self._registerGameObject)
-
-    def _registerGameObject(self, gameObject):
-        self.gameObject.setPosition(self.position)
-        self.gameObject.setIsEnemyMarkerEnabled(self.isMarkerEnabled)
-        self.gameObject.setIsActivated(self.isActivated)
-        self.gameObject.setActivationTimeDelay(self.activationTimeDelay)
-        self.gameObject.setMineNumber(self.mineNumber)
-        super(BasicMine, self)._registerGameObject(gameObject)
-
-    def set_isActivated(self, prev=None):
-        if self.isActivated:
-            if self.gameObject is not None:
-                self.gameObject.activateMine()
-        return
-
-    def set_isMarkerEnabled(self, prev=None):
-        if self.gameObject is not None:
-            self.gameObject.enableEnemyIdleEffect(self.isMarkerEnabled)
-        return

0b4140eb138817b09f64a6cb9d274398eef0faf7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:39 +0300

    comment

--- source/res/frontline/scripts/client/frontline/gui/Scaleform/__init__.py
+++ source/res/frontline/scripts/client/frontline/gui/Scaleform/__init__.py
@@ -1,3 +1,11 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: frontline/scripts/client/frontline/gui/Scaleform/__init__.py
-pass
+# frontline/scripts/client/frontline/gui/Scaleform/__init__.py
+
+# This is an empty __init__.py file for the frontline.gui.Scaleform package.
+# Its purpose is to signal that this directory should be treated as a package
+# in Python, and to allow for any necessary package-level initialization.
+#
+# In this particular case, there are no additional modules or sub-packages
+# within frontline.gui.Scaleform, so this file is empty. However, if there
+# were additional modules or sub-packages, they could be imported here
+# to make them easily accessible to users of the frontline.gui.Scaleform
+# package.

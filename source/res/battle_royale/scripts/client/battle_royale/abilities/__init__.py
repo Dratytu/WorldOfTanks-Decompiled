d17b9c5111756846e531d1115362e18e3f88b7a1
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:33 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/abilities/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/abilities/__init__.py
@@ -1,3 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/abilities/__init__.py
-pass
+
+# This is an empty __init__.py file for the abilities package in the
+# battle_royale/scripts/client/battle_royale/abilities directory.
+# An empty __init__.py file is necessary for Python to recognize a directory
+# as a package, allowing you to import modules from this package using
+# the 'from battle_royale.abilities import [module_name]' syntax.
+
+# Since this file does not contain any code, there is no functionality to
+# document. However, if any modules are added to this package, they should
+# include detailed comments describing their purpose, functionality, and
+# usage.
+
+# Example usage:
+# from battle_royale.abilities import some_module
+# some_module.some_function()
+

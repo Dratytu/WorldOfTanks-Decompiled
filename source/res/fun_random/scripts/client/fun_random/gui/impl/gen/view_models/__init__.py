b0d6f71eca1577dd92c82680d7ef8afb7cd26e43
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:00 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/impl/gen/view_models/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/impl/gen/view_models/__init__.py
@@ -1,3 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/impl/gen/view_models/__init__.py
-pass
+# This is an empty __init__.py file for the fun\_random.gui.impl.gen.view\_models package.
+# It is used to ensure that the package is recognized as a package by the Python interpreter.
+# No code should be added to this file.
+

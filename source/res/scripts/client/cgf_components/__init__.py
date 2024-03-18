bb803fbca24b6de6808079ad377aebe1eac00a37
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:33 +0300

    comment

--- source/res/scripts/client/cgf_components/__init__.py
+++ source/res/scripts/client/cgf_components/__init__.py
@@ -1,3 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/cgf_components/__init__.py
-pass
+
+# This is the initialization file for CGF components.
+# It does not contain any directly executable code, but rather sets up the necessary modules and namespaces for the CGF components.
+
+# cgf_components package
+# This package contains various components for use in the CGF (Client Game Facade) framework.
+
+from scripts.client.cgf_components import *
+
+# The above line imports all modules from the cgf_components package into the current namespace.
+# This allows for easy access to all CGF components without having to specify the package name.
+

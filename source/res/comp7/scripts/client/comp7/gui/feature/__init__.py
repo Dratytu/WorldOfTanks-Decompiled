2bc006674ba396d7f59ff8ef68cf536baf4acef3
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:53 +0300

    comment

--- source/res/comp7/scripts/client/comp7/gui/feature/__init__.py
+++ source/res/comp7/scripts/client/comp7/gui/feature/__init__.py
@@ -1,3 +1,19 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: comp7/scripts/client/comp7/gui/feature/__init__.py
-pass
+# comp7/scripts/client/comp7/gui/feature/__init__.py
+#
+# This is an empty __init__.py file for the 'feature' package within the 'comp7/gui' module.
+# Its purpose is to mark the directory as a Python package, allowing it to be imported as a
+# namespace in other Python scripts. Since this file does not contain any code, there is
+# nothing to comment on directly within this script.
+#
+# However, it is important to note that the 'feature' package is likely to contain various
+# features related to the graphical user interface (GUI) of the 'comp7' application.
+# These features might include new widgets, dialogs, or other visual elements that enhance
+# the user experience.
+#
+# By keeping these features organized within a separate package, the code becomes more
+# modular, easier to maintain, and simpler to understand for other developers working on
+# the project.
+#
+# For more information on Python packages and __init__.py files, please refer to the official
+# Python documentation:
+# https://docs.python.org/3/tutorial/modules.html#packages

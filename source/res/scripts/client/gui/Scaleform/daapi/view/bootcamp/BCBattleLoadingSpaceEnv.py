c5685f0771b1dfef81be0bf1c6cb1ad913afa67b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:41 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/bootcamp/BCBattleLoadingSpaceEnv.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/bootcamp/BCBattleLoadingSpaceEnv.py
@@ -1,10 +1,8 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/bootcamp/BCBattleLoadingSpaceEnv.py
+
+# Importing the required classes and constants for sound management
 from gui.sounds.ambients import SoundEnv
 from gui.sounds.ambients import NoMusic
 from gui.sounds.ambients import NoAmbient
 
-class BCBattleLoadingSpaceEnv(SoundEnv):
-
-    def __init__(self, soundsCtrl):
-        super(BCBattleLoadingSpaceEnv, self).__init__(soundsCtrl, 'battleLoading', music=NoMusic(), ambient=NoAmbient())

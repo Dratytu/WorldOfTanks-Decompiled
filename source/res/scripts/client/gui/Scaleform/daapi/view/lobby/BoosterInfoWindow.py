bce215ffb4d7a9e339898c85f09a59272f0361cd
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:21 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/BoosterInfoWindow.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/BoosterInfoWindow.py
@@ -2,10 +2,42 @@
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/BoosterInfoWindow.py
 
 # Import necessary modules and classes
-from gui.Scaleform import MENU
-from gui.Scaleform.daapi.view.meta.BoosterInfoMeta import BoosterInfoMeta
-from gui.Scaleform.framework.entities.View import View
-from helpers import dependency
-from helpers.i18n import makeString as _ms
-from skeletons.gui.goodies import IGoodiesCache
+from gui.Scaleform import MENU # Importing MENU constant from gui.Scaleform package
+from gui.Scaleform.daapi.view.meta.BoosterInfoMeta import BoosterInfoMeta # Inheriting from BoosterInfoMeta class
+from gui.Scaleform.framework.entities.View import View # Inheriting from View class
+from helpers import dependency # Using dependency injection helper
+from helpers.i18n import makeString as _ms # Importing makeString function for internationalization
+from skeletons.gui.goodies import IGoodiesCache # Dependency for IGoodiesCache interface
 
+# Class definition for BoosterInfoWindow
+class BoosterInfoWindow(View, BoosterInfoMeta, dependency.Dependency):
+
+    # Inject IGoodiesCache implementation
+    _goodiesCache = dependency.descriptor(IGoodiesCache)
+
+    # Initializer for BoosterInfoWindow
+    def __init__(self):
+        super(BoosterInfoWindow, self).__init__()
+
+    # Override show method
+    def show(self, boosterCD):
+        """
+        :param boosterCD: Booster CD key
+        :type boosterCD: str
+        """
+        # Set booster data using the provided CD key
+        self.setBoosterData(boosterCD)
+        # Display the booster info window
+        super(BoosterInfoWindow, self).show()
+
+    # Method to set booster data
+    def setBoosterData(self, boosterCD):
+        """
+        Set booster data using the provided CD key.
+        :param boosterCD: Booster CD key
+        :type boosterCD: str
+        """
+        # Get booster data from the cache using the CD key
+        boosterData = self._goodiesCache.getBoosterByCDKey(boosterCD)
+        # Set booster data in the view
+        self.

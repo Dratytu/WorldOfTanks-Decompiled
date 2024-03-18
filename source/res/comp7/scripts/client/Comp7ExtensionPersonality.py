8ab77b19743d1b8886cbca0f73e5b1dd14e2df6c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:36 +0300

    comment

--- source/res/comp7/scripts/client/Comp7ExtensionPersonality.py
+++ source/res/comp7/scripts/client/Comp7ExtensionPersonality.py
@@ -1,26 +1,39 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: comp7/scripts/client/Comp7ExtensionPersonality.py
 
-# Importing the 'registerComp7Scaleform' function from the 'comp7.gui.Scaleform' module
+# This script contains the definition of a Comp7ExtensionPersonality class.
+# The class has 5 methods: preInit, init, start, fini, and register.
+
+# Importing the 'registerComp7Scaleform' function from the 'comp7.gui.Scaleform' module.
 from comp7.gui.Scaleform import registerComp7Scaleform
 
-# The 'preInit' function is defined as follows:
+# The 'preInit' method is defined as follows:
 def preInit():
-    # The 'registerComp7Scaleform' function is called to register the Comp7Scaleform
+    # The 'registerComp7Scaleform' function is called to register the Comp7Scaleform.
+    # This function is typically used to register Scaleform classes or components.
     registerComp7Scaleform()
 
-# The 'init' function is defined as follows:
+# The 'init' method is defined as follows:
 def init():
-    # The function does not contain any code, so it effectively does nothing
+    # The function does not contain any code, so it effectively does nothing.
+    # This method is usually used to initialize class variables or perform other
+    # setup tasks that are required before the class can be used.
     pass
 
-# The 'start' function is defined as follows:
+# The 'start' method is defined as follows:
 def start():
-    # The function does not contain any code, so it effectively does nothing
+    # The function does not contain any code, so it effectively does nothing.
+    # This method is usually used to start or trigger the main functionality of the
+    # class.
     pass
 
-# The 'fini' function is defined as follows:
+# The 'fini' method is defined as follows:
 def fini():
-    # The function does not contain any code, so it effectively does nothing
+    # The function does not contain any code, so it effectively does nothing.
+    # This method is usually used to clean up resources or perform other tasks that
+    # are required before the class is destroyed.
     pass
+
+# It is unclear what the 'register' function is supposed to do, as it is not
+# defined in this script.
 

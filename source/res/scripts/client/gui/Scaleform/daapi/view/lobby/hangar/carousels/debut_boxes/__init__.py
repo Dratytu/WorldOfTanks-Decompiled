54f6867139809bca48297a4465e44dc56008f44b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:29 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/debut_boxes/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/debut_boxes/__init__.py
@@ -1,3 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/hangar/carousels/debut_boxes/__init__.py
-pass
+
+# This script defines the initialization of debut boxes in the hangar view.
+# Debut boxes are special types of pop-up windows that appear in the game lobby
+# to showcase new features or promotions.
+#
+# The script does not contain any concrete implementation, but rather
+# provides a framework for creating and managing debut boxes.
+#
+# The script imports necessary modules and classes to enable the creation
+# of debut boxes, but does not contain any functional code.
+#
+# Author: [Your Name]
+# Date: [Date]
+
+pass 
+

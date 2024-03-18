74a1bcea7ded4a54ab33a9d3be51f957b903d50d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:17 +0300

    comment

--- source/res/scripts/client/ArtilleryEquipment.py
+++ source/res/scripts/client/ArtilleryEquipment.py
@@ -1,13 +1,25 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/ArtilleryEquipment.py
+
 import math_utils
 import BigWorld
 
+# Define the ArtilleryEquipment class that inherits from BigWorld.UserDataObject
 class ArtilleryEquipment(BigWorld.UserDataObject):
-    launchVelocity = property(lambda self: self.__launchVelocity)
+    # Define a read-only property for launchVelocity
+    @property
+    def launchVelocity(self):
+        return self.__launchVelocity
 
     def __init__(self):
+        # Call the constructor of BigWorld.UserDataObject
         BigWorld.UserDataObject.__init__(self)
-        launchDir = math_utils.createRotationMatrix((self.__dict__['yaw'], self.__dict__['pitch'], 0)).applyToAxis(2)
-        launchDir.normalise()
-        self.__launchVelocity = launchDir * self.speed
+
+        # Extract yaw and pitch from the instance's __dict__
+        launch_dir = math_utils.createRotationMatrix((self.__dict__['yaw'], self.__dict__['pitch'], 0)).applyToAxis(2)
+
+        # Normalize the launch direction
+        launch_dir.normalise()
+
+        # Calculate and store the launch velocity
+        self.__launchVelocity = launch_dir * self.speed

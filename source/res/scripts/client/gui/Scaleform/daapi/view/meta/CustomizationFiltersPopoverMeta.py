6731bfdb7352d598dfbd2f6dc57803ab21ba4b87
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:04 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/CustomizationFiltersPopoverMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/CustomizationFiltersPopoverMeta.py
@@ -1,38 +1,39 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/CustomizationFiltersPopoverMeta.py
-from gui.Scaleform.daapi.view.lobby.popover.SmartPopOverView import SmartPopOverView
+
+import flashcall
+from gui.Scaleform.daapi.view.lobby.popover.SmartPopOverView import SmartPopOverView # Importing SmartPopOverView class from the lobby.popover package
 
 class CustomizationFiltersPopoverMeta(SmartPopOverView):
 
+    # Defining a method to change the group of an item
     def changeGroup(self, itemId):
-        self._printOverrideError('changeGroup')
+        self._printOverrideError('changeGroup') # Printing an error message if this method is overridden
 
+    # Defining a method to set the default filter
     def setDefaultFilter(self):
-        self._printOverrideError('setDefaultFilter')
+        self._printOverrideError('setDefaultFilter') # Printing an error message if this method is overridden
 
+    # Defining a method to set whether only historic items are shown
     def setShowOnlyHistoric(self, value):
-        self._printOverrideError('setShowOnlyHistoric')
+        self._printOverrideError('setShowOnlyHistoric') # Printing an error message if this method is overridden
 
+    # Defining a method to set whether only acquired items are shown
     def setShowOnlyAcquired(self, value):
-        self._printOverrideError('setShowOnlyAcquired')
+        self._printOverrideError('setShowOnlyAcquired') # Printing an error message if this method is overridden
 
+    # Defining a method to set whether items hidden on another vehicle are shown
     def setHideOnAnotherVeh(self, value):
-        self._printOverrideError('setHideOnAnotherVeh')
+        self._printOverrideError('setHideOnAnotherVeh') # Printing an error message if this method is overridden
 
+    # Defining a method to set whether only progression decals are shown
     def setShowOnlyProgressionDecals(self, value):
-        self._printOverrideError('setShowOnlyProgressionDecals')
+        self._printOverrideError('setShowOnlyProgressionDecals') # Printing an error message if this method is overridden
 
+    # Defining a method to set whether only editable styles are shown
     def setShowOnlyEditableStyles(self, value):
-        self._printOverrideError('setShowOnlyEditableStyles')
+        self._printOverrideError('setShowOnlyEditableStyles') # Printing an error message if this method is overridden
 
+    # Defining a method to handle filter changes
     def onFilterChange(self, index, value):
-        self._printOverrideError('onFilterChange')
-
-    def onFormChange(self, index, value):
-        self._printOverrideError('onFormChange')
-
-    def as_setDataS(self, data):
-        return self.flashObject.as_setData(data) if self._isDAAPIInited() else None
-
-    def as_enableDefBtnS(self, value):
-        return self.flashObject.as_enableDefBtn(value) if self._isDAAPIInited() else None
+        self._printOverrideError('onFilterChange') # Printing an error message if this method is over

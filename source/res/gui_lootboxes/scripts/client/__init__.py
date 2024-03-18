5760897e7d7ecf1cc6f4a0bd49c0fe61e08b5b37
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:09 +0300

    comment

--- source/res/gui_lootboxes/scripts/client/__init__.py
+++ source/res/gui_lootboxes/scripts/client/__init__.py
@@ -1,3 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: gui_lootboxes/scripts/client/__init__.py
-pass
+
+# This is an empty __init__.py file for the gui_lootboxes.scripts.client package.
+# In Python, __init__.py files are used to mark a directory as a package, and
+# can contain initialization code for the package. However, in this case, the
+# file is empty, indicating that there is no initialization code for this
+# package.
+
+# The gui_lootboxes.scripts.client package likely contains scripts related
+# to the client-side of a graphical user interface (GUI) for a loot box feature
+# in a software application.
+
+# For more information on __init__.py files and Python packages, see:
+# https://docs.python.org/3/tutorial/modules.html#packages
+

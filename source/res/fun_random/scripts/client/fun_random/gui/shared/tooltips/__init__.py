7f439aa0d2b22402d76e605bdcdfac4a997b9916
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:07 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/shared/tooltips/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/shared/tooltips/__init__.py
@@ -1,6 +1,14 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: fun_random/scripts/client/fun_random/gui/shared/tooltips/__init__.py
-from shared_utils import CONST_CONTAINER
+# This module defines a container class for tooltip types used in the fun_random module.
+# The tooltip types are used to differentiate between different kinds of tooltips in the
+# user interface.
+
+from shared_utils import CONST_CONTAINER  # Importing the CONST_CONTAINER class from shared_utils module.
 
 class TooltipType(CONST_CONTAINER):
+    # TooltipType is a subclass of CONST_CONTAINER, which is used to define a set of named
+    # constants for tooltip types. This allows for easier and more readable code when
+    # referencing tooltip types throughout the module.
+
     FUN_RANDOM = 'funRandom'
+    # The 'FUN_RANDOM' constant is defined as a string with the value 'funRandom'. This
+    # constant represents the tooltip type for fun_random related tooltips.

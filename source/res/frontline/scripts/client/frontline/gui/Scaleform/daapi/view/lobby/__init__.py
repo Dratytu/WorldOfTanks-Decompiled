60b0b4701c5dda99fab7afa8d12d7ac29c9fc3d3
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:52 +0300

    comment

--- source/res/frontline/scripts/client/frontline/gui/Scaleform/daapi/view/lobby/__init__.py
+++ source/res/frontline/scripts/client/frontline/gui/Scaleform/daapi/view/lobby/__init__.py
@@ -1,14 +1,23 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: frontline/scripts/client/frontline/gui/Scaleform/daapi/view/lobby/__init__.py
 
+# This script is for the frontline lobby in the client. It contains various
+# functions that are used to handle different aspects of the lobby.
 
+# The getContextMenuHandlers() function is used to define handlers for context
+# menus in the frontline lobby. However, the function is currently empty and
+# does not contain any code.
 def getContextMenuHandlers():
     pass
 
 
+# The getViewSettings() function is used to define view settings for the
+# frontline lobby. Similar to the previous function, it is currently empty.
 def getViewSettings():
     pass
 
 
+# The getBusinessHandlers() function is used to define business handlers for
+# the frontline lobby. Again, the function is currently empty.
 def getBusinessHandlers():
     pass
+

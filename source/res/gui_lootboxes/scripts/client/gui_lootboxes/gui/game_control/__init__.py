fccc9a594248a92b98925a26e496a3dd3b4e00a0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:54 +0300

    comment

--- source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/game_control/__init__.py
+++ source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/game_control/__init__.py
@@ -1,8 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: gui_lootboxes/scripts/client/gui_lootboxes/gui/game_control/__init__.py
-from gui.shared.system_factory import registerGameControllers
-from gui_lootboxes.gui.game_control.gui_lootboxes_controller import GuiLootBoxesController
-from skeletons.gui.game_control import IGuiLootBoxesController
 
+# Import necessary modules and functions
+from gui.shared.system_factory import registerGameControllers  # Function to register game controllers
+from gui_lootboxes.gui.game_control.gui_lootboxes_controller import GuiLootBoxesController  # The game controller for gui_lootboxes
+from skeletons.gui.game_control import IGuiLootBoxesController  # Interface for the gui_lootboxes game controller
+
+# Function to register gui_lootboxes game controllers
 def registerGuiLootBoxesGameControllers():
+    # Register the game controller with the system factory using the IGuiLootBoxesController interface,
+    # GuiLootBoxesController implementation, and set to auto-create the controller
     registerGameControllers([(IGuiLootBoxesController, GuiLootBoxesController, True)])
+

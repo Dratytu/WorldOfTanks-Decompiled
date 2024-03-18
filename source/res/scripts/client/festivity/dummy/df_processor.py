78b11fa4efd8c35300e09779ac349f1beb1ec2e2
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:10 +0300

    comment

--- source/res/scripts/client/festivity/dummy/df_processor.py
+++ source/res/scripts/client/festivity/dummy/df_processor.py
@@ -1,6 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/festivity/dummy/df_processor.py
+
+# Importing the required base class for festivity processors
 from festivity.base import BaseFestivityProcessor
 
+# Defining the DummyCommandsProcessor class which inherits from BaseFestivityProcessor
 class DummyCommandsProcessor(BaseFestivityProcessor):
+    # Passing (i.e., not modifying) to proceed with the default implementation
     pass
+

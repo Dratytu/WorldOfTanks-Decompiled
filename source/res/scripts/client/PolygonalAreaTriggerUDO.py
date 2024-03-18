c82f6d69968681d1d1ec7665a053ee934335acbf
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:17 +0300

    comment

--- source/res/scripts/client/PolygonalAreaTriggerUDO.py
+++ source/res/scripts/client/PolygonalAreaTriggerUDO.py
@@ -1,8 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/PolygonalAreaTriggerUDO.py
+
 import BigWorld
 
+# Define a class named PolygonalAreaTriggerUDO that inherits from BigWorld.UserDataObject
 class PolygonalAreaTriggerUDO(BigWorld.UserDataObject):
 
+    # Constructor method for the PolygonalAreaTriggerUDO class
     def __init__(self):
+        # Call the constructor of the parent class BigWorld.UserDataObject
         BigWorld.UserDataObject.__init__(self)
+

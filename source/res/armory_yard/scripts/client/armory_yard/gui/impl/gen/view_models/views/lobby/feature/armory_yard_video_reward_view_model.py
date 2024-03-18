b1ed6c40fdb0111fc468ca125999b712f9d42195
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:17 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_video_reward_view_model.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_video_reward_view_model.py
@@ -1,30 +1,13 @@
 # Python bytecode 3.x (to be compiled in Python 3.x)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_video_reward_view_model.py
-from gui.impl.gen.view_models.common.vehicle_info_model import VehicleInfoModel
 
 class ArmoryYardVideoRewardViewModel(VehicleInfoModel):
-    __slots__ = ('on_close', 'on_error', 'on_show_vehicle', 'on_video_started', 'is_window_accessible', 'video_name')
-
-    def __init__(self, properties=10, commands=4):
-        super().__init__(properties=properties, commands=commands)
-
-    def get_is_window_accessible(self):
-        return self._get_bool(self.is_window_accessible.get())
-
-    def set_is_window_accessible(self, value):
-        self.is_window_accessible.set(value)
-
-    def get_video_name(self):
-        return self._get_string(self.video_name.get())
-
-    def set_video_name(self, value):
-        self.video_name.set(value)
-
-    def _initialize(self):
-        super()._initialize()
-        self.is_window_accessible = self._add_bool_property('isWindowAccessible', True)
-        self.video_name = self._add_string_property('videoName', '')
-        self.on_close = self._add_command('onClose')
-        self.on_error = self._add_command('onError')
-        self.on_show_vehicle = self._add_command('onShowVehicle')
-        self.on_video_started = self._add_command('onVideoStarted')
+    """View model for the armory yard video reward window."""
+    __slots__ = (
+        'on_close',  # Command called when the window is closed.
+        'on_error',  # Command called when an error occurs during video playback.
+        'on_show_vehicle',  # Command called when the vehicle is displayed after the video finishes playing.
+        'on_video_started',  # Command called when the video starts playing.
+        'is_window_accessible',  # Boolean property that determines whether the window is accessible or not.
+        'video_name'  # String property that stores the name of the video to be played.
+    )

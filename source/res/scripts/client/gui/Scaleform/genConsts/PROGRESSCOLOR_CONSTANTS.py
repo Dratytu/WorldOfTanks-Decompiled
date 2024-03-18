3cda1cbd5f70fb2bd3e1c92ad091784e8be5e940
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:01 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/genConsts/PROGRESSCOLOR_CONSTANTS.py
+++ source/res/scripts/client/gui/Scaleform/genConsts/PROGRESSCOLOR_CONSTANTS.py
@@ -1,8 +1,18 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/gui/Scaleform/genConsts/PROGRESSCOLOR_CONSTANTS.py
+# This module, PROGRESSCOLOR_CONSTANTS, defines a class with constant strings
+# representing different progress bar colors.
 
-
+# The class is named PROGRESSCOLOR_CONSTANTS and is in PascalCase, which is a
+# common naming convention for classes in Python.
 class PROGRESSCOLOR_CONSTANTS(object):
+
+    # Each constant is defined as a string with all lowercase letters and words
+    # separated by underscores. This is a common naming convention for constants
+    # in Python.
     ORANGE = 'orange'
     GREEN = 'green'
     GRAY = 'gray'
+
+    # By defining these constants as class variables, they can be accessed using
+    # the class name, like PROGRESSCOLOR_CONSTANTS.ORANGE, rather than creating
+    # an instance of the class. This is a common practice in Python for defining
+    # constants.

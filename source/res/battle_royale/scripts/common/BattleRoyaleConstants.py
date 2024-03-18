53d39c35d17b01ce9a655617cd44f6f8761ca349
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:40 +0300

    comment

--- source/res/battle_royale/scripts/common/BattleRoyaleConstants.py
+++ source/res/battle_royale/scripts/common/BattleRoyaleConstants.py
@@ -1,6 +1,14 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale/scripts/common/BattleRoyaleConstants.py
+# BattleRoyaleConstants.py
+# This module contains constant values used in the Battle Royale feature.
+
+# Constant for the test drive command in Battle Royale.
 CMD_BATTLE_ROYALE_TEST_DRIVE = 21001
+
+# Constant for the rent command in Battle Royale.
 CMD_BATTLE_ROYALE_RENT = 21002
+
+# Constant for operating with BRCOIN in Battle Royale.
 CMD_BATTLE_ROYALE_OPERATE_BRCOIN = 21003
+
+# Constant for renting all available items in Battle Royale.
 CMD_BATTLE_ROYALE_RENT_ALL = 21004

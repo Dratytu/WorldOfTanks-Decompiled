08dbfb7399ad7ef375f3594e850f5c91b5720bfe
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:33 +0300

    comment

--- source/res/scripts/client/frameworks/state_machine/__init__.py
+++ source/res/scripts/client/frameworks/state_machine/__init__.py
@@ -1,15 +1,17 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/frameworks/state_machine/__init__.py
-from .events import StateEvent
-from .events import StringEvent
-from .machine import StateMachine
-from .states import State
-from .states import StateFlags
-from .observers import BaseStateObserver
-from .observers import SingleStateObserver
-from .observers import MultipleStateObserver
-from .observers import StateObserversContainer
-from .transitions import BaseTransition
-from .transitions import ConditionTransition
-from .transitions import StringEventTransition
-__all__ = ('StateEvent', 'StringEvent', 'StateMachine', 'State', 'StateFlags', 'BaseStateObserver', 'SingleStateObserver', 'MultipleStateObserver', 'StateObserversContainer', 'BaseTransition', 'ConditionTransition', 'StringEventTransition')
+
+from .events import StateEvent, StringEvent  # Import StateEvent and StringEvent from the events module
+from .machine import StateMachine  # Import StateMachine from the machine module
+from .states import State, StateFlags  # Import State and StateFlags from the states module
+from .observers import BaseStateObserver, SingleStateObserver, MultipleStateObserver, StateObserversContainer  # Import various observer classes from the observers module
+from .transitions import BaseTransition, ConditionTransition, StringEventTransition  # Import various transition classes from the transitions module
+
+# List of all publicly available classes and modules
+__all__ = (
+    'StateEvent', 'StringEvent', 'StateMachine', 'State', 'StateFlags',
+    'BaseStateObserver', 'SingleStateObserver', 'MultipleStateObserver',
+    'StateObserversContainer', 'BaseTransition', 'ConditionTransition',
+    'StringEventTransition'
+)
+

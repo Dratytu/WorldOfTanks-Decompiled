60253d8957634b8fe19154f7533f7dc02c0fd71e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:05 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/RankedBattlesRewardsLeaguesMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/RankedBattlesRewardsLeaguesMeta.py
@@ -11,8 +11,19 @@ class RankedBattlesRewardsLeaguesMeta(BaseDAAPIComponent):
     def onStyleClick(self, styleID):
         # Print an error message if onStyleClick is overridden
         self._printOverrideError('onStyleClick')
+        """
+        This method is called when a user clicks on a style in the ranked battles rewards leagues view.
+        The `styleID` parameter is the unique identifier for the style that was clicked.
+        If this method is overridden, an error message will be printed to the console.
+        """
 
     # as_setRewardsS method is defined to set rewards
     def as_setRewardsS(self, rewards):
         # Return the result of as_setRewards if DAAPI is initialized, otherwise return None
         return self.flashObject.as_setRewards(rewards) if self._isDAAPIInited() else None
+        """
+        This method is used to set the rewards for the ranked battles rewards leagues view.
+        The `rewards` parameter is a data structure containing information about the rewards.
+        The method returns the result of the `as_setRewards` method if DAAPI is initialized,
+        otherwise it returns None.
+        """

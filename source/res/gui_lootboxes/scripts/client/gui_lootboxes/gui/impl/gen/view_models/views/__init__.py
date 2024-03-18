be890b2b687f498688354ef4bf39e348efa9fe20
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:13 +0300

    comment

--- source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/gen/view_models/views/__init__.py
+++ source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/gen/view_models/views/__init__.py
@@ -1,3 +1,5 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/gen/view_models/views/__init__.py
-pass
+
+# This is an empty __init__.py file, which is used to mark this directory as a package
+# in Python. It does not contain any code or functionality itself.

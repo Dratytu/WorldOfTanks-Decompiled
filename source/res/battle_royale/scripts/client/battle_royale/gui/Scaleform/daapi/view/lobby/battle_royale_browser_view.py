0efdfc23666eb03be727b9253fa4bff4920a0477
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:21 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/battle_royale_browser_view.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/battle_royale_browser_view.py
@@ -1,24 +1,33 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/battle_royale_browser_view.py
-from gui.Scaleform.daapi.view.lobby.shared.web_view import WebView
-from helpers import dependency
-from skeletons.gui.game_control import IBattleRoyaleController
-from web.web_client_api.battle_royale import createBattleRoyaleWebHanlders
 
+# Import necessary modules and classes
+from gui.Scaleform.daapi.view.lobby.shared.web_view import WebView  # Base class for web-based views
+from helpers import dependency  # Helper module for dependency injection
+from skeletons.gui.game_control import IBattleRoyaleController  # Interface for the Battle Royale controller
+from web.web_client_api.battle_royale import createBattleRoyaleWebHanlders  # Module for creating web handlers for Battle Royale
+
+# Define the BattleRoyaleBrowserView class
 class BattleRoyaleBrowserView(WebView):
+
+    # Inject the IBattleRoyaleController dependency
     __battleRoyaleController = dependency.descriptor(IBattleRoyaleController)
 
+    # Define the webHandlers method
     def webHandlers(self):
+        # Create and return the Battle Royale web handlers
         return createBattleRoyaleWebHanlders()
 
+    # Override the _populate method
     def _populate(self):
+        # Call the superclass's _populate method
         super(BattleRoyaleBrowserView, self)._populate()
+
+        # Register the onUpdated callback for the IBattleRoyaleController
         self.__battleRoyaleController.onUpdated += self.__onSettingsChange
 
+    # Override the _dispose method
     def _dispose(self):
+        # Call the superclass's _dispose method
         super(BattleRoyaleBrowserView, self)._dispose()
-        self.__battleRoyaleController.onUpdated -= self.__onSettingsChange
 
-    def __onSettingsChange(self, diff):
-        if not self.__battleRoyaleController.isEnabled():
-            self.destroy()

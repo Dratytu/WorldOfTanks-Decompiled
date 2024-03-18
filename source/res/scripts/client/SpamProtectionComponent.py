fcb3c935632734d69de7315fead7b2cb3abe9d5b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:21 +0300

    comment

--- source/res/scripts/client/SpamProtectionComponent.py
+++ source/res/scripts/client/SpamProtectionComponent.py
@@ -1,8 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/SpamProtectionComponent.py
-import logging
-import BigWorld
-_logger = logging.getLogger(__name__)
+
+import logging # This line imports the logging module, which is used for logging events and messages in the application
+import BigWorld # This line imports the BigWorld module, which is a set of tools and services for creating online games
+
+_logger = logging.getLogger(__name__) # This line creates a logger instance using the logging module. The logger is named after the current module (__name__)
 
 class SpamProtectionComponent(BigWorld.DynamicScriptComponent):
-    pass
+    pass # This class, SpamProtectionComponent, inherits from BigWorld.DynamicScriptComponent. Currently, it doesn't contain any methods or properties.
+

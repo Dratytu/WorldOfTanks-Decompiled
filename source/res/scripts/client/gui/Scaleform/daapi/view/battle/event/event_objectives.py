e6d5529f728a096338cf821aba2232add53e4dbc
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:56 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/event/event_objectives.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/event/event_objectives.py
@@ -1,11 +1,19 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/event/event_objectives.py
-from gui.Scaleform.daapi.view.meta.EventObjectivesMeta import EventObjectivesMeta
 
+# Import the EventObjectivesMeta class from the gui.Scaleform.daapi.view.meta package
+from gui.Scaleform.daapi.view.meta import EventObjectivesMeta
+
+# Define the EventObjectivesPanel class that inherits from EventObjectivesMeta
 class EventObjectivesPanel(EventObjectivesMeta):
 
+    # Define the updateObjectives method that takes a single argument 'txt'
     def updateObjectives(self, txt):
+        # Call the as_updateObjectivesS method on 'self' and pass 'txt' as a parameter
         self.as_updateObjectivesS(txt)
 
+    # Define the hide method
     def hide(self):
+        # Call the as_hideS method on 'self'
         self.as_hideS()
+

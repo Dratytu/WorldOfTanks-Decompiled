aa4d472c2291c868dced8ceeaac3dd4a86f86cfa
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:08 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: armory_yard/scripts/client/armory_yard/__init__.py
+
+# This is the armory_yard module's initialization file, which serves as the entry point for the module.
+# There are no explicit functions or classes defined in this file.
+
+# The 'pass' statement is used as a placeholder and is equivalent to a null operation.
+# It is used when a statement is required syntactically, but no action is needed at runtime.
 pass
+

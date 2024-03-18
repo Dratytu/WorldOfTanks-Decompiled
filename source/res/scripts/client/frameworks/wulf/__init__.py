486743542bd0b0b02b76fb51f797a220eb108488
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:22 +0300

    comment

--- source/res/scripts/client/frameworks/wulf/__init__.py
+++ source/res/scripts/client/frameworks/wulf/__init__.py
@@ -1,41 +1,39 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/frameworks/wulf/__init__.py
-from .gui_application import GuiApplication
-from .gui_constants import PropertyType
-from .gui_constants import PositionAnchor
-from .gui_constants import ViewFlags
-from .gui_constants import ViewStatus
-from .gui_constants import ViewEventType
-from .gui_constants import WindowFlags
-from .gui_constants import WindowLayer
-from .gui_constants import WindowStatus
-from .gui_constants import NumberFormatType
-from .gui_constants import RealFormatType
-from .gui_constants import TimeFormatType
-from .gui_constants import DateFormatType
-from .gui_constants import CaseType
-from .py_object_wrappers import isTranslatedKeyValid
-from .py_object_wrappers import isTranslatedTextExisted
-from .py_object_wrappers import getTranslatedText
-from .py_object_wrappers import getTranslatedPluralText
-from .py_object_wrappers import getImagePath
-from .py_object_wrappers import getSoundEffectId
-from .py_object_wrappers import getLayoutPath
-from .py_object_wrappers import getTranslatedTextByResId
-from .py_object_wrappers import getTranslatedPluralTextByResId
-from .py_object_wrappers import getTranslatedKey
-from .py_object_wrappers import getNumberFormat
-from .py_object_wrappers import getRealFormat
-from .py_object_wrappers import getTimeFormat
-from .py_object_wrappers import getDateFormat
-from .py_object_wrappers import caseMap
-from .view.array import Array
-from .view.command import Command
-from .view.view import ViewSettings
-from .view.view import View
-from .view.view_event import ViewEvent
-from .windows_system.windows_area import WindowsArea
-from .windows_system.window import WindowSettings
-from .windows_system.window import Window
+
+# wulf framework initialization file
+# This script imports various modules and classes that define the core functionality
+# of the wulf framework, a graphical user interface (GUI) framework for a specific application.
+
+from .gui_application import GuiApplication  # The main application class for the GUI framework
+
+# Constants used throughout the framework to define various properties and flags
+from .gui_constants import (PropertyType, PositionAnchor, ViewFlags, ViewStatus,
+                            ViewEventType, WindowFlags, WindowLayer, WindowStatus,
+                            NumberFormatType, RealFormatType, TimeFormatType,
+                            DateFormatType, CaseType)
+
+# Functions for working with translated text, formatting, and case mapping
+from .py_object_wrappers import (isTranslatedKeyValid, isTranslatedTextExisted,
+                                  getTranslatedText, getTranslatedPluralText,
+                                  getImagePath, getSoundEffectId, getLayoutPath,
+                                  getTranslatedTextByResId,
+                                  getTranslatedPluralTextByResId, getTranslatedKey,
+                                  getNumberFormat, getRealFormat, getTimeFormat,
+                                  getDateFormat, caseMap)
+
+# Various classes defining different aspects of the GUI framework
+from .view.array import Array  # A class for working with arrays in views
+from .view.command import Command  # A class for defining commands in views
+from .view.view import ViewSettings  # Settings for configuring a view
+from .view.view import View  # A base class for creating views in the framework
+from .view.view_event import ViewEvent  # A class for handling events in views
+
+from .windows_system.windows_area import WindowsArea  # A class for managing windows areas
+from .windows_system.window import WindowSettings  # Settings for configuring a window
+from .windows_system.window import Window  # A base class for creating windows in the framework
+
+# A class for creating view models in the framework
 from .view.view_model import ViewModel
-__all__ = ('GuiApplication', 'PropertyType', 'PositionAnchor', 'ViewFlags', 'ViewStatus', 'ViewEventType', 'WindowFlags', 'WindowLayer', 'WindowStatus', 'NumberFormatType', 'RealFormatType', 'TimeFormatType', 'DateFormatType', 'CaseType', 'Array', 'Command', 'ViewSettings', 'View', 'ViewEvent', 'WindowsArea', 'WindowSettings', 'Window', 'ViewModel', 'isTranslatedKeyValid', 'isTranslatedTextExisted', 'getTranslatedText', 'getTranslatedPluralText', 'getImagePath', 'getSoundEffectId', 'getLayoutPath', 'getTranslatedTextByResId', 'getTranslatedPluralTextByResId', 'getTranslatedKey', 'getNumberFormat', 'getRealFormat', 'getTimeFormat', 'getDateFormat', 'caseMap')
+
+__all__ = (    'GuiApplication', 'PropertyType', 'PositionAnchor', 'ViewFlags', 'ViewStatus', 'ViewEventType', 'WindowFlags',

02b279b8ecf370b10487860734cb2d06aed082ce
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:26 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/__init__.py
+# This is an empty __init__.py file, which is used to indicate that this directory
+# should be treated as a package in Python. It is common practice to include an
+# empty __init__.py file in directories that contain Python modules or sub-packages.
+# In this case, the package is "armory_yard.gui.impl.gen.view_models", which likely
+# contains various view model classes for use in the Armory Yard application.
 pass
+

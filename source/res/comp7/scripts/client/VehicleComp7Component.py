90645e2a2beebe7a324ca2c288cc155dab466ae8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:30 +0300

    comment

--- source/res/comp7/scripts/client/VehicleComp7Component.py
+++ source/res/comp7/scripts/client/VehicleComp7Component.py
@@ -1,6 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: comp7/scripts/client/VehicleComp7Component.py
+
+# Import the DynamicScriptComponent class from the script_component package
 from script_component.DynamicScriptComponent import DynamicScriptComponent
 
+# Define the VehicleComp7Component class, which inherits from the DynamicScriptComponent class
 class VehicleComp7Component(DynamicScriptComponent):
+    # The VehicleComp7Component class doesn't contain any additional methods or attributes,
+    # so it inherits the behavior of the DynamicScriptComponent class.
     pass
+

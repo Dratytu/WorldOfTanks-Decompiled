2c44e0344dbfb667b6d2925fbb60f3295702f3c6
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:43 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/__init__.py
@@ -1,3 +1,21 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/__init__.py
-pass
+
+# This is an empty __init__.py file for the 'regular' entity module within
+# the 'battle_royale/gui/prb_control' package. This module will contain
+# classes and functions related to the creation and management of
+# regular Player vs. Environment (PvE) battles in the Battle Royale game mode.
+
+# Currently, this file does not contain any code and only serves to ensure
+# that the 'regular' entity package is recognized as a Python package.
+
+# As this module is developed further, it may include classes such as
+# 'BattleRoyaleRegularBattle', which would manage the creation and
+# execution of regular battles in the Battle Royale game mode.
+
+# Additionally, this module may include functions for handling player
+# invitations, team creation, and other aspects of the PvE battle system.
+
+# By keeping these classes and functions organized within the 'regular'
+# entity package, we can ensure that the code remains modular,
+# maintainable, and easy to understand for other developers.

eda688c91d1935b64c23bf11cb74478fc627fc38
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:44 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/shared/stats_exchange/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/shared/stats_exchange/__init__.py
@@ -1,15 +1,32 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/shared/stats_exchange/__init__.py
+
+# Import necessary modules
 import weakref
-from gui.Scaleform.daapi.view.battle.shared.stats_exchange import broker
-from gui.Scaleform.daapi.view.battle.shared.stats_exchange import player
+
+# Import custom modules from the same package
+from gui.Scaleform.daapi.view.battle.shared.stats_exchange import broker, player
 from gui.Scaleform.daapi.view.battle.shared.stats_exchange.stats_ctrl import BattleStatisticsDataController
+
+# Define the list of all exported symbols from this module
 __all__ = ('BattleStatisticsDataController', 'createExchangeBroker')
 
 def createExchangeBroker(exchangeCtx):
+    # Create a weak reference proxy for the exchange context
     proxy = weakref.proxy(exchangeCtx)
+    
+    # Create an ExchangeBroker instance and associate it with the exchange context
     exchangeBroker = broker.ExchangeBroker(exchangeCtx)
+    
+    # Set up the PlayerStatusExchange component for the broker
     exchangeBroker.setPlayerStatusExchange(player.PlayerStatusComponent())
+    
+    # Set up the UsersTagsExchangeData component for the broker, using the proxy for the exchange context
     exchangeBroker.setUsersTagsExchange(player.UsersTagsListExchangeData(proxy))
+    
+    # Set up the InvitationsExchangeBlock component for the broker
     exchangeBroker.setInvitationsExchange(player.InvitationsExchangeBlock())
+    
+    # Return the configured ExchangeBroker instance
     return exchangeBroker
+

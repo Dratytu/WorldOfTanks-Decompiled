95883d15ceb01f64217afc8b3af687b23831d035
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:31 +0300

    comment

--- source/res/battle_modifiers/scripts/common/__init__.py
+++ source/res/battle_modifiers/scripts/common/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_modifiers/scripts/common/__init__.py
+
+# This script is part of the battle_modifiers module and contains
+# the initialization code for the module.
+
+# The 'pass' statement is a placeholder and does not affect the
+# execution of the script. It is typically used as a placeholder
+# when the code for a particular block has not yet been written.
 pass

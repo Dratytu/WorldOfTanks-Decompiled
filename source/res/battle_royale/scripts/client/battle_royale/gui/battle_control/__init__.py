4f54245122084165477a5bc8a1ecfa48c5896ed4
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:36 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/battle_control/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/battle_control/__init__.py
@@ -1,3 +1,21 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/battle_control/__init__.py
-pass
+
+# This is an empty __init__.py file for the battle_royale.gui.battle_control module.
+# It serves the purpose of marking this directory as a package in Python, allowing
+# other modules to import its contents.
+
+# The battle_control module is likely responsible for handling various aspects
+# of the game's battle control system in the context of the battle royale mode.
+# However, without additional information or contents in this file, it is difficult
+# to provide a more specific description of its functionality.
+
+# If you are a developer working on this module, you may want to consider adding
+# more modules and scripts to this package as needed to implement the desired
+# functionality. Be sure to include clear and informative code comments for any
+# new modules or scripts you add.
+
+# For more information on Python packages and modules, you can refer to the official
+# Python documentation:
+# https://docs.python.org/3/tutorial/modules.html
+

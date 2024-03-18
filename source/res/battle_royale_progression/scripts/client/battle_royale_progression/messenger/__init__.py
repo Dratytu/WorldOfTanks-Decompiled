790325b537dbd6d5ac3a24def7142685e5afe64d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:31 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/battle_royale_progression/messenger/__init__.py
+++ source/res/battle_royale_progression/scripts/client/battle_royale_progression/messenger/__init__.py
@@ -1,3 +1,12 @@
+# This is the \_\_init__.py file for the battle\_royale\_progression.messenger module.
+# It serves as the entry point for this module and should contain any necessary
+# imports or initialization code.
+
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale_progression/scripts/client/battle_royale_progression/messenger/__init__.py
 pass
+
+# The 'pass' statement is a placeholder and does nothing. It's used here
+# because \_\_init__.py files in Python must contain at least one statement.
+# In a real implementation, this file would likely contain import statements
+# and/or other initialization code for the battle\_royale\_progression.messenger module.

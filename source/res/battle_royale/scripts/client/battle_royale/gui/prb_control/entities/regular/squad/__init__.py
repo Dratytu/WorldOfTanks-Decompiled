f6895c932e3d173b42257bd27fc1feff332195ca
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:24 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/squad/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/squad/__init__.py
@@ -1,3 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/squad/__init__.py
-pass
+
+# This is an empty __init__.py file for the 'battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/squad' package.
+# It serves as a marker file to indicate that this directory should be treated as a package in Python.
+
+# The 'regular' package contains classes and functions related to the creation and management of squads in the Battle Royale game mode.
+# A squad is a group of players who have joined together to play a match as a team.
+
+# The 'squad' package specifically contains classes and functions related to the Squad entity, which represents a single squad.
+# The Squad entity contains information about the squad, such as its name, leader, and members.
+
+# This file, __init__.py, is an empty file that is required for Python to recognize this directory as a package.
+# It should not contain any code or functionality itself, but is instead used to mark the directory as a package.

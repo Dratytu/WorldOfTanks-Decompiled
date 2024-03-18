d5fa838a2af33b64320914d66cd5c4420b0aa225
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:44 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/__init__.py
+++ source/res/battle_royale_progression/scripts/client/__init__.py
@@ -1,3 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale_progression/scripts/client/__init__.py
-pass
+
+# This is an empty __init__.py file for the battle_royale_progression.scripts.client package.
+# In Python, __init__.py files are used to initialize a package, making it possible for Python to recognize it as a package.
+# An empty __init__.py file like this one serves the purpose of initializing the client package within the battle_royale_progression.scripts module.
+# This file does not contain any code, as its sole purpose is to mark the directory as a package.

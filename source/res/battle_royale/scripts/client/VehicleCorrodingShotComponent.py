075b4e7e5236932b773cefd3793120941bd93f6c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:32 +0300

    improve

--- source/res/battle_royale/scripts/client/VehicleCorrodingShotComponent.py
+++ source/res/battle_royale/scripts/client/VehicleCorrodingShotComponent.py
@@ -1,10 +1,11 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale/scripts/client/VehicleCorrodingShotComponent.py
+# Python bytecode 3.7 (decompiled from Python 3.7)
+# Embedded file name: battle_royale/scripts/client/vehicle_corroding_shot_component.py
 from gui.battle_control.battle_constants import VEHICLE_VIEW_STATE
-from VehicleAbilityBaseComponent import VehicleAbilityBaseComponent
+from vehicle_ability_base_component import VehicleAbilityBaseComponent
 
 class VehicleCorrodingShotComponent(VehicleAbilityBaseComponent):
-    __TIMER_VIEW_ID = VEHICLE_VIEW_STATE.CORRODING_SHOT
+    TIMER_VIEW_ID = VEHICLE_VIEW_STATE.CORRODING_SHOT
 
     def __init__(self, *args):
-        super(VehicleCorrodingShotComponent, self).__init__(self.__TIMER_VIEW_ID)
+        super().__init__(self.TIMER_VIEW_ID)
+

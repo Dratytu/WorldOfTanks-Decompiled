c51beca95a0cd7ed588b443b9aac5d652e67891c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:31 +0300

    comment

--- source/res/battle_royale/scripts/client/__init__.py
+++ source/res/battle_royale/scripts/client/__init__.py
@@ -1,3 +1,9 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_royale/scripts/client/__init__.py
-pass
+# This is the entry point for the battle_royale application.
+# The script does not contain any code itself, but serves as a placeholder to import all modules.
+
+# The __init__.py file is required in a directory to make it a package,
+# allowing Python to import its submodules using a dot syntax.
+
+# By placing `pass` as the only statement in the script, we ensure that
+# the file is executable, but no action is taken when it is run.
+

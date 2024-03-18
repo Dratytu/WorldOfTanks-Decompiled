96bc1f805874a04a7b088508314fc293c9c453f1
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:24 +0300

    comment

--- source/res/scripts/client/benchmarklocation.py
+++ source/res/scripts/client/benchmarklocation.py
@@ -1,8 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/BenchmarkLocation.py
-import BigWorld
 
+import BigWorld # Importing BigWorld module to inherit from UserDataObject
+
+# Define a new class named BenchmarkLocation that inherits from BigWorld.UserDataObject
 class BenchmarkLocation(BigWorld.UserDataObject):
 
+    # Constructor method for the BenchmarkLocation class
     def __init__(self):
+        # Call the constructor method of the parent class (BigWorld.UserDataObject)
         BigWorld.UserDataObject.__init__(self)
+

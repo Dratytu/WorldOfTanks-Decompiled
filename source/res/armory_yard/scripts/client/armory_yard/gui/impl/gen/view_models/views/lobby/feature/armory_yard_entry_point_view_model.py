1c8d04d342266204a431382290f40ef2647dd674
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:20 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_entry_point_view_model.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_entry_point_view_model.py
@@ -1,43 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/armory_yard_entry_point_view_model.py
+
+# Importing the Enum class from the built-in enum module
 from enum import Enum
+
+# Importing the ViewModel class from the frameworks.wulf module
 from frameworks.wulf import ViewModel
 
-class State(Enum):
-    BEFOREPROGRESSION = 'beforeProgression'
-    ACTIVE = 'active'
-    POSTPROGRESSION = 'postProgression'
-    COMPLETED = 'completed'
-    DISABLED = 'disabled'
-
-
-class ArmoryYardEntryPointViewModel(ViewModel):
-    __slots__ = ('onAction',)
-
-    def __init__(self, properties=3, commands=1):
-        super(ArmoryYardEntryPointViewModel, self).__init__(properties=properties, commands=commands)
-
-    def getState(self):
-        return State(self._getString(0))
-
-    def setState(self, value):
-        self._setString(0, value.value)
-
-    def getProgressionLevel(self):
-        return self._getNumber(1)
-
-    def setProgressionLevel(self, value):
-        self._setNumber(1, value)
-
-    def getEndTimestamp(self):
-        return self._getNumber(2)
-
-    def setEndTimestamp(self, value):
-        self._setNumber(2, value)
-
-    def _initialize(self):
-        super(ArmoryYardEntryPointViewModel, self)._initialize()
-        self._addStringProperty('state')
-        self._addNumberProperty('progressionLevel', 0)
-        self._addNumberProperty('endTimestamp', 0)
-        self.onAction = self._addCommand('onAction')

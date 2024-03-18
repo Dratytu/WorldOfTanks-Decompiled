4f046ca53aa5f2dbb5debc31e2466ca5ca39ee25
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:40 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/storages/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/storages/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/prb_control/storages/__init__.py
-pass
+
+# This file serves as the entry point for the `storages` module in the
+# `battle_royale/gui/prb_control` package. Currently, this module does
+# not contain any code or functionality. Any classes or functions that
+# are added to this module in the future will be used to manage and
+# persist data related to the player-versus-player matchmaking process
+# in the Battle Royale game mode.

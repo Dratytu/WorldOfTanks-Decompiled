1ac03d81928b6cbc8f93cba4fb975cd793530ae7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:19 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/missions/regular/vehicle_selector.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/missions/regular/vehicle_selector.py
@@ -1,5 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/missions/regular/vehicle_selector.py
+
+# Import necessary modules and classes
 from account_helpers.AccountSettings import MISSION_SELECTOR_FILTER, AccountSettings
 from gui.Scaleform import getButtonsAssetPath
 from gui.Scaleform.daapi.view.lobby.missions.vehicle_selector import MissionVehicleSelector, MissionVehicleSelectorCarousel, SelectorCriteriesGroup
@@ -11,8 +13,10 @@ from gui.server_events import caches
 from gui.shared.formatters import text_styles
 from helpers.i18n import makeString as ms
 
+# Define RegularMissionVehicleSelector class, which inherits from MissionVehicleSelector
 class RegularMissionVehicleSelector(MissionVehicleSelector):
 
+    # Override _getTitle method to provide a custom title for the vehicle selector
     def _getTitle(self):
         suitableVehiclesCount = self._carousel.getSuitableVehiclesCount()
         totalVehiclesCount = self._carousel.getTotalVehiclesCount()
@@ -23,20 +27,24 @@ class RegularMissionVehicleSelector(MissionVehicleSelector):
         count = '{} / {}'.format(suitableStyle(suitableVehiclesCount), text_styles.main(totalVehiclesCount))
         return text_styles.highTitle(ms(QUESTS.MISSIONS_VEHICLESELECTOR_TITLE, count=count))
 
+    # Static class method _getCarouselAlias, which returns the alias for the carousel
     @classmethod
     def _getCarouselAlias(cls):
         return QUESTS_ALIASES.VEHICLE_SELECTOR_CAROUSEL_ALIAS
 
+    # Override _getNotAvailableStatusText method to provide a custom message when no vehicles are available
     def _getNotAvailableStatusText(self):
         return text_styles.error(QUESTS.MISSIONS_VEHICLESELECTOR_STATUS_NOTAVAILABLE)
 
-
+# Define RegularVehicleSelectorCarousel class, which inherits from MissionVehicleSelectorCarousel
 class RegularVehicleSelectorCarousel(MissionVehicleSelectorCarousel):
 
+    # Initialize the class with a custom carousel filter class
     def __init__(self):
         super(RegularVehicleSelectorCarousel, self).__init__()
         self._carouselFilterCls = _RegularCarouselFilter
 
+    # Override _setCarouselFilters method to set custom filters for the carousel
     def _setCarouselFilters(self):
         filters = self.filter.getFilters(self._usedFilters)
         self.as_initCarouselFilterS({'isVisible': True,
@@ -46,14 +54,16 @@ class RegularVehicleSelectorCarousel(MissionVehicleSelectorCarousel):
                          'tooltip': TANK_CAROUSEL_FILTER.TOOLTIP_INVENTORY,
                          'selected': filters['inventory']}]})
 
-
+# Define a custom carousel filter class, which inherits from CarouselFilter
 class _RegularCarouselFilter(CarouselFilter):
 
+    # Initialize the class with custom server sections and criteria groups
     def __init__(self):
         super(_RegularCarouselFilter, self).__init__()
         self._serverSections += (MISSION_SELECTOR_FILTER,)
         self._criteriesGroups = (EventCriteriesGroup(), SelectorCriteriesGroup())
 
+    # Override load method to load custom filters from the user's account settings
     def load(self):
         filters = AccountSettings.getFilterDefaults(self._serverSections)
         for section in self._clientSections:
@@ -63,6 +73,7 @@ class _RegularCarouselFilter(CarouselFilter):
         self._filters = filters
         self.update(filters, save=False)
 
+    # Override save method to save custom filters to the user's account settings
     def save(self):
         filters = AccountSettings.getFilterDefault(MISSION_SELECTOR_FILTER)
         filters = {key:value for key, value in self._filters.iteritems() if key in filters}

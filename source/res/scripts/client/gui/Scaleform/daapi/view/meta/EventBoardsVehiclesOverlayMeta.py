32c22b033a78f2e4bc3fad0ce730643795278932
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:18 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/EventBoardsVehiclesOverlayMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/EventBoardsVehiclesOverlayMeta.py
@@ -1,20 +1,26 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/EventBoardsVehiclesOverlayMeta.py
-from gui.Scaleform.framework.entities.BaseDAAPIComponent import BaseDAAPIComponent
+
+import gui.Scaleform.framework.entities.BaseDAAPIComponent # Importing BaseDAAPIComponent to inherit its properties and methods
 
 class EventBoardsVehiclesOverlayMeta(BaseDAAPIComponent):
 
+    # Defining a method to change the filter based on the given id
     def changeFilter(self, id):
-        self._printOverrideError('changeFilter')
+        self._printOverrideError('changeFilter') # Printing an error message if this method is overridden
 
+    # Defining a method to apply filters based on nation, vehicleType, level, isMain, and hangarOnly
     def applyFilters(self, nation, vehicleType, level, isMain, hangarOnly):
-        self._printOverrideError('applyFilters')
+        self._printOverrideError('applyFilters') # Printing an error message if this method is overridden
 
+    # Defining an actionscript method to set the header data
     def as_setHeaderS(self, data):
-        return self.flashObject.as_setHeader(data) if self._isDAAPIInited() else None
+        return self.flashObject.as_setHeader(data) if self._isDAAPIInited() else None # Returning None if DAAPI is not initialized, otherwise calling the corresponding actionscript method
 
+    # Defining an actionscript method to set the filter data
     def as_setFiltersS(self, data):
-        return self.flashObject.as_setFilters(data) if self._isDAAPIInited() else None
+        return self.flashObject.as_setFilters(data) if self._isDAAPIInited() else None # Returning None if DAAPI is not initialized, otherwise calling the corresponding actionscript method
 
+    # Defining an actionscript method to set the vehicle data
     def as_setVehiclesS(self, data):
-        return self.flashObject.as_setVehicles(data) if self._isDAAPIInited() else None
+        return self.flashObject.as_setVehicles(data) if self._isDAAPIInited() else None # Returning None if DAAPI is not initialized, otherwise calling the corresponding actionscript method

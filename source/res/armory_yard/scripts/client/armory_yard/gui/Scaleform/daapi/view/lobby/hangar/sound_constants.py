da36b5f7611ed20117fac86137bad7113b7e4f3e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:19 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/Scaleform/daapi/view/lobby/hangar/sound_constants.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/Scaleform/daapi/view/lobby/hangar/sound_constants.py
@@ -1,25 +1,21 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/Scaleform/daapi/view/lobby/hangar/sound_constants.py
-from gui.sounds.filters import StatesGroup, States
-from sound_gui_manager import CommonSoundSpaceSettings
-from shared_utils import CONST_CONTAINER
+
+from gui.sounds.filters import StatesGroup, States  # Importing state constants for sound settings.
+from sound_gui_manager import CommonSoundSpaceSettings  # Importing CommonSoundSpaceSettings class for defining sound spaces.
+from shared_utils import CONST_CONTAINER  # Importing CONST_CONTAINER for defining constants container.
 
 class SOUNDS(CONST_CONTAINER):
-    COMMON_SOUND_SPACE = 'armory_yard'
-    COMMON_SOUND_INTRO_SPACE = 'armory_yard_intro'
-    COMMON_SOUND_VIDEO_REWARD_SPACE = 'armory_yard_reward_video'
-    STATE_PLACE = 'STATE_hangar_place'
-    STATE_PLACE_AY = 'STATE_hangar_place_customization'
-    VO_TAPE_RECORDER = 'ay_voiceover_taperecorder_ep_02_stage_{:02d}_start'
-    FIRST_ENTER = 'armory_yard_enter_first'
-    ENTER = 'armory_yard_enter'
-    EXIT = 'armory_yard_exit'
+    # A container for various sound-related constants.
+    COMMON_SOUND_SPACE = 'armory_yard'  # The name of the common sound space for the armory yard.
+    COMMON_SOUND_INTRO_SPACE = 'armory_yard_intro'  # The name of the common sound space for the armory yard intro.
+    COMMON_SOUND_VIDEO_REWARD_SPACE = 'armory_yard_reward_video'  # The name of the common sound space for the reward video in the armory yard.
+    STATE_PLACE = 'STATE_hangar_place'  # The entrance state for the armory yard sound space.
+    STATE_PLACE_AY = 'STATE_hangar_place_customization'  # The entrance state for the armory yard intro sound space.
+    VO_TAPE_RECORDER = 'ay_voiceover_taperecorder_ep_02_stage_{:02d}_start'  # The format string for the voice-over tape recorder sound name.
+    FIRST_ENTER = 'armory_yard_enter_first'  # The name of the sound event for the first entry to the armory yard.
+    ENTER = 'armory_yard_enter'  # The name of the sound event for entering the armory yard.
+    EXIT = 'armory_yard_exit'  # The name of the sound event for exiting the armory yard.
 
 
-def getStageVoTapeRecorderName(stage):
-    return SOUNDS.VO_TAPE_RECORDER.format(stage)
-
-
-ARMORY_YARD_SOUND_SPACE = CommonSoundSpaceSettings(name=SOUNDS.COMMON_SOUND_SPACE, entranceStates={SOUNDS.STATE_PLACE: SOUNDS.STATE_PLACE_AY}, exitStates={}, persistentSounds=(), stoppableSounds=(), priorities=(), autoStart=True, enterEvent='', exitEvent='')
-ARMORY_YARD_INTRO_SOUND_SPACE = CommonSoundSpaceSettings(name=SOUNDS.COMMON_SOUND_INTRO_SPACE, entranceStates={SOUNDS.STATE_PLACE: SOUNDS.STATE_PLACE_AY}, exitStates={}, persistentSounds=(), stoppableSounds=(), priorities=(), autoStart=True, enterEvent='', exitEvent='')
-ARMORY_YARD_REWARD_VIDEO_SOUND_SPACE = CommonSoundSpaceSettings(name=SOUNDS.COMMON_SOUND_VIDEO_REWARD_SPACE, entranceStates={StatesGroup.VIDEO_OVERLAY: States.VIDEO_OVERLAY_ON}, exitStates={StatesGroup.VIDEO_OVERLAY: States.VIDEO_OVERLAY_OFF}, persistentSounds=(), stoppableSounds=(), priorities=(), autoStart=True, enterEvent='', exitEvent='')
+def getStageVoTapeRecorderName

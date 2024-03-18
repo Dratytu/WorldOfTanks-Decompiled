94abf534a429a09d2afaa9e496aeaf6583f1729e
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:04 +0300

    comment

--- source/res/scripts/client/GraphicsPresets.py
+++ source/res/scripts/client/GraphicsPresets.py
@@ -1,19 +1,27 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/GraphicsPresets.py
+
 import BigWorld
 import ResMgr
+
+# The resource file for the graphics settings presets
 graphicsPresetsResource = 'system/data/graphics_settings_presets.xml'
 
 class GraphicsPresets:
 
     def __init__(self):
+        # Open the resource file and initialize the presets object
         sect = ResMgr.openSection(graphicsPresetsResource)
         self.entries = {}
         self.entryNames = []
         self.selectedOption = -1
+        
+        # Iterate through each group of settings in the resource file
         for group in sect.values():
             if group.asString != '':
                 entry = {}
+                
+                # For each setting in the group, add it to the entry dictionary
                 for setting in group.values():
                     if setting.name == 'entry':
                         entry[setting.readString('label')] = setting.readInt('activeOption')
@@ -21,14 +29,18 @@ class GraphicsPresets:
                 self.entries[group.asString] = entry
                 self.entryNames.append(group.asString)
 
+        # Set the selected option based on the current graphics settings
         self.setSelectedOption()
 
     def setSelectedOption(self):
         self.selectedOption = -1
         currentOptionMap = {}
+        
+        # Create a map of the current graphics settings
         for currentOption in BigWorld.graphicsSettings():
             currentOptionMap[currentOption[0]] = currentOption[1]
 
+        # Iterate through each entry and check if it matches the current graphics settings
         for i in range(0, len(self.entryNames)):
             foundOption = True
             for setting in self.entries[self.entryNames[i]].items():
@@ -41,6 +53,7 @@ class GraphicsPresets:
                 break
 
     def selectGraphicsOptions(self, option):
+        # Set the graphics settings based on the selected entry
         currentOption = self.entries[self.entryNames[option]]
         for setting in currentOption.items():
             try:

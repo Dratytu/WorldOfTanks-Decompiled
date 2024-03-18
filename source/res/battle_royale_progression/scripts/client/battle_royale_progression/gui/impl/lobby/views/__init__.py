2d5f1bb9260f43b8bc88ca58d8cd140662ebf6c8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:47 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/lobby/views/__init__.py
+++ source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/lobby/views/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale_progression/scripts/client/battle_royale_progression/gui/impl/lobby/views/__init__.py
-pass
+
+# This script defines the initialization file for the lobby views in the Battle Royale progression module.
+# Currently, it does not contain any code, as the specific view components are likely defined in separate modules.
+# However, this file serves as an entry point for importing all lobby view-related modules in the Battle Royale progression system.
+
+# The 'pass' statement is a placeholder and does nothing when executed. It is used here to avoid raising a SyntaxError for an empty suite.
+# In a fully implemented version of this script, the 'pass' statement would be replaced with actual code defining lobby view components.

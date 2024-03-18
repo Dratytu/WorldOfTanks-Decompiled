98338bf8b0a9e35419cc8ec5d7405978e0f0ce78
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:40 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/comp7/damage_panel.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/comp7/damage_panel.py
@@ -1,25 +1,34 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/comp7/damage_panel.py
+
 import logging
 import BigWorld
 from gui.Scaleform.daapi.view.battle.shared.damage_panel import DamagePanel, STATUS_ID, ConcurrentStatusManager
 from gui.battle_control.battle_constants import VEHICLE_VIEW_STATE
+
+# Initialize the logger for this module
 _logger = logging.getLogger(__name__)
 
+# Subclass of DamagePanel specifically for Comp7 battles
 class Comp7DamagePanel(DamagePanel):
 
+    # Override the method for getting the concurrent status manager
     def _getConcurrentStatusManager(self):
         return _Comp7ConcurrentStatusManager
 
+    # Update the stun status of the vehicle
     def _updateStun(self, stunInfo):
         self.__updateCrewStun(buffName=VEHICLE_VIEW_STATE.STUN, isActive=stunInfo.endTime > 0, duration=stunInfo.endTime)
 
+    # Update the crew buff status of the vehicle
     def _updateCrewBuff(self, buffName, isActive, duration):
         self.__updateEffect(buffName, isActive, duration, self._statusAnimPlayers[STATUS_ID.BUFF])
 
+    # Helper method for updating the crew stun status
     def __updateCrewStun(self, buffName, isActive, duration):
         self.__updateEffect(buffName, isActive, duration, self._statusAnimPlayers[STATUS_ID.STUN])
 
+    # Helper method for updating the effect status
     def __updateEffect(self, buffName, isActive, duration, animationPlayer):
         if isActive:
             self._crewBuffManager.setStatus(buffName, duration, animationPlayer)
@@ -27,25 +36,31 @@ class Comp7DamagePanel(DamagePanel):
             self._crewBuffManager.deleteStatus(buffName)
 
 
+# Tuple of status effects, ordered by priority
 _STATUS_EFFECTS_PRIORITY = (VEHICLE_VIEW_STATE.AOE_INSPIRE, VEHICLE_VIEW_STATE.AOE_HEAL, VEHICLE_VIEW_STATE.STUN)
 
+# Subclass of ConcurrentStatusManager specifically for Comp7 battles
 class _Comp7ConcurrentStatusManager(ConcurrentStatusManager):
 
+    # Override the method for setting the status
     def setStatus(self, statusName, endTime, animationPlayer=None):
         self._currentStatuses[statusName] = (endTime, animationPlayer or self._animationPlayer)
         self._updateBuffAnimation()
 
+    # Override the method for deleting the status
     def deleteStatus(self, statusName):
         self._currentStatuses.pop(statusName, None)
         self._updateBuffAnimation()
         return
 
+    # Helper method for getting the priority of an effect
     def _getEffectsPriority(self, effect):
         try:
             return _STATUS_EFFECTS_PRIORITY.index(effect)
         except ValueError:
             return -1
 
+    # Helper method for updating the animation of the status effects
     def _updateBuffAnimation(self):
         self._exposedStatusEndTime, self._exposedStatus = (0, None)
         currTime = BigWorld.serverTime()
@@ -65,3 +80,4 @@ class _Comp7ConcurrentStatusManager(ConcurrentStatusManager):
         else:
             self._animationPlayer.hideStatus(True)
         return None
+

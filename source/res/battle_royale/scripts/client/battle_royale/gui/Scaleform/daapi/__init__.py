2008b3e1d0508f2f57f131b9e351092058ea0b51
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:17 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/__init__.py
-pass
+
+# This script is the initialization file for the Scaleform DAAPI (Dynamic Actionscript API)
+# for the Battle Royale game mode. The DAAPI is a set of interfaces and classes that
+# provide the communication between the client and the server for the game.
+#
+# The script does not contain any implemented functions, but rather sets up the
+# necessary imports and namespaces for the DAAPI.

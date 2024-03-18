a9164d6302d47d8a84c53898707c001f54c3f88a
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:38 +0300

    comment

--- source/res/battle_royale/scripts/client/InBattleUpgradesAvatar.py
+++ source/res/battle_royale/scripts/client/InBattleUpgradesAvatar.py
@@ -1,22 +1,46 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/InBattleUpgradesAvatar.py
-import BigWorld
-from debug_utils import LOG_DEBUG, LOG_WARNING
-import BattleReplay
-from items import vehicles, ITEM_TYPES
+
+import BigWorld  # Imported for creating a DynamicScriptComponent
+from debug_utils import LOG_DEBUG, LOG_WARNING  # Imported for logging debug and warning messages
+import BattleReplay  # Imported for replay control
+from items import vehicles, ITEM_TYPES  # Imported for vehicle-related functionalities
 
 class InBattleUpgradesAvatar(BigWorld.DynamicScriptComponent):
+    """
+    A class representing the in-battle upgrades for an avatar.
+    """
 
     def onEnterWorld(self, *args):
+        """
+        Called when the avatar enters the world.
+        """
         pass
 
     def onLeaveWorld(self, *args):
+        """
+        Called when the avatar leaves the world.
+        """
         pass
 
     def vehicleUpgradeResponse(self, intCDs, reasons):
-        player = self.entity
+        """
+        Handles the response for vehicle upgrades.
+
+        :param intCDs: A list of integer compact descriptor codes for the upgrades.
+        :param reasons: A list of boolean values indicating success or failure for each upgrade.
+        """
+        player = self.entity  # The player entity
 
         def __vehicleUpgradeLogger(isSuccess, intCD, reason, moduleTxt):
+            """
+            A helper function for logging upgrade successes and failures.
+
+            :param isSuccess: A boolean value indicating success or failure.
+            :param intCD: The integer compact descriptor code for the upgrade.
+            :param reason: The reason for the upgrade failure.
+            :param moduleTxt: The text for the module type.
+            """
             if isSuccess:
                 LOG_DEBUG('{} intCD = {} successfully installed'.format(moduleTxt, intCD))
             else:
@@ -25,19 +49,15 @@ class InBattleUpgradesAvatar(BigWorld.DynamicScriptComponent):
         for intCD, reason in zip(intCDs, reasons):
             __vehicleUpgradeLogger(reason == '', intCD, reason, 'Main' if intCD is intCDs[0] else 'Additional')
 
-        mainIntCDs = intCDs[0]
-        mainSuccess = not reasons[0]
-        if mainSuccess and ITEM_TYPES.vehicleGun in [ vehicles.parseIntCompactDescr(intCD)[0] for intCD in intCDs ]:
+        mainIntCDs = intCDs[0]  # The main integer compact descriptor code
+        mainSuccess = not reasons[0]  # The success or failure of the main upgrade
+
+        if mainSuccess and ITEM_TYPES.vehicleGun in [vehicles.parseIntCompactDescr(intCD)[0] for intCD in intCDs]:
+            """
+            If the main upgrade was successful and it's a vehicle gun, upgrade the vehicle gun.
+            """
             self.__upgradeVehicleGun()
+
         if player.guiSessionProvider.dynamic.progression:
             if BattleReplay.g_replayCtrl.isPlaying:
-                player.guiSessionProvider.dynamic.progression.vehicleUpgradeRequest(mainIntCDs)
-            player.guiSessionProvider.dynamic.progression.vehicleUpgradeResponse(mainIntCDs, mainSuccess)
-
-    def __upgradeVehicleGun(self):
-        player = self.entity
-        if player.guiSessionProvider.shared.ammo:
-            player.guiSessionProvider.shared.ammo.clear(leave=False)
-
-    def testAvatarMethod(self):
-        LOG_DEBUG('testAvatarMethod')
+                player.guiSession

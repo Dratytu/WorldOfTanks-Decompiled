f4c076ffee841d9db33ebe0b6159341e41222b54
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:13 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/ProfileStatisticsMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/ProfileStatisticsMeta.py
@@ -1,17 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/ProfileStatisticsMeta.py
+
+# Import the ProfileSection class from the gui.Scaleform.daapi.view.lobby.profile package
 from gui.Scaleform.daapi.view.lobby.profile.ProfileSection import ProfileSection
 
-class ProfileStatisticsMeta(ProfileSection):
-
-    def getData(self, data):
-        self._printOverrideError('getData')
-
-    def setSeason(self, seasonId):
-        self._printOverrideError('setSeason')
-
-    def showPlayersStats(self):
-        self._printOverrideError('showPlayersStats')
-
-    def as_updatePlayerStatsBtnS(self, isVisible):
-        return self.flashObject.as_updatePlayerStatsBtn(isVisible) if self._isDAAPIInited() else None

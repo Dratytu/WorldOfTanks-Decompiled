2736b7a6b91ea5c8eba4758f42c8048e90837cf3
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:09 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/framework/entities/abstract/SoundManagerMeta.py
+++ source/res/scripts/client/gui/Scaleform/framework/entities/abstract/SoundManagerMeta.py
@@ -1,8 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/framework/entities/abstract/SoundManagerMeta.py
+
+# Import the BaseDAAPIComponent class from the gui.Scaleform.framework.entities.BaseDAAPIComponent module
 from gui.Scaleform.framework.entities.BaseDAAPIComponent import BaseDAAPIComponent
 
+# Define the SoundManagerMeta class that inherits from BaseDAAPIComponent
 class SoundManagerMeta(BaseDAAPIComponent):
 
+    # Define the soundEventHandler method
     def soundEventHandler(self, group, state, type, id):
+        # Print an error message indicating that the method has been overridden
+        # This is useful for debugging and ensuring that the method is being called correctly
         self._printOverrideError('soundEventHandler')
+

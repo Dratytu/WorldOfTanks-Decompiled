883177eb92f33d0c2e90e11b7253f62026b0d6c7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:31 +0300

    comment

--- source/res/comp7/scripts/client/comp7/gui/feature/comp7_modifiers_data_provider.py
+++ source/res/comp7/scripts/client/comp7/gui/feature/comp7_modifiers_data_provider.py
@@ -1,9 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: comp7/scripts/client/comp7/gui/feature/comp7_modifiers_data_provider.py
+
+# Importing required modules
 from battle_modifiers.gui.feature.modifiers_data_provider import ModifiersDataProvider
 from comp7.constants import COMP7_SEASON_MODIFIERS_DOMAIN
 
+# Inheriting from the ModifiersDataProvider class
 class Comp7ModifiersDataProvider(ModifiersDataProvider):
 
+    # Overriding the _readClientDomain method
     def _readClientDomain(self, modifier):
+        # Returning the COMP7_SEASON_MODIFIERS_DOMAIN for the given modifier
         return COMP7_SEASON_MODIFIERS_DOMAIN

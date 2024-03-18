174a49905edd1b51542859ad55dbcddb8b746510
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:59 +0300

    comment

--- source/res/scripts/client/VehicleBuff.py
+++ source/res/scripts/client/VehicleBuff.py
@@ -1,6 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/VehicleBuff.py
+
+# Importing the ViewStateComponent class from the view_state_component module
 from view_state_component import ViewStateComponent
 
+# Defining the VehicleBuff class which inherits from the ViewStateComponent class
 class VehicleBuff(ViewStateComponent):
+    # The class definition is empty, meaning it doesn't contain any additional methods or attributes
     pass
+
+# This class can be used to represent a buff on a vehicle in the context of a view state component
+# It is a placeholder for future functionality related to vehicle buffs

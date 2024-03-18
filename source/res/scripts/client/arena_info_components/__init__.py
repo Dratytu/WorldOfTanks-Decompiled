2b0cbf8b381368537279a1c9d6786d30789022da
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:22 +0300

    comment

--- source/res/scripts/client/arena_info_components/__init__.py
+++ source/res/scripts/client/arena_info_components/__init__.py
@@ -1,3 +1,14 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/arena_info_components/__init__.py
-pass
+# This is a Python file that contains various arena information components.
+# It is currently empty, but may be used in the future to define classes or functions
+# related to displaying or managing information about game arenas.
+
+# The file is written in Python 2.7 and has been automatically decompiled from bytecode.
+# This means that the original source code for this file is not available.
+
+# The file contains a single pass statement, which serves as a placeholder and has no effect.
+# It is likely that additional code will be added to this file in the future.
+
+# Example usage:
+# from arena_info_components import *
+# ...
+

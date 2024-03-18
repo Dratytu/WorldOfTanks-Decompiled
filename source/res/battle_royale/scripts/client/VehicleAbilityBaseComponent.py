6de33f53a66e9effd4c1723ff733d50b06b37b26
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:52 +0300

    comment

--- source/res/battle_royale/scripts/client/VehicleAbilityBaseComponent.py
+++ source/res/battle_royale/scripts/client/VehicleAbilityBaseComponent.py
@@ -1,5 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/VehicleAbilityBaseComponent.py
+
 import BigWorld
 from gui.battle_control.battle_constants import FEEDBACK_EVENT_ID
 from Event import EventsSubscriber
@@ -7,36 +8,43 @@ from Event import EventsSubscriber
 class VehicleAbilityBaseComponent(BigWorld.DynamicScriptComponent):
 
     def __init__(self, timerViewID=None, markerID=None):
+        # Initialize the component with optional timerViewID and markerID
         super(VehicleAbilityBaseComponent, self).__init__()
         self._guiSessionProvider = self.entity.guiSessionProvider
         self._guiFeedback = self.entity.guiSessionProvider.shared.feedback
-        self._finishTime = self.finishTime
-        self._timerViewID = timerViewID
-        self._markerID = markerID
-        self._es = EventsSubscriber()
-        self.__postponedCalls = []
-        self.__isSwitching = False
-        self._avatar = BigWorld.player()
-        self._subscribeOnEvents()
-        self._updateVisuals()
+        self._finishTime = self.finishTime  # Finish time for the ability
+        self._timerViewID = timerViewID  # ID for the timer visual component
+        self._markerID = markerID  # ID for the marker visual component
+        self._es = EventsSubscriber()  # Event subscriber for handling events
+        self.__postponedCalls = []  # List for storing postponed function calls
+        self.__isSwitching = False  # Flag for checking if the view is switching
+        self._avatar = BigWorld.player()  # The local player avatar
+        self._subscribeOnEvents()  # Subscribe to relevant events
+        self._updateVisuals()  # Update visual components initially
 
     def set_finishTime(self, _=None):
+        # Setter for finishTime, updates visual components
         self._finishTime = self.finishTime
         self._updateVisuals()
 
     def onDestroy(self):
+        # Called when the component is destroyed, cleans up resources
         self._destroy()
 
     def onLeaveWorld(self, *args):
+        # Called when the entity leaves the game world, cleans up resources
         self._destroy()
 
     def getInfo(self):
+        # Returns an empty dictionary, not used in this script
         return {}
 
     def _onAvatarReady(self):
+        # Called when the avatar is ready, not used in this script
         pass
 
     def _subscribeOnEvents(self):
+        # Subscribes to relevant events for the component
         self._es.subscribeToEvent(self._guiSessionProvider.onUpdateObservedVehicleData, self._onUpdateObservedVehicleData)
         self._es.subscribeToEvent(self.entity.onAppearanceReady, self._onAppearanceReady)
         if self._avatar is not None and self._avatar.inputHandler is not None:
@@ -45,9 +53,11 @@ class VehicleAbilityBaseComponent(BigWorld.DynamicScriptComponent):
         return
 
     def _unSubscribeFromAllEvents(self):
+        # Unsubscribes from all subscribed events
         self._es.unsubscribeFromAllEvents()
 
     def _destroy(self):
+        # Clean up resources when the component is destroyed
         self._unSubscribeFromAllEvents()
         self.__isSwitching = False
         self._updateVisuals(isShow=False)
@@ -56,25 +66,29 @@ class VehicleAbilityBaseComponent(BigWorld.DynamicScriptComponent):
         return
 
     def _updateVisuals(self, isShow=True):
+        # Update visual components like the timer and marker
         self._updateTimer(self._getTimerData(isShow))
         self._updateMarker(self._getMarkerData(isShow))
 
     def _updateTimer(self, data):
+        # Update the timer visual component
         if self._timerViewID is None:
             return
         else:
             if self.__isSwitching:
-                self.__pushToPostponeCall(self._updateTimer, (data,))
+              `enter code here`                self.__pushToPostponeCall(self._updateTimer, (data,))
             elif self._canUpdateTimer():
                 self._guiSessionProvider.invalidateVehicleState(self._timerViewID, data)
                 self._updateMarker(self._getMarkerData(isShow=False), isHide=True)
             return
 
     def _canUpdateTimer(self):
+        # Check if the timer can be updated based on the current camera mode and vehicle
         modeName = self._avatar.inputHandler.ctrlModeName
         return True if self.entity.id == self._avatar.inputHandler.ctrl.curVehicleID and modeName != 'video' else False
 
     def _updateMarker(self, data, isHide=False):
+        # Update the marker visual component
         if self._markerID is None:
             return
         else:
@@ -87,11 +101,13 @@ class VehicleAbilityBaseComponent(BigWorld.DynamicScriptComponent):
             return
 
     def _getTimerData(self, isShow=True):
+        # Prepare data for updating the timer visual component
         data = {'duration': self._getDuration() if isShow else 0.0,
          'endTime': self._finishTime}
         return data
 
     def _getMarkerData(self, isShow=True):
+        # Prepare data for updating the marker visual component
         data = {'isShown': isShow,
          'isSourceVehicle': True,
          'duration': self._getDuration() if isShow else 0.0,
@@ -100,38 +116,5 @@ class VehicleAbilityBaseComponent(BigWorld.DynamicScriptComponent):
         return data
 
     def _getDuration(self):
-        return self._finishTime - BigWorld.serverTime() if self._finishTime else 0.0
-
-    def _onUpdateObservedVehicleData(self, vehicleID, *args):
-        if self.__isSwitching:
-            self.__isSwitching = False
-            self.__postponeCallProcessing()
-        self._updateVisuals()
-
-    def _onAppearanceReady(self):
-        vehicle = self.entity
-        appearance = vehicle.appearance
-        if appearance is None or not appearance.isConstructed:
-            return
-        elif vehicle.health <= 0:
-            return
-        else:
-            self._updateVisuals()
-            return
-
-    def __onSwitchingViewPoint(self):
-        self.__isSwitching = True
-
-    def __onCameraChanged(self, cameraName, currentVehicleId=None):
-        if cameraName == 'video':
-            self._updateMarker(self._getMarkerData())
-
-    def __pushToPostponeCall(self, func, args):
-        self.__postponedCalls.append((func, args))
-
-    def __postponeCallProcessing(self):
-        for f_data in self.__postponedCalls:
-            f, args = f_data
-            f(*args)
-
-        self.__postponedCalls = []
+        # Calculate the remaining duration for the ability
+        return self._finishTime - BigWorld.serverTime

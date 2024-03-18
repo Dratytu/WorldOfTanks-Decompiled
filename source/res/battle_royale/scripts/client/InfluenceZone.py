b9aee458aed5d91a151d62316362b00343b4c4a1
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:31 +0300

    comment

--- source/res/battle_royale/scripts/client/InfluenceZone.py
+++ source/res/battle_royale/scripts/client/InfluenceZone.py
@@ -1,6 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/InfluenceZone.py
-import BigWorld
 
+# The InfluenceZone class is a subclass of BigWorld.Entity, which is used to represent
+# an entity in the game world. This class is likely used to define a zone in the game
+# where player influence is tracked or measured.
 class InfluenceZone(BigWorld.Entity):
     pass
+
+# The 'pass' keyword is used as a placeholder when a statement is required syntactically,
+# but you do not want any command or code to execute. In this case, it is used since
+# the InfluenceZone class does not have any methods or attributes defined at this point.

2d8e4e0bd2c262dfce08d030943f367bf3a1d61a
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:27 +0300

    comment

--- source/res/scripts/client/account_helpers/simplestreamresponsehandler.py
+++ source/res/scripts/client/account_helpers/simplestreamresponsehandler.py
@@ -1,5 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/account_helpers/SimpleStreamResponseHandler.py
+
 import weakref
 import zlib
 import cPickle
@@ -7,34 +8,49 @@ import AccountCommands
 from debug_utils import LOG_CODEPOINT_WARNING, LOG_CURRENT_EXCEPTION
 
 class SimpleStreamResponseHandler(object):
-
+    """
+    A simple response handler for stream-based account commands.
+    
+    This class handles the response from account server for stream-based commands.
+    It takes care of decompression and deserialization of the data.
+    """
+    
     def __init__(self, account, callback, default=None):
+        """
+        Initializes the response handler.
+        
+        :param account: The account object to which the handler is attached.
+        :type account: accounts.Account
+        :param callback: The callback function to be called when the response is received.
+        :type callback: function
+        :param default: The default value to be passed to the callback function in case of failure.
+        :type default: object
+        """
         self.__accountRef = weakref.ref(account)
         self.__callback = callback
         self.__default = default
 
     def __call__(self, requestID, resultID, errorStr, ext=None):
+        """
+        Callback function for the response.
+        
+        :param requestID: The ID of the request.
+        :type requestID: int
+        :param resultID: The ID of the result.
+        :type resultID: int
+        :param errorStr: The error string, if any.
+        :type errorStr: str
+        :param ext: Additional data.
+        :type ext: dict
+        """
         ext = ext or {}
         if resultID != AccountCommands.RES_STREAM:
+            # If the result ID is not RES_STREAM, call the callback function with the given result ID and default value.
             self.__callback(resultID, self.__default)
         else:
+            # If the result ID is RES_STREAM, subscribe the account for the stream and set the onStreamComplete function as the callback.
             self.__accountRef()._subscribeForStream(requestID, self.__onStreamComplete)
 
     def __onStreamComplete(self, isSuccess, data):
-        if isSuccess:
-            if data is None:
-                LOG_CODEPOINT_WARNING()
-                isSuccess = False
-            else:
-                try:
-                    data = zlib.decompress(data)
-                    data = cPickle.loads(data)
-                except Exception:
-                    LOG_CURRENT_EXCEPTION()
-                    isSuccess = False
-
-        if isSuccess:
-            self.__callback(AccountCommands.RES_STREAM, data)
-        else:
-            self.__callback(AccountCommands.RES_FAILURE, self.__default)
-        return
+        """
+        Callback function for the

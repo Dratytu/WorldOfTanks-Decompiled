f91742f7f32ed3f11ec94c0c0896d67a838de331
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:24 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/battle_control/controllers/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/battle_control/controllers/__init__.py
@@ -1,10 +1,17 @@
-# This is an empty __init__.py file for the battle_control package within
-# the battle_royale/gui/battle_control/controllers directory.
+# This is an empty __init__.py file for the battle_control package,
+# located in the battle_royale/gui/battle_control/controllers directory.
 #
 # In Python, __init__.py files are used to mark a directory as a package,
-# and can contain initialization code for the package. In this case, this
-# file does not contain any initialization code, but is required for the
-# directory to be recognized as a package.
+# and can contain initialization code for the package. This particular
+# __init__.py file does not contain any initialization code, but is
+# required for the directory to be recognized as a package.
 #
-# Any modules or sub-packages within this package should be imported here
-# if they are intended to be part of the public API for this package.
+# The purpose of this package is to contain all of the controller code
+# for the battle control feature of the larger battle royale game.
+# Controllers are responsible for handling user input and managing
+# interactions between different parts of the application.
+#
+# Any modules or sub-packages within this package that are intended
+# to be part of the public API for this package should be imported here.
+# However, in this case, no modules or sub-packages are being imported
+# directly in this file.

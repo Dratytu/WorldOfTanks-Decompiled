5e647c7517e4769af8cc4f6753b50a6a6b7c0ab0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:25 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/EpicBattlesAfterBattleViewMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/EpicBattlesAfterBattleViewMeta.py
@@ -1,32 +1,38 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/EpicBattlesAfterBattleViewMeta.py
+
+# Import View class from gui.Scaleform.framework.entities.View, which is a metaclass for all views in the Scaleform framework
 from gui.Scaleform.framework.entities.View import View
 
+# Define EpicBattlesAfterBattleViewMeta class that inherits from View class
 class EpicBattlesAfterBattleViewMeta(View):
 
+    # Define a method that gets called when the intro starts playing
     def onIntroStartsPlaying(self):
+        # Print an error message if this method is not overridden in a subclass
         self._printOverrideError('onIntroStartsPlaying')
 
+    # Define a method that gets called when the ribbon starts playing
     def onRibbonStartsPlaying(self):
+        # Print an error message if this method is not overridden in a subclass
         self._printOverrideError('onRibbonStartsPlaying')
 
+    # Define a method that gets called when the next button is clicked
     def onNextBtnClick(self):
+        # Print an error message if this method is not overridden in a subclass
         self._printOverrideError('onNextBtnClick')
 
+    # Define a method that gets called when the close button is clicked
     def onCloseBtnClick(self):
+        # Print an error message if this method is not overridden in a subclass
         self._printOverrideError('onCloseBtnClick')
 
+    # Define a method that gets called when the rewards button is clicked
     def onRewardsBtnClick(self):
+        # Print an error message if this method is not overridden in a subclass
         self._printOverrideError('onRewardsBtnClick')
 
+    # Define a method that gets called when the escape key is pressed
     def onEscapePress(self):
-        self._printOverrideError('onEscapePress')
-
-    def onProgressBarStartAnim(self):
-        self._printOverrideError('onProgressBarStartAnim')
-
-    def onProgressBarCompleteAnim(self):
-        self._printOverrideError('onProgressBarCompleteAnim')
-
-    def as_setDataS(self, data):
-        return self.flashObject.as_setData(data) if self._isDAAPIInited() else None
+        # Print an error message if this method is not overridden in a subclass
+        self._printOverrideError

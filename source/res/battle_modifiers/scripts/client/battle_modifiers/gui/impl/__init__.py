e223001a9368b74f69297bb1b9771d8d928b3d01
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:30 +0300

    comment

--- source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/__init__.py
+++ source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/__init__.py
@@ -1,3 +1,7 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: battle_modifiers/scripts/client/battle_modifiers/gui/impl/__init__.py
-pass
+# This is a Python script for the 'battle_modifiers' module, specifically for the 'gui/impl' package.
+# The script does not contain any executable code, but rather serves to import all submodules in the package.
+
+# The 'pass' statement is a placeholder and does nothing when executed. It is used here to indicate that the script is intentionally empty.
+
+# By importing all submodules in this way, the package's functionality can be easily accessed from other parts of the application.
+# However, it's important to note that this approach can potentially lead to issues with circular imports, so care should be taken when structuring the code.

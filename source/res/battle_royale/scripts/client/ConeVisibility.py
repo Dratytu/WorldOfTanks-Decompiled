c70f235f47889256a353fe9b3fa6a0f725683400
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:22 +0300

    comment

--- source/res/battle_royale/scripts/client/ConeVisibility.py
+++ source/res/battle_royale/scripts/client/ConeVisibility.py
@@ -1,27 +1,9 @@
-# Python 2.7
 
-import BigWorld
 
-class ConeVisibility(BigWorld.DynamicScriptComponent):
-    """
-    A dynamic script component for managing cone visibility.
-    """
 
-    def __init__(self):
-        """
-        Initialize the cone visibility component.
-        """
-        self.view_cone_angle = 45  # View cone angle in degrees
-        self.view_cone_range = 50  # View cone range in meters
-        self.visible_entities = set()  # Set to store visible entities
 
-    def onEnterWorld(self, *args):
-        """
-        Called when the entity enters the world.
-        """
-        self.space_proxy = BigWorld.SpaceProxy(self.spaceID)
-        self.space_proxy.addEntityVisibilityListener(self)
-
-    def onLeaveWorld(self, *args):
-        """
-
+"""
+Called when the entity's position changes.
+This method updates the view cone direction and checks the visibility of all entities within the view cone.
+"""
+self.view_cone_direction = self.get

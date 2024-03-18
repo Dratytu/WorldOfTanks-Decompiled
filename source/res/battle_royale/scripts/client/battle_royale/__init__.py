4304a6b6b9ed97180d387717df28583468102d7d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:33 +0300

    improve

--- source/res/battle_royale/scripts/client/battle_royale/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/__init__.py
@@ -1,3 +1,3 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
+# Python bytecode 3.8 (decompiled from Python 3.8)
 # Embedded file name: battle_royale/scripts/client/battle_royale/__init__.py
-pass
+

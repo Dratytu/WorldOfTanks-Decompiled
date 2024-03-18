22c6fa87cde09f914d1369ec621fcd84c58d4cd5
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:06 +0300

    comment

--- source/res/scripts/client/bwobsolete_helpers/V4ShaderConsts.py
+++ source/res/scripts/client/bwobsolete_helpers/V4ShaderConsts.py
@@ -1,32 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/bwobsolete_helpers/V4ShaderConsts.py
+
+# Importing the Math module which contains various mathematical operations
 import Math
-_mov = 0
-_rcp = 1
-_bias = 2
-_comp = 3
-_mul = 4
-_div = 5
-_add = 6
-_sub = 7
-_dot = 8
-_min = 9
-_max = 10
-_sge = 11
-_slt = 12
-_r0 = Math.getRegister(0)
-_r1 = Math.getRegister(1)
-_r2 = Math.getRegister(2)
-_r3 = Math.getRegister(3)
-_r4 = Math.getRegister(4)
-_r5 = Math.getRegister(5)
-_r6 = Math.getRegister(6)
-_r7 = Math.getRegister(7)
-_r8 = Math.getRegister(8)
-_r9 = Math.getRegister(9)
-_r10 = Math.getRegister(10)
-_r11 = Math.getRegister(11)
-_r12 = Math.getRegister(12)
-_r13 = Math.getRegister(13)
-_r14 = Math.getRegister(14)
-_r15 = Math.getRegister(15)
+
+# Defining constants for various shader operations
+_mov = 0  # Move
+_rcp = 1  # Reciprocal
+_bias = 2  # Bias
+_comp = 3  # Comparison
+_mul = 4  # Multiply
+_div = 5  # Divide
+_add = 6 

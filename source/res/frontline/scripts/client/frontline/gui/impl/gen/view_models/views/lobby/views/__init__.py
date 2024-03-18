1a244ec6a34337c46d5eb000f2f04a0c5f0d961b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:57 +0300

    comment

--- source/res/frontline/scripts/client/frontline/gui/impl/gen/view_models/views/lobby/views/__init__.py
+++ source/res/frontline/scripts/client/frontline/gui/impl/gen/view_models/views/lobby/views/__init__.py
@@ -1,3 +1,17 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: frontline/scripts/client/frontline/gui/impl/gen/view_models/views/lobby/views/__init__.py
-pass
+# frontline/scripts/client/frontline/gui/impl/gen/view_models/views/__init__.py
+#
+# This module is an empty init file for the 'frontline.gui.impl.gen.view_models.views' package.
+# It serves the purpose of allowing the package to be importable as a namespace, while not
+# containing any actual code or functionality.
+#
+# The 'views' package contains view models for the Frontline lobby, which are used to define
+# the data and logic for each lobby screen. View models are a key part of the MVVM (Model-View-
+# ViewModel) architectural pattern, which is used to separate the user interface from the
+# underlying data and logic.
+#
+# By keeping the user interface and data logic separate, view models make it easier to test,
+# maintain, and reuse code. They also make it easier to create user interfaces that are more
+# responsive, dynamic, and adaptable to different screen sizes and user inputs.
+#
+# This init file is intentionally left blank, as it is not necessary to include any code or
+# functionality in this file.

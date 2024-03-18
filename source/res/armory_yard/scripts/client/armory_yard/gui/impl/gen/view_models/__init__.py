ad19572460bb76a30865cb783df81d8f053057e2
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:08 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/__init__.py
@@ -1,8 +1,3 @@
-# armory_yard/gui/impl/gen/view_models/__init__.py
-
-"""
-This package contains various view model classes for use in the Armory Yard application.
-"""
-
 from .base_view_model import BaseViewModel
 # Include other view model classes here as needed
+

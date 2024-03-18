fa3c8a77549d1213fb8360f8684f1f6bff7bafaf
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:31 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/server_events/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/server_events/__init__.py
@@ -1,3 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/server_events/__init__.py
-pass
+
+# This is an empty __init__.py file for the server_events package in the
+# gui/Scaleform/daapi/view/lobby module.
+#
+# The purpose of this file is to serve as an entry point for the package,
+# providing a namespace for any modules or sub-packages within it.
+#
+# In this case, the server_events package is used for implementing
+# server event-related views in the game's lobby.
+#
+# However, since this file is empty, there are currently no views or
+# functionality provided by this package.

af4c04349a43f1fef08dcf245b1ceb84203ca8d6
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:28 +0300

    comment

--- source/res/scripts/client/ProtectionZone.py
+++ source/res/scripts/client/ProtectionZone.py
@@ -1,17 +1,27 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/ProtectionZone.py
+
 import BigWorld
 import Math
 from debug_utils import LOG_DEBUG
 
 class ProtectionZone(BigWorld.Entity):
+    """
+    A class representing a protection zone in a game world.
+    """
 
     def __init__(self):
+        """
+        Initializes a new ProtectionZone instance.
+        """
         super(ProtectionZone, self).__init__(self)
-        self.__lowerLeft = Math.Vector2(0, 0)
-        self.__upperRight = Math.Vector2(0, 0)
+        self.__lowerLeft = Math.Vector2(0, 0)  # The lower-left coordinate of the protection zone.
+        self.__upperRight = Math.Vector2(0, 0)  # The upper-right coordinate of the protection zone.
 
     def onEnterWorld(self, prereqs):
+        """
+        Called when the protection zone is added to the game world.
+        """
         LOG_DEBUG('ProtectionZone added ', self.zoneID)
         self.__lowerLeft = Math.Vector2(self.position.x - self.lengthX * 0.5, self.position.z - self.lengthZ * 0.5)
         self.__upperRight = Math.Vector2(self.position.x + self.lengthX * 0.5, self.position.z + self.lengthZ * 0.5)
@@ -23,12 +33,18 @@ class ProtectionZone(BigWorld.Entity):
         return
 
     def onLeaveWorld(self):
+        """
+        Called when the protection zone is removed from the game world.
+        """
         protectionZoneComponent = BigWorld.player().arena.componentSystem.protectionZoneComponent
         if protectionZoneComponent is not None:
             protectionZoneComponent.removeProtectionZone(self)
         return
 
     def set_isActive(self, oldValue):
+        """
+        Sets the active status of the protection zone.
+        """
         LOG_DEBUG('ProtectionZone active: ' + str(self.isActive))
         protectionZoneComponent = BigWorld.player().arena.componentSystem.protectionZoneComponent
         if protectionZoneComponent is not None:
@@ -37,4 +53,7 @@ class ProtectionZone(BigWorld.Entity):
 
     @property
     def bound(self):
+        """
+        Gets the bounding coordinates of the protection zone.
+        """
         return (self.__lowerLeft, self.__upperRight)

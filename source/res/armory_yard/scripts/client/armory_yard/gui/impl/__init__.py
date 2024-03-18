d7980a15e46c99e0ca70ef7bc3f75a801d21c3ff
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:16 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/__init__.py
@@ -1,3 +1,18 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/__init__.py
+# armory_yard/scripts/client/armory_yard/gui/impl/__init__.py
+
+# This is an empty Python file that serves as an entry point for the
+# graphical user interface (GUI) implementation of the Armory Yard
+# application.
+#
+# By importing this file, the various modules and components that make
+# up the GUI implementation will be imported and made available for
+# use. This allows for a more organized and modular codebase, as it
+# separates the GUI implementation from the other components of the
+# application.
+#
+# As this is an empty file, there is no code or functionality present
+# here. However, it is an important part of the overall structure of
+# the Armory Yard application.
+
 pass
+

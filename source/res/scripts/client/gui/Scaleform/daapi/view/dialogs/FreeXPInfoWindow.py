8c39815648a5f903f007c22bff8d526d9b870996
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:48 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/dialogs/FreeXPInfoWindow.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/dialogs/FreeXPInfoWindow.py
@@ -1,32 +1,45 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/dialogs/FreeXPInfoWindow.py
+
 from gui.Scaleform.daapi.view.meta.FreeXPInfoWindowMeta import FreeXPInfoWindowMeta
 __author__ = 'd_savitski'
 
 class FreeXPInfoWindow(FreeXPInfoWindowMeta):
+    """
+    A dialog window for displaying information about free experience points.
+
+    This class is a view component for the Free XP info window, which is displayed when the player
+    clicks on the "Free XP" button in the garage. The window shows the current amount of free XP
+    and allows the player to convert it to another type of experience points.
+
+    Attributes:
+        meta (dict): A dictionary containing metadata for the window, such as the title, submit
+            button label, and text information.
+        handler (callable): A callback function to be called when the window is closed.
+    """
 
     def __init__(self, ctx=None):
+        """
+        Initializes the FreeXPInfoWindow instance.
+
+        Args:
+            ctx (dict, optional): A dictionary containing initialization data for the window.
+        """
         super(FreeXPInfoWindow, self).__init__()
         self.meta = ctx.get('meta')
         self.handler = ctx.get('handler')
 
     def _populate(self):
+        """
+        Populates the window with data from the metadata dictionary.
+        """
         super(FreeXPInfoWindow, self)._populate()
         self.as_setTitleS(self.meta.getTitle())
         self.as_setSubmitLabelS(self.meta.getSubmitLbl())
         self.as_setTextS(self.meta.getTextInfo())
 
     def onWindowClose(self):
-        self.handler(True)
-        self.destroy()
+        """
+        Called when the window is closed.
 
-    def onSubmitButton(self):
-        self.onWindowClose()
-
-    def onCancelButton(self):
-        self.onWindowClose()
-
-    def _dispose(self):
-        super(FreeXPInfoWindow, self)._dispose()
-        self.handler = None
-        return
+        This method

2b702bc8f66510ceb1669d683186a3a67e5907c8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:16 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/Comp7PrimeTimeMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/Comp7PrimeTimeMeta.py
@@ -1,11 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/Comp7PrimeTimeMeta.py
-from gui.Scaleform.daapi.view.lobby.prime_time_view_base import PrimeTimeViewBase
 
-class Comp7PrimeTimeMeta(PrimeTimeViewBase):
+import sys
+if sys.version_info[0] < 3:
+    from gui.Scaleform.daapi.view.lobby.prime_time_view_base import PrimeTimeViewBase  # noqa
 
-    def as_setHeaderTextS(self, value):
-        return self.flashObject.as_setHeaderText(value) if self._isDAAPIInited() else None
-
-    def as_setBackgroundSourceS(self, source):
-        return self.flashObject.as_setBackgroundSource(source) if self._isDAAPIInited() else None

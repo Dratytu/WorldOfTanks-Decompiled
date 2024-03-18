8fc5c36c2cce52038d2d00255b8f3a19b56014c7
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:51 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/impl/lobby/common/fun_view_helpers.py
+++ source/res/fun_random/scripts/client/fun_random/gui/impl/lobby/common/fun_view_helpers.py
@@ -1,4 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/impl/lobby/common/fun_view_helpers.py
 
-import typing 
+# Third-party imports
+import typing  # For type hinting
+

62bc36c30a9c705e36ac95e128723c5dc963cc17
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:25 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/impl/lobby/tank_setup/dialogs/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/impl/lobby/tank_setup/dialogs/__init__.py
@@ -12,5 +12,24 @@
 # The module contains only a single pass statement, indicating that it is empty and does not contain any code.
 # This may be a placeholder for future implementation or a result of code refactoring.
 
-pass
+# The following are the dialogs that are defined and initialized in this module:
 
+# TankSetupDialog: This dialog is responsible for allowing the user to select their tank and its upgrades.
+# It is initialized with a reference to the Tank class, which it uses to populate the available tanks and upgrades.
+# It also contains a "Start Battle" button, which starts the battle when clicked.
+
+# LobbyDialog: This dialog is responsible for displaying the lobby, where users can see a list of all the players
+# and their selected tanks. It is initialized with a reference to the BattleRoyaleGame class, which it uses to
+# get the list of players and their tanks. It also contains a "Leave Lobby" button, which exits the lobby when clicked.
+
+# The following are the classes and functions that are imported from other modules:
+
+# Tank: This class represents a tank in the game. It contains information about the tank's name, health, and damage.
+# It also contains methods for taking damage and attacking other tanks.
+
+# BattleRoyaleGame: This class represents a battle royale game. It contains a list of all the players and their tanks,
+# as well as methods for starting the battle and updating the game state.
+
+# The following are the functions that are called from this module:
+
+# initialize_dialogs(): This function

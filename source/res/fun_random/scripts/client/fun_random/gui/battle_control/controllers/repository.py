1e78044f22dd7a92380e1afbce275924e133da78
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:56 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/battle_control/controllers/repository.py
+++ source/res/fun_random/scripts/client/fun_random/gui/battle_control/controllers/repository.py
@@ -1,13 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/battle_control/controllers/repository.py
+
+# Import necessary modules and functions
 from fun_random.gui.battle_control.controllers.sound_ctrls.fun_random_battle_sounds import createFunRandomBattleSoundsController
 from gui.battle_control.controllers.repositories import ClassicControllersRepository
 
-class FunRandomControllerRepository(ClassicControllersRepository):
-    __slots__ = ()
-
-    @classmethod
-    def create(cls, setup):
-        repository = super(FunRandomControllerRepository, cls).create(setup)
-        repository.addController(createFunRandomBattleSoundsController(setup))
-        return repository

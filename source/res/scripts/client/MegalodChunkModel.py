6e3cd0b61b65622ba211b549208c2d5011f18777
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:58 +0300

    comment

--- source/res/scripts/client/MegalodChunkModel.py
+++ source/res/scripts/client/MegalodChunkModel.py
@@ -1,14 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/MegalodChunkModel.py
 
 import BigWorld # Importing BigWorld module to inherit from UserDataObject
 
+# Define a class representing a MegalodChunkModel, which inherits from BigWorld's UserDataObject
 class MegalodChunkModel(BigWorld.UserDataObject):
     """
     A class representing a MegalodChunkModel, which inherits from BigWorld's UserDataObject.
     This class is a user data object that can be used to store and manage data in the game world.
     """
 
+    # Initialize the MegalodChunkModel instance
     def __init__(self):
         """
         Initialize the MegalodChunkModel instance.

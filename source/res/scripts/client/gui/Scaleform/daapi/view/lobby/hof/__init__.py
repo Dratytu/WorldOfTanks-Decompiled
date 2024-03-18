fa35108d7b19e6d239263cd6982a51822fc130c9
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:13 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/hof/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/hof/__init__.py
@@ -1,3 +1,8 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/hof/__init__.py
-pass
+
+# This is an empty __init__.py file, which is used to mark this directory as a package
+# in Python. It does not contain any code and does not have any functionality on its own.
+# Its sole purpose is to enable the import of modules and sub-packages contained within
+# this directory using the "from gui.Scaleform.daapi.view.lobby.hof import <module>"
+# syntax.

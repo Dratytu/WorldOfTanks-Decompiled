fb77962718a24ad8bf09146a0a44c9be1d99dff8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:00 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/genConsts/CUSTOMIZATION_CONSTS.py
+++ source/res/scripts/client/gui/Scaleform/genConsts/CUSTOMIZATION_CONSTS.py
@@ -1,8 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/gui/Scaleform/genConsts/CUSTOMIZATION_CONSTS.py
 
+# This script defines a class named CUSTOMIZATION_CONSTS, which contains
+# various constants used in customization-related functionality.
+# The constants are defined as class level variables.
 
 class CUSTOMIZATION_CONSTS(object):
+    # HISTORICAL_TYPE constant, which is assigned the value of 0.
+    # This constant likely represents a historical customization type.
     HISTORICAL_TYPE = 0
+
+    # NON_HISTORICAL_TYPE constant, which is assigned the value of 1.
+    # This constant likely represents a non-historical customization type.
     NON_HISTORICAL_TYPE = 1
+
+    # FANTASTICAL_TYPE constant, which is assigned the value of 2.
+    # This constant likely represents a fantastical customization type.
     FANTASTICAL_TYPE = 2

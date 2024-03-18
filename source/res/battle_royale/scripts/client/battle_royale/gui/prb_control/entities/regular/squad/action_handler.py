2cae1a496bb08640008c5d38025897e81a01e3e4
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:37 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/squad/action_handler.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/squad/action_handler.py
@@ -1,6 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/prb_control/entities/regular/squad/action_handler.py
+
+# Importing the base SquadActionsHandler class from the gui.prb_control.entities.base.squad.actions_handler module
 from gui.prb_control.entities.base.squad.actions_handler import SquadActionsHandler
 
-class BattleRoyaleSquadActionsHandler(SquadActionsHandler):
-    pass

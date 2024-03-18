a3f0709874467dfd7934319d1d68c0d786bb434a
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:41 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/battle_royale_progression/messenger/formatters/__init__.py
+++ source/res/battle_royale_progression/scripts/client/battle_royale_progression/messenger/formatters/__init__.py
@@ -1,10 +1,17 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale_progression/scripts/client/battle_royale_progression/messenger/formatters/__init__.py
+
+# Import necessary modules and classes
 from battle_royale_progression.gui import gui_constants
 from battle_royale_progression.messenger.formatters.service_channel import BRProgressionSystemMessageFormatter
 from gui.shared.system_factory import registerMessengerClientFormatter
-clientFormatters = {gui_constants.SCH_CLIENT_MSG_TYPE.BR_PROGRESSION_NOTIFICATIONS: BRProgressionSystemMessageFormatter()}
 
+# Define a dictionary of client formatters for different system message types
+clientFormatters = {
+    gui_constants.SCH_CLIENT_MSG_TYPE.BR_PROGRESSION_NOTIFICATIONS: BRProgressionSystemMessageFormatter()
+}
+
+# Function to register the client formatters with the messenger system
 def registerMessengerClientFormatters():
     for sysMsgType, formatter in clientFormatters.iteritems():
         registerMessengerClientFormatter(sysMsgType, formatter)

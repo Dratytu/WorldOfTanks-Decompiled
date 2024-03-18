0d5a6de258e82e3147fbab496f3e702e5e7c0daf
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:22 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/boosters/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/boosters/__init__.py
@@ -1,16 +1,27 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/boosters/__init__.py
+
+# Import necessary modules and functions
 from gui.Scaleform.daapi.settings.views import VIEW_ALIAS
 from gui.Scaleform.framework import ScopeTemplates, ComponentSettings
 
+# A function that returns a dictionary of context menu handlers
 def getContextMenuHandlers():
-    pass
+    pass  # Placeholder for context menu handlers
 
 
+# A function that returns a list of view settings
 def getViewSettings():
+    # Import the BoostersPanelComponent class locally to avoid circular imports
     from gui.Scaleform.daapi.view.lobby.boosters.BoostersPanelComponent import BoostersPanelComponent
-    return (ComponentSettings(VIEW_ALIAS.BOOSTERS_PANEL, BoostersPanelComponent, ScopeTemplates.DEFAULT_SCOPE),)
+
+    # Return a tuple containing view settings
+    return (
+        ComponentSettings(VIEW_ALIAS.BOOSTERS_PANEL, BoostersPanelComponent, ScopeTemplates.DEFAULT_SCOPE),
+    )
 
 
+# A function that returns a dictionary of business handlers
 def getBusinessHandlers():
-    pass
+    pass  # Placeholder for business handlers
+

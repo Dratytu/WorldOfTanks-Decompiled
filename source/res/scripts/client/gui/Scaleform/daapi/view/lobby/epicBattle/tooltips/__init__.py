0ab784b950e0687ca8d04e432e5ed5f9f4a04a85
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:50 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/epicBattle/tooltips/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/epicBattle/tooltips/__init__.py
@@ -1,3 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/epicBattle/tooltips/__init__.py
-pass
+
+# This is an empty __init__.py file for the 'tooltips' package within the
+# 'scripts/client/gui/Scaleform/daapi/view/lobby/epicBattle' module.
+# This file is necessary for Python to recognize this directory as a package.
+# Currently, this package does not contain any modules or classes.

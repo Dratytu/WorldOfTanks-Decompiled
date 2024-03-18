0c444aa556bb84471ded0c65787912e917c7c50a
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:18 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/EpicDeploymentMapMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/EpicDeploymentMapMeta.py
@@ -1,8 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/EpicDeploymentMapMeta.py
+
+# Import the EpicMinimapMeta class from the EpicMinimapMeta module
 from gui.Scaleform.daapi.view.meta.EpicMinimapMeta import EpicMinimapMeta
 
+# Define the EpicDeploymentMapMeta class, which inherits from the EpicMinimapMeta class
 class EpicDeploymentMapMeta(EpicMinimapMeta):
 
+    # Define the as_setMapDimensionsS method, which takes two arguments: widthPx and heightPx
     def as_setMapDimensionsS(self, widthPx, heightPx):
+        # Call the as_setMapDimensions method of the flashObject attribute, passing widthPx and heightPx as arguments
+        # If the _isDAAPIInited method returns True
         return self.flashObject.as_setMapDimensions(widthPx, heightPx) if self._isDAAPIInited() else None
+

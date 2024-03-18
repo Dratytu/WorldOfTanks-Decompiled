b464119f9086181555cdb256b50678093f0bfeb0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:38 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/impl/dialogs/sub_views/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/impl/dialogs/sub_views/__init__.py
@@ -1,3 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/impl/dialogs/sub_views/__init__.py
-pass
+
+# This is a blank file that serves as a placeholder for importing various
+# sub-views related to dialogs in the Battle Royale game mode.
+# Currently, there are no specific sub-views imported or defined in this file.
+
+# As the project progresses and more dialog sub-views are needed, this file
+# will be updated to include the necessary imports and code for those views.
+
+# By keeping these imports and definitions separate from the main dialog
+# code, the codebase remains organized and easier to navigate.
+
+# Example usage:
+# from battle_royale.gui.impl.dialogs.sub_views import SubView1, SubView2
+

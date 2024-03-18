da836f7cc5a80577550958b8f8fb27482dcae789
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:18 +0300

    comment

--- source/res/battle_modifiers/scripts/common/battle_modifiers_ext/battle_modifier/__init__.py
+++ source/res/battle_modifiers/scripts/common/battle_modifiers_ext/battle_modifier/__init__.py
@@ -1,3 +1,19 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/common/battle_modifiers_ext/battle_modifier/__init__.py
-pass
+
+# This is an empty __init__.py file for the battle_modifier module.
+# It serves to mark the directory as a package and allows for the importing
+# of submodules within this package.
+
+# The battle_modifier module is part of the battle_modifiers_ext package,
+# which is used for implementing various battle modifiers in the game.
+# This module does not contain any code by itself, but serves as a container
+# for other submodules that implement specific battle modifiers.
+
+# Example usage:
+# from battle_modifiers_ext.battle_modifier.some_specific_modifier import SomeSpecificModifier
+# modifier = SomeSpecificModifier()
+# modifier.apply()
+
+# Note: This code is written in Python 2.7, so it may require modifications
+# to be compatible with Python 3.

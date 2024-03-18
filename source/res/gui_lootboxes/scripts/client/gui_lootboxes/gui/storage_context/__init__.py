6107ecdfbbe122e36619ba863a0440cd96d325b8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:14 +0300

    comment

--- source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/storage_context/__init__.py
+++ source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/storage_context/__init__.py
@@ -1,3 +1,8 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: gui_lootboxes/scripts/client/gui_lootboxes/gui/storage_context/__init__.py
-pass
+
+# This is an empty __init__.py file for the gui_lootboxes.gui.storage_context package.
+# It serves to mark the directory as a package and can be used to include any
+# initialization code that should be executed when the package is imported.
+# In this case, there is no initialization code, and the package is simply used
+# to organize related modules and sub-packages.

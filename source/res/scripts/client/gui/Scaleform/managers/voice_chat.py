4f14f139f463b31664dd1ad70318d38af903a1d9
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:16 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/managers/voice_chat.py
+++ source/res/scripts/client/gui/Scaleform/managers/voice_chat.py
@@ -1,43 +1,87 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/managers/voice_chat.py
-from frameworks.wulf import WindowLayer
-from VOIP import getVOIPManager
-from messenger.proto.events import g_messengerEvents
-from gui.Scaleform.daapi.settings.views import VIEW_ALIAS
-from gui.shared.utils import getPlayerDatabaseID
-from gui import DialogsInterface
-from messenger.m_constants import PROTO_TYPE
-from messenger.proto import proto_getter
-from gui.Scaleform.framework.entities.abstract.VoiceChatManagerMeta import VoiceChatManagerMeta
+
+from frameworks.wulf import WindowLayer  # Importing WindowLayer from wulf framework
+from VOIP import getVOIPManager  # Importing getVOIPManager from VOIP module
+from messenger.proto.events import g_messengerEvents  # Importing g_messengerEvents from messenger.proto.events
+from gui.Scaleform.daapi.settings.views import VIEW_ALIAS  # Importing VIEW_ALIAS from daapi.settings.views
+from gui.shared.utils import getPlayerDatabaseID  # Importing getPlayerDatabaseID from shared.utils
+from gui import DialogsInterface  # Importing DialogsInterface from gui
+from messenger.m_constants import PROTO_TYPE  # Importing PROTO_TYPE from m_constants
+from messenger.proto import proto_getter  # Importing proto_getter from proto
+from gui.Scaleform.framework.entities.abstract.VoiceChatManagerMeta import VoiceChatManagerMeta  # Importing VoiceChatManagerMeta from framework.entities.abstract
+
 _MESSAGE_INIT_SUCCESS = 'voiceChatInitSucceded'
 _MESSAGE_INIT_FAILED = 'voiceChatInitFailed'
 
 class BaseVoiceChatManager(VoiceChatManagerMeta):
+    """
+    Base class for voice chat managers.
+    """
 
     def __init__(self, app):
+        """
+        Initialize the base voice chat manager.
+
+        :param app: The application instance.
+        """
         super(BaseVoiceChatManager, self).__init__()
         self.setEnvironment(app)
 
     @proto_getter(PROTO_TYPE.BW_CHAT2)
     def bwProto(self):
+        """
+        Get the BW_CHAT2 protocol object.
+
+        :return: The BW_CHAT2 protocol object.
+        """
         return None
 
     def isPlayerSpeaking(self, accountDBID):
+        """
+        Check if the player with the given accountDBID is speaking.
+
+        :param accountDBID: The account database ID of the player.
+        :return: True if the player is speaking, False otherwise.
+        """
         return self.bwProto.voipController.isPlayerSpeaking(accountDBID)
 
     def isVivox(self):
+        """
+        Check if the voice chat is Vivox.
+
+        :return: True if the voice chat is Vivox, False otherwise.
+        """
         return self.bwProto.voipController.isVivox()
 
     def isYY(self):
+        """
+        Check if the voice chat is YY.
+
+        :return: True if the voice chat is YY, False otherwise.
+        """
         return self.bwProto.voipController.isYY()
 
     def isVOIPEnabled(self):
+        """
+        Check if the voice chat is enabled.
+
+        :return: True if the voice chat is enabled, False otherwise.
+        """
         return self.bwProto.voipController.isVOIPEnabled()
 
     def isVOIPAvailable(self):
+        """
+        Check if the voice chat is available.
+
+        :return: True if the voice chat is available, False otherwise.
+        """
         return getVOIPManager().isChannelAvailable()
 
     def _populate(self):
+        """
+        Populate the voice chat manager with event listeners.
+        """
         super(BaseVoiceChatManager, self)._populate()
         voipEvents = g_messengerEvents.voip
         voipEvents.onVoiceChatInitSucceeded += self._showChatInitSuccessMessage
@@ -46,6 +90,9 @@ class BaseVoiceChatManager(VoiceChatManagerMeta):
         self.app.containerManager.onViewAddedToContainer += self.__onViewAddedToContainer
 
     def _dispose(self):
+        """
+        Dispose the voice chat manager and remove event listeners.
+        """
         voipEvents = g_messengerEvents.voip
         voipEvents.onVoiceChatInitSucceeded -= self._showChatInitSuccessMessage
         voipEvents.onVoiceChatInitFailed -= self._showChatInitErrorMessage
@@ -56,75 +103,50 @@ class BaseVoiceChatManager(VoiceChatManagerMeta):
         super(BaseVoiceChatManager, self)._dispose()
 
     def _onViewAdded(self, viewAlias):
+        """
+        Handle the event when a view is added to the container.
+
+        :param viewAlias: The alias of the added view.
+        """
         raise NotImplementedError
 
     def _showChatInitSuccessMessage(self):
+        """
+        Show the chat initialization success message.
+        """
         raise NotImplementedError
 
     def _showChatInitErrorMessage(self):
+        """
+        Show the chat initialization error message.
+        """
         raise NotImplementedError
 
     def _showDialog(self, key):
+        """
+        Show an info dialog with the given key.
+
+        :param key: The key of the dialog.
+        """
         DialogsInterface.showI18nInfoDialog(key, lambda result: None)
 
     def __onPlayerSpeaking(self, accountDBID, isSpeak):
+        """
+        Handle the event when a player starts or stops speaking.
+
+        :param accountDBID: The account database ID of the player.
+        :param isSpeak: True if the player started speaking, False otherwise.
+        """
         self.as_onPlayerSpeakS(accountDBID, isSpeak, accountDBID == getPlayerDatabaseID())
 
     def __onViewAddedToContainer(self, _, pyView):
+        """
+        Handle the event when a view is added to the container.
+
+        :param _: The event data (not used).
+        :param pyView: The added view.
+        """
         if pyView.layer == WindowLayer.VIEW:
             self._onViewAdded(pyView.alias)
 
 
-class LobbyVoiceChatManager(BaseVoiceChatManager):
-
-    def __init__(self, app):
-        super(LobbyVoiceChatManager, self).__init__(app)
-        self.__failedEventRaised = False
-        self.__pendingMessage = None
-        self.__enterToLobby = False
-        return
-
-    def _onViewAdded(self, viewAlias):
-        if viewAlias == VIEW_ALIAS.LOBBY:
-            self.__enterToLobby = True
-            if self.__pendingMessage is not None:
-                self._showDialog(self.__pendingMessage)
-                self.__pendingMessage = None
-        else:
-            self.__enterToLobby = False
-        return
-
-    def _showChatInitSuccessMessage(self):
-        if self.__failedEventRaised:
-            self.__failedEventRaised = False
-            self.__pendingMessage = None
-            if self.__enterToLobby:
-                self._showDialog(_MESSAGE_INIT_SUCCESS)
-        return
-
-    def _showChatInitErrorMessage(self):
-        if not self.__failedEventRaised:
-            self.__failedEventRaised = True
-            if self.__enterToLobby:
-                self._showDialog(_MESSAGE_INIT_FAILED)
-            else:
-                self.__pendingMessage = _MESSAGE_INIT_FAILED
-
-
-class BattleVoiceChatManager(BaseVoiceChatManager):
-
-    def __init__(self, app):
-        super(BattleVoiceChatManager, self).__init__(app)
-        self.__enteredToBattle = False
-        self.__failedEventRaised = False
-
-    def _onViewAdded(self, viewAlias):
-        self.__enteredToBattle = viewAlias in VIEW_ALIAS.BATTLE_PAGES
-
-    def _showChatInitSuccessMessage(self):
-        pass
-
-    def _showChatInitErrorMessage(self):
-        if self.__enteredToBattle and not self.__failedEventRaised:
-            self._showDialog(_MESSAGE_INIT_FAILED)
-            self.__failedEventRaised = True

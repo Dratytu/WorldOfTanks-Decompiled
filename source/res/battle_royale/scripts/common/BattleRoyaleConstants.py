957d2ef51d06767e45823c3a10ccaa34f3d73c46
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:26 +0300

    comment

--- source/res/battle_royale/scripts/common/BattleRoyaleConstants.py
+++ source/res/battle_royale/scripts/common/BattleRoyaleConstants.py
@@ -1,14 +1,20 @@
 # BattleRoyaleConstants.py
 # This module contains constant values used in the Battle Royale feature.
 
-# Constant for the test drive command in Battle Royale.
+# Constant for the test drive command in Battle Royale. This command allows
+# players to test drive a vehicle before purchasing it.
 CMD_BATTLE_ROYALE_TEST_DRIVE = 21001
 
-# Constant for the rent command in Battle Royale.
+# Constant for the rent command in Battle Royale. This command allows
+# players to rent a vehicle or other item for a set period of time.
 CMD_BATTLE_ROYALE_RENT = 21002
 
-# Constant for operating with BRCOIN in Battle Royale.
+# Constant for operating with BRCOIN in Battle Royale. BRCOIN is the virtual
+# currency used in the Battle Royale feature. This command allows players
+# to purchase items, rent vehicles, and perform other transactions.
 CMD_BATTLE_ROYALE_OPERATE_BRCOIN = 21003
 
-# Constant for renting all available items in Battle Royale.
+# Constant for renting all available items in Battle Royale. This command
+# allows players to rent all available items at once, rather than renting
+# them individually.
 CMD_BATTLE_ROYALE_RENT_ALL = 21004

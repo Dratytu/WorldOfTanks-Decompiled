41b20ff0a85c7a78121aa2e65a4a1ca14342f4ef
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:51 +0300

    comment

--- source/res/fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/common/__init__.py
+++ source/res/fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/common/__init__.py
@@ -1,3 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: fun_random/scripts/client/fun_random/gui/impl/gen/view_models/views/lobby/common/__init__.py
-pass
+
+# This is an empty Python module that is intended to serve as a container for
+# view models related to the lobby and common functionality in the fun_random
+# package. Currently, there are no view models defined in this module.
+
+# When view models are added to this module, they will typically include
+# class definitions that inherit from the `ViewModel` base class and define
+# various properties and methods to manage the state and behavior of user
+# interface elements.
+
+# By keeping related view models in a separate module like this, it can help
+# to organize the code and make it easier to find and reuse view models in
+# different parts of the application.

41f001ce703ed7ba8033616580108b8e27104372
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:28 +0300

    comment

--- source/res/battle_modifiers/scripts/client/battle_modifiers/gui/feature/__init__.py
+++ source/res/battle_modifiers/scripts/client/battle_modifiers/gui/feature/__init__.py
@@ -1,3 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/client/battle_modifiers/gui/feature/__init__.py
-pass
+
+# This is an empty __init__.py file for the battle_modifiers.gui.feature package.
+# It serves to prevent the package from being recognized as a namespace package.
+
+# A namespace package is a package that only contains other packages and no modules or executable files.
+# In this case, the battle_modifiers.gui.feature package contains modules and should not be treated
+# as a namespace package.
+
+# For more information on namespace packages, see the following link:
+# https://packaging.python.org/guides/packaging-namespace-packages/

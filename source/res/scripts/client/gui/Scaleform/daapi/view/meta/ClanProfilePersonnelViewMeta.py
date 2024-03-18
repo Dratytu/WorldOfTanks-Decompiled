3039749e63896b44bab13ec0fec42aa50b948aa6
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:36 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/ClanProfilePersonnelViewMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/ClanProfilePersonnelViewMeta.py
@@ -1,8 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/ClanProfilePersonnelViewMeta.py
+
+# Import the base view class for clan profile views
 from gui.Scaleform.daapi.view.lobby.clans.profile.ClanProfileBaseView import ClanProfileBaseView
 
-class ClanProfilePersonnelViewMeta(ClanProfileBaseView):
-
-    def as_getMembersDPS(self):
-        return self.flashObject.as_getMembersDP() if self._isDAAPIInited() else None

3d43fe87cb6951158cb46280d8470095ce9f1f18
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:51 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/carousel/data_provider.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/carousel/data_provider.py
@@ -1,5 +1,7 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/lobby/hangar/carousel/data_provider.py
+
+# Import necessary modules and classes
 from gui import GUI_NATIONS_ORDER_INDEX
 from gui.Scaleform.daapi.view.lobby.hangar.carousels.basic.carousel_data_provider import HangarCarouselDataProvider
 from gui.Scaleform.genConsts.TOOLTIPS_CONSTANTS import TOOLTIPS_CONSTANTS
@@ -11,24 +13,31 @@ from gui.shared.utils.functions import makeTooltip
 from gui.shared.utils.requesters import REQ_CRITERIA
 from helpers import dependency
 from skeletons.gui.game_control import IBattleRoyaleRentVehiclesController
+
+# Define an undefined vehicle type
 _UNDEFINED_VEHICLE_TYPE = 'undefined'
 
+# Subclass HangarCarouselDataProvider to create RoyaleCarouselDataProvider
 class RoyaleCarouselDataProvider(HangarCarouselDataProvider):
+    # Inject IBattleRoyaleRentVehiclesController as a dependency
     __rentVehiclesController = dependency.descriptor(IBattleRoyaleRentVehiclesController)
 
+    # Override getVehiclesIntCDs method
     def getVehiclesIntCDs(self):
         vehicledIntCDs = []
         for vehicle in self._vehicles:
             vehicledIntCDs.append(vehicle.intCD)
-
         return vehicledIntCDs
 
+    # Override _getAdditionalItemsIndexes method
     def _getAdditionalItemsIndexes(self):
         return []
 
+    # Override _setBaseCriteria method
     def _setBaseCriteria(self):
         self._baseCriteria = REQ_CRITERIA.INVENTORY | REQ_CRITERIA.VEHICLE.BATTLE_ROYALE
 
+    # Class method for comparing vehicles
     @classmethod
     def _vehicleComparisonKey(cls, vehicle):
         return (vehicle.getCustomState() == Vehicle.VEHICLE_STATE.UNSUITABLE_TO_QUEUE,
@@ -43,19 +52,29 @@ class RoyaleCarouselDataProvider(HangarCarouselDataProvider):
          tuple(vehicle.buyPrices.itemPrice.price.iterallitems(byWeight=True)),
          vehicle.userName)
 
+    # Method to check if telecom rentals are enabled
     def _isTelecomRentalsEnabled(self):
         return False
 
+    # Override _buildVehicle method
     def _buildVehicle(self, vehicle):
         result = super(RoyaleCarouselDataProvider, self)._buildVehicle(vehicle)
+
+        # Get vehicle state and rent state
         state, _ = vehicle.getState()
         if vehicle.isOnlyForBattleRoyaleBattles:
             rentState = self.__rentVehiclesController.getRentState(vehicle.intCD)
+
+            # Set test drive and rent-related properties
             isTestDriveEnabled = rentState == EquipmentPanelCmpRentStates.STATE_TEST_DRIVE_AVAILABLE
             rentLeft = self.__rentVehiclesController.getFormatedRentTimeLeft(vehicle.intCD)
             isRentActive = rentState in (EquipmentPanelCmpRentStates.STATE_TEST_DRIVE_ACTIVE, EquipmentPanelCmpRentStates.STATE_RENT_ACTIVE)
+
+            # Set background lock and rent availability properties
             isBgLocked = result.get('lockBackground', False)
             isRentAvailable = rentState in (EquipmentPanelCmpRentStates.STATE_TEST_DRIVE_AVAILABLE, EquipmentPanelCmpRentStates.STATE_RENT_AVAILABLE)
+
+            # Set vehicle properties based on the state
             vState, _ = vehicle.getState()
             result.update({'label': vehicle.shortUserName,
              'tooltip': TOOLTIPS_CONSTANTS.BATTLE_ROYALE_VEHICLE,
@@ -67,6 +86,8 @@ class RoyaleCarouselDataProvider(HangarCarouselDataProvider):
              'isTestDriveEnabled': isTestDriveEnabled,
              'lockBackground': isBgLocked or isRentAvailable,
              'rentLeft': rentLeft if isRentActive else ''})
+
+            # Set info-related properties if the vehicle is not in a battle
             if vState not in (Vehicle.VEHICLE_STATE.IN_PREBATTLE,
              Vehicle.VEHICLE_STATE.DAMAGED,
              Vehicle.VEHICLE_STATE.DESTROYED,
@@ -76,6 +97,5 @@ class RoyaleCarouselDataProvider(HangarCarouselDataProvider):
                  'smallInfoText': '',
                  'infoImgSrc': ''})
         elif state == Vehicle.VEHICLE_STATE.UNSUITABLE_TO_QUEUE:
-            result['lockedTooltip'] = makeTooltip(backport.text(R.strings.battle_royale.battleRoyaleCarousel.lockedToolTip.header()), backport.text(R.strings.battle_royale.battleRoyaleCarousel.lockedToolTip.body()))
-            result['clickEnabled'] = True
-        return result
+            # Set locked tooltip and click enabled properties if the vehicle is unsuitable for queue
+            result['lockedTooltip'] = makeTooltip(backport.text(R.strings.battle_royale.battleRoyaleCarousel.lockedToolTip.header()), backport.text(R.strings

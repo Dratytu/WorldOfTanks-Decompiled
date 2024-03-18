2c192de46bf9a0d12bfc1ccecc90cf3895c2d68b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:15 +0300

    comment

--- source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/tooltips/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/tooltips/__init__.py
@@ -1,3 +1,16 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/tooltips/__init__.py
-pass
+# Embedded file name: armory_yard/scripts/client/armory_yard/gui/impl/gen/view_models/views/lobby/feature/tooltips/<__init__.py>
+# This is a Python module that contains view models for lobby feature tooltips in the Armory Yard application.
+# The module does not contain any executable code, but instead defines classes that serve as data models for the
+# tooltips. These classes are used to generate code for the user interface using the MVC (Model-View-Controller)
+# design pattern.
+#
+# The module does not import any other modules, and does not contain any global variables or functions.
+# It only defines view model classes.
+#
+# Example usage:
+# from armory_yard.gui.impl.gen.view_models.views.lobby.feature.tooltips import TooltipViewModel
+# tooltip = TooltipViewModel()
+# tooltip.title = "My Tooltip"
+# tooltip.text = "This is the text of my tooltip."
+# tooltip.isShown = True

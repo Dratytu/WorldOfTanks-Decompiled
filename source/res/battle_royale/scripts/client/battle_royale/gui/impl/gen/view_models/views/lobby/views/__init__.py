12d948f92b1cea13d5b9867973d32b555bd6ffcb
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:39 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/views/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/views/__init__.py
@@ -1,3 +1,18 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/impl/gen/view_models/views/lobby/views/__init__.py
-pass
+
+# This is an empty Python module that serves as a container for view models
+# related to the lobby and its various views in the Battle Royale mode of a
+# hypothetical game. Currently, there are no view models defined in this module.
+
+# When view models are added to this module, they will likely include
+# information about the state of various elements in the lobby UI, such as
+# player status, available game modes, and in-game purchases. View models
+# are typically used to mediate between the UI and the underlying game
+# logic, allowing for a clear separation of concerns and easier testing.
+
+# Example usage:
+# from battle_royale.gui.impl.gen.view_models.views.lobby.views import some_view_model
+# view_model = some_view_model()
+# view_model.some_attribute = some_value
+# some_view.set_view_model(view_model)

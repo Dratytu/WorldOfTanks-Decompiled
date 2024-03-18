6d4323fd0ac4e3f3d41e2ae02fbb256e45a23e89
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:18 +0300

    comment

--- source/res/scripts/client/Proximity.py
+++ source/res/scripts/client/Proximity.py
@@ -1,6 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/Proximity.py
+
+# Import the DynamicScriptComponent class from the script_component module
 from script_component.DynamicScriptComponent import DynamicScriptComponent
 
+# Define the Proximity class, which inherits from the DynamicScriptComponent class
 class Proximity(DynamicScriptComponent):
+    # Pass (i.e., do nothing) in the constructor of the Proximity class
     pass
+

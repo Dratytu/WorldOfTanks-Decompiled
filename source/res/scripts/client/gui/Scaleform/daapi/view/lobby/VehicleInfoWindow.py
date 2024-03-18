5be84fa79fe448d958933d7a8622f4e9b24e8cb8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:11 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/VehicleInfoWindow.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/VehicleInfoWindow.py
@@ -1,5 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/VehicleInfoWindow.py
+
 import typing
 from debug_utils import LOG_ERROR
 from gui.impl import backport
@@ -23,43 +24,72 @@ from gui.shared.gui_items.items_actions import factory as ItemsActionsFactory
 from account_helpers import AccountSettings
 from account_helpers.AccountSettings import NATION_CHANGE_VIEWED
 if typing.TYPE_CHECKING:
-    from account_helpers.settings_core.ServerSettingsManager import ServerSettingsManager
+    from account_helpers.settings_core.ServerSettingsManager import ServerSettingsManager  # NoQA
+
 
 class _Highlight(object):
+    """
+    A helper class to highlight specific parameters of a vehicle.
+    """
     __slots__ = ('__checker', '__highlight')
 
     def __init__(self, checker):
+        """
+        Initialize the _Highlight object with a checker function.
+
+        :param checker: A function that checks if the parameter should be highlighted.
+        """
         super(_Highlight, self).__init__()
         self.__highlight = None
         self.__checker = checker
         return
 
     def __nonzero__(self):
+        """
+        Check if the parameter should be highlighted.
+
+        :return: True if the parameter should be highlighted, False otherwise.
+        """
         if self.__highlight is None:
             self.__highlight = self.__checker()
         return self.__highlight
 
 
 def _highlightsMap(settings, vehicle=None):
-    config = (((AUTO_RELOAD_PROP_NAME,), _Highlight(lambda : settings.checkAutoReloadHighlights(increase=True))),
-     ((TURBOSHAFT_ENGINE_POWER,
+    """
+    Create a mapping of parameters and their highlight status.
+
+    :param settings: The settings to use for highlighting.
+    :param vehicle: The vehicle to check for dual accuracy.
+    :return: A dictionary of parameter names and their corresponding _Highlight objects.
+    """
+    config = (    (    (AUTO_RELOAD_PROP_NAME,), _Highlight(lambda : settings.checkAutoReloadHighlights(increase=True))),
+     (    (TURBOSHAFT_ENGINE_POWER,
        TURBOSHAFT_SPEED_MODE_SPEED,
        TURBOSHAFT_SWITCH_TIME,
        TURBOSHAFT_INVISIBILITY_STILL_FACTOR,
        TURBOSHAFT_INVISIBILITY_MOVING_FACTOR), _Highlight(lambda : settings.checkTurboshaftHighlights(increase=True))),
-     ((ROCKET_ACCELERATION_ENGINE_POWER, ROCKET_ACCELERATION_SPEED_LIMITS, ROCKET_ACCELERATION_REUSE_AND_DURATION), _Highlight(lambda : settings.checkRocketAccelerationHighlights(increase=True))),
-     ((DUAL_ACCURACY_COOLING_DELAY, SHOT_DISPERSION_ANGLE), _Highlight(lambda : vehicle.descriptor.hasDualAccuracy and settings.checkDualAccuracyHighlights(increase=True))))
+     (    (ROCKET_ACCELERATION_ENGINE_POWER, ROCKET_ACCELERATION_SPEED_LIMITS, ROCKET_ACCELERATION_REUSE_AND_DURATION), _Highlight(lambda : settings.checkRocketAccelerationHighlights(increase=True))),
+     (    (DUAL_ACCURACY_COOLING_DELAY, SHOT_DISPERSION_ANGLE), _Highlight(lambda : vehicle.descriptor.hasDualAccuracy and settings.checkDualAccuracyHighlights(increase=True))))
     mapping = [ zip(params, [highlight] * len(params)) for params, highlight in config ]
     return dict([ item for sub in mapping for item in sub ])
 
 
 class VehicleInfoWindow(VehicleInfoMeta):
+    """
+    The VehicleInfoWindow class is responsible for displaying vehicle information in a pop-up window.
+    """
     _itemsCache = dependency.descriptor(IItemsCache)
     _comparisonBasket = dependency.descriptor(IVehicleComparisonBasket)
     _settingsCore = dependency.descriptor(ISettingsCore)
     _lobbyContext = dependency.instance(ILobbyContext)
 
     def __init__(self, ctx=None):
+        """
+        Initialize the VehicleInfoWindow with a vehicle compact descriptor.
+
+        :param ctx: The context dictionary that may contain the vehicle compact descriptor.
+        """
         super(VehicleInfoWindow, self).__init__()
         self.__vehicleCompactDescr = ctx.get('vehicleCompactDescr', 0)
         serverSettings = self._settingsCore.serverSettings
@@ -67,109 +97,11 @@ class VehicleInfoWindow(VehicleInfoMeta):
         self.__highlightsMap = _highlightsMap(serverSettings, vehicle)
 
     def onCancelClick(self):
+        """
+        Close the window when the cancel button is clicked.
+        """
         self.destroy()
 
     def onWindowClose(self):
-        self.destroy()
-
-    def getVehicleInfo(self):
-        vehicle = self._itemsCache.items.getItemByCD(self.__vehicleCompactDescr)
-        if vehicle is None:
-            LOG_ERROR('There is error while showing vehicle info window: ', self.__vehicleCompactDescr)
-            return
-        else:
-            params = vehicle.getParams()
-            tankmenParams = list()
-            skillsConfig = tankmen.getSkillsConfig()
-            for slotIdx, tankman in vehicle.crew:
-                role = vehicle.descriptor.type.crewRoles[slotIdx][0]
-                tankmanLabel = ''
-                if tankman is not None:
-                    lastUserName = tankman.lastUserName if tankman.lastUserName else tankman.firstUserName
-                    if tankman.skinID != NO_CREW_SKIN_ID:
-                        skinItem = self._itemsCache.items.getCrewSkin(tankman.skinID)
-                        lastUserName = i18n.makeString(skinItem.getLastName())
-                    tankmanLabel = '%s %s (%d%%)' % (tankman.rankUserName, lastUserName, tankman.roleLevel)
-                tankmenParams.append({'tankmanType': i18n.convert(skillsConfig.getSkill(role).userString),
-                 'value': tankmanLabel})
-
-            roleStr = getRoleTextWithLabel(vehicle.role, vehicle.roleLabel)
-            paramsList = formatters.getFormattedParamsList(vehicle.descriptor, params['parameters'], excludeRelative=True)
-            info = {'vehicleName': vehicle.longUserName,
-             'vehicleDescription': vehicle.fullDescription,
-             'vehicleImage': vehicle.icon,
-             'vehicleLevel': vehicle.level,
-             'vehicleNation': vehicle.nationID,
-             'vehicleElite': vehicle.isElite,
-             'vehicleType': vehicle.type,
-             'propsData': self.__packParams(paramsList),
-             'baseData': params['base'],
-             'crewData': tankmenParams,
-             'roleStr': roleStr}
-            self.as_setVehicleInfoS(info)
-            return
-
-    def addToCompare(self):
-        self._comparisonBasket.addVehicle(self.__vehicleCompactDescr)
-
-    def changeNation(self):
-        vehicle = self._itemsCache.items.getItemByCD(self.__vehicleCompactDescr)
-        vehCD = vehicle.intCD if vehicle.activeInNationGroup else iterVehTypeCDsInNationGroup(vehicle.intCD).next()
-        ItemsActionsFactory.doAction(ItemsActionsFactory.CHANGE_NATION, vehCD)
-        self.destroy()
-
-    def _populate(self):
-        super(VehicleInfoWindow, self)._populate()
-        self._comparisonBasket.onChange += self.__onVehCompareBasketChanged
-        self._comparisonBasket.onSwitchChange += self.__updateCompareButtonState
-        self._itemsCache.onSyncCompleted += self.__updateNationChangeBtn
-        self._lobbyContext.getServerSettings().onServerSettingsChange += self.__updateNationChangeBtn
-        self.__updateCompareButtonState()
-        self.__updateChangeNationButtonState()
-
-    def _dispose(self):
-        self._comparisonBasket.onSwitchChange -= self.__updateCompareButtonState
-        self._comparisonBasket.onChange -= self.__onVehCompareBasketChanged
-        self._itemsCache.onSyncCompleted -= self.__updateNationChangeBtn
-        self._lobbyContext.getServerSettings().onServerSettingsChange -= self.__updateNationChangeBtn
-        super(VehicleInfoWindow, self)._dispose()
-
-    def __packParams(self, paramsList):
-        result = []
-        for name, value in paramsList:
-            paramVO = {'name': name,
-             'value': value}
-            if self.__highlightsMap.get(name, False):
-                paramVO['highlight'] = True
-            result.append(paramVO)
-
-        return result
-
-    def __updateCompareButtonState(self):
-        if not self._comparisonBasket.isAvailable():
-            tooltip = VEH_COMPARE.COMPAREVEHICLEBTN_TOOLTIPS_MINICLIENT
-        elif self._comparisonBasket.isFull():
-            tooltip = MENU.VEHICLEINFO_COMPAREBTN_TOOLTIP
-        else:
-            tooltip = ''
-        self.as_setCompareButtonDataS({'visible': self._comparisonBasket.isEnabled(),
-         'enabled': self._comparisonBasket.isReadyToAdd(self._itemsCache.items.getItemByCD(self.__vehicleCompactDescr)),
-         'label': MENU.VEHICLEINFO_COMPAREBTN_LABEL,
-         'tooltip': tooltip})
-
-    def __onVehCompareBasketChanged(self, changedData):
-        if changedData.isFullChanged:
-            self.__updateCompareButtonState()
-
-    def __updateChangeNationButtonState(self):
-        vehicle = self._itemsCache.items.getItemByCD(self.__vehicleCompactDescr)
-        enabled = vehicle.isNationChangeAvailable
-        tooltip = getChangeNationTooltip(vehicle)
-        self.as_setChangeNationButtonDataS({'visible': vehicle.hasNationGroup and vehicle.isInInventory,
-         'enabled': enabled,
-         'label': backport.text(R.strings.menu.vehicleInfo.nationChangeBtn.label()),
-         'isNew': not AccountSettings.getSettings(NATION_CHANGE_VIEWED),
-         'tooltip': tooltip})
-
-    def __updateNationChangeBtn(self, *args, **kwargs):
-        self.__updateChangeNationButtonState()
+        """
+        Close the window when it is

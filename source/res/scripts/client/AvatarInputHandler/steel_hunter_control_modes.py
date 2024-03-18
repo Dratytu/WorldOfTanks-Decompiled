0ce1ee0651f1283d7f190e80f1d43d835b2b83de
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:16 +0300

    comment

--- source/res/scripts/client/AvatarInputHandler/steel_hunter_control_modes.py
+++ source/res/scripts/client/AvatarInputHandler/steel_hunter_control_modes.py
@@ -1,8 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/AvatarInputHandler/steel_hunter_control_modes.py
+
+# Importing the PostMortemControlMode class from the control_modes module.
 from AvatarInputHandler.control_modes import PostMortemControlMode
 
-class SHPostMortemControlMode(PostMortemControlMode):
-
-    def _isPostmortemDelayEnabled(self):
-        return False

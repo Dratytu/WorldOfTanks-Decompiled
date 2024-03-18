43ff68595022797d73476a9007c503bc7c793261
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:49 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/image_view/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/image_view/__init__.py
@@ -1,3 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/image_view/__init__.py
-pass
+
+# This is an empty __init__.py file for the image_view package in the
+# daapi.view.lobby module of the client's GUI.
+# The purpose of this file is to serve as a placeholder and enable the
+# creation of a package in Python.
+
+# The image_view package is likely responsible for handling the display
+# of various images within the game's lobby, such as user profile pictures,
+# clan logos, or in-game item previews. However, based on the contents of
+# this file, it appears that there are no classes or functions defined here.
+# Any functionality related to image display would likely be implemented
+# in separate modules or scripts within this package.

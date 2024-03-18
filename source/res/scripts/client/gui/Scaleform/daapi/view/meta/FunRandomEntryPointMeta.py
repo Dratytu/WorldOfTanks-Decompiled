91776c1ebbb2bd8b3bbb0c2bc0eea8b15154e7dc
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:03:36 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/FunRandomEntryPointMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/FunRandomEntryPointMeta.py
@@ -1,6 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/FunRandomEntryPointMeta.py
-from gui.Scaleform.framework.entities.inject_component_adaptor import InjectComponentAdaptor
+
+from gui.Scaleform.framework.entities.inject_component_adaptor import InjectComponentAdaptor  # Importing InjectComponentAdaptor from the gui.Scaleform.framework.entities module
 
 class FunRandomEntryPointMeta(InjectComponentAdaptor):
+    """
+    Meta class for the FunRandomEntryPoint view, inherited from InjectComponentAdaptor.
+    This class does not contain any implemented methods or properties,
+    and it serves as a base for creating and configuring the FunRandomEntryPoint view.
+    """
     pass
+

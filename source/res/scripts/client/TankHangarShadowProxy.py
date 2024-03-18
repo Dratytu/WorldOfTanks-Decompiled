cbacc5543c18ab4c51bbbb70c582a509dcb53af6
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:16 +0300

    comment

--- source/res/scripts/client/TankHangarShadowProxy.py
+++ source/res/scripts/client/TankHangarShadowProxy.py
@@ -1,8 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/TankHangarShadowProxy.py
+
 import BigWorld
 
+# This is the main class for creating a Tank Hangar Shadow Proxy object
 class TankHangarShadowProxy(BigWorld.UserDataObject):
 
+    # Initializes the Tank Hangar Shadow Proxy object
     def __init__(self):
+        # Calls the constructor of the UserDataObject class
         BigWorld.UserDataObject.__init__(self)

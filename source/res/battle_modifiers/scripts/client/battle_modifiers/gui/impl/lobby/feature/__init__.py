8a34076ad87828bc7c8c2fbf665c62f11326eec0
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:31 +0300

    comment

--- source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/lobby/feature/__init__.py
+++ source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/lobby/feature/__init__.py
@@ -1,3 +1,13 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/client/battle_modifiers/gui/impl/lobby/feature/__init__.py
-pass
+
+# This is an empty __init__.py file for the 'battle_modifiers.gui.impl.lobby.feature' package.
+# Its purpose is to signal to Python that this directory should be treated as a package,
+# allowing it to import modules and sub-packages using dotted notation.
+
+# Since this file does not contain any code, it will not have any direct impact on the
+# behavior of the application. However, it is an essential part of the package's
+# organization and structure.
+
+# By including this comment, other developers can quickly understand the purpose of
+# this file and its role within the larger codebase.

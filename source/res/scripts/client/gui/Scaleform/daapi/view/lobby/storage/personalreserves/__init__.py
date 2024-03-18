7d26d82ac39b699e53d0e18f5931da10a07e6731
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:17 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/lobby/storage/personalreserves/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/lobby/storage/personalreserves/__init__.py
@@ -1,3 +1,12 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
+# This is a Python script for the gui/Scaleform/daapi/view/lobby/storage/personalreserves package.
+
+# The script does not contain any executable code, but rather serves as a placeholder or import module.
+
+# It is possible that other modules or classes within the application import functions or classes
+# from this module, or that this module is used to organize the code in a logical and modular way.
+
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/lobby/storage/personalreserves/__init__.py
 pass
+

0985d11d9c47e96b93a928ec72239e2cdd792258
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:30 +0300

    comment

--- source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/__init__.py
+++ source/res/battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/__init__.py
@@ -1,3 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_modifiers/scripts/client/battle_modifiers/gui/impl/gen/__init__.py
-pass
+# This is an empty __init__.py file, which is used to indicate that this directory
+# should be treated as a package in Python. In this case, the package is
+# 'battle_modifiers.gui.impl.gen'. An empty __init__.py file is necessary for
+# a directory to be recognized as a package, and can contain initialization
+# code if needed. However, in this case, no initialization code is required.
+
+# Author: [Your Name]
+# Created: [Creation Date]
+# Last Updated: [Last Update Date]

58da25d00285b75b9e4eacd452a32e33e71dadd5
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:36 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/constants.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/constants.py
@@ -1,83 +1,88 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/constants.py
+
+# The number of post-battle rewards in Battle Royale mode
 ROYALE_POSTBATTLE_REWARDS_COUNT = 8
 
+# A class representing various equipments in Battle Royale mode
 class BattleRoyaleEquipments(object):
+    # A large repair kit for Battle Royale mode
     LARGE_REPAIRKIT = 'large_repairkit_battle_royale'
+    
+    # Afterburning equipment for Battle Royale mode
     AFTER_BURNING = 'afterburning_battle_royale'
+    
+    # Regeneration kit equipment for Battle Royale mode
     REGENERATION_KIT = 'regenerationKit'
+    
+    # Self buff equipment for Battle Royale mode
     SELF_BUFF = 'selfBuff'
+    
+    # Trap point equipment for Battle Royale mode
     TRAP_POINT = 'trappoint'
+    
+    # Repair point equipment for Battle Royale mode
     REPAIR_POINT = 'repairpoint'
+    
+    # Heal point equipment for Battle Royale mode
     HEAL_POINT = 'healPoint'
+    
+    # Smoke equipment for Battle Royale mode
     SMOKE = 'smoke_battle_royale'
+    
+    # Bomber equipment for Battle Royale mode
     BOMBER = 'arcade_bomber_battle_royale'
+    
+    # Kamikaze equipment for Battle Royale mode
     KAMIKAZE = 'spawn_kamikaze'
+    
+    # Berserker equipment for Battle Royale mode
     BERSERKER = 'berserker'
+    
+    # Mine field equipment for Battle Royale mode
     MINE_FIELD = 'arcade_minefield_battle_royale'
+    
+    # Smoke with damage equipment for Battle Royale mode
     SMOKE_WITH_DAMAGE = 'arcade_smoke_battle_royale_with_damage'
+    
+    # Regular smoke equipment for Battle Royale mode
     ARCADE_SMOKE = 'arcade_smoke_battle_royale'
+    
+    # Fire circle equipment for Battle Royale mode
     FIRE_CIRCLE = 'fireCircle'
+    
+    # Corroding shot equipment for Battle Royale mode
     CORRODING_SHOT = 'corrodingShot'
+    
+    # Cling brander equipment for Battle Royale mode
     CLING_BRANDER = 'clingBrander'
+    
+    # Adaptation health restore equipment for Battle Royale mode
     ADAPTATION_HEALTH_RESTORE = 'adaptationHealthRestore'
+    
+    # Thunder strike equipment for Battle Royale mode
     THUNDER_STRIKE = 'thunderStrike'
+    
+    # Shot passion equipment for Battle Royale mode
     SHOT_PASSION = 'shotPassion'
 
-
+# A class representing various components in Battle Royale mode
 class BattleRoyaleComponents(object):
+    # Shot passion component for Battle Royale mode
     SHOT_PASSION = 'shotPassionComponent'
+    
+    # Fire circle component for Battle Royale mode
     FIRE_CIRCLE = 'vehicleFireCircleEffectComponent'
 
-
+# A frozenset containing all equipments that have messages associated with them
 BR_EQUIPMENTS_WITH_MESSAGES = frozenset([BattleRoyaleEquipments.TRAP_POINT,
  BattleRoyaleEquipments.AFTER_BURNING,
  BattleRoyaleEquipments.REGENERATION_KIT,
  BattleRoyaleEquipments.SELF_BUFF,
- BattleRoyaleEquipments.HEAL_POINT,
+ BattleRoyments.HEAL_POINT,
  BattleRoyaleEquipments.LARGE_REPAIRKIT,
  BattleRoyaleEquipments.BERSERKER,
  BattleRoyaleEquipments.MINE_FIELD,
  BattleRoyaleEquipments.REPAIR_POINT,
  BattleRoyaleEquipments.BOMBER,
- BattleRoyaleEquipments.KAMIKAZE,
- BattleRoyaleEquipments.SMOKE,
- BattleRoyaleEquipments.SMOKE_WITH_DAMAGE,
- BattleRoyaleEquipments.ARCADE_SMOKE,
- BattleRoyaleEquipments.FIRE_CIRCLE,
- BattleRoyaleEquipments.CORRODING_SHOT,
- BattleRoyaleEquipments.CLING_BRANDER,
- BattleRoyaleEquipments.ADAPTATION_HEALTH_RESTORE,
- BattleRoyaleEquipments.THUNDER_STRIKE,
- BattleRoyaleEquipments.SHOT_PASSION])
-
-class AmmoTypes(object):
-    BASIC_SHELL = 'bshell'
-    PREMIUM_SHELL = 'pshell'
-    ITEM = 'item'
-    CHARGE1 = 'charge1'
-    CHARGE2 = 'charge2'
-    CHARGE3 = 'charge3'
-    CHARGE4 = 'charge4'
-    CHARGES = (CHARGE1,
-     CHARGE2,
-     CHARGE3,
-     CHARGE4)
-    SHELLS = (BASIC_SHELL, PREMIUM_SHELL)
-
-
-class BattleRoyalePerfProblems(object):
-    HIGH_RISK = 1
-    MEDIUM_RISK = 2
-    LOW_RISK = 3
-
-
-class ParamTypes(object):
-    DELTA = 'delta'
-    CONST = 'const'
-    SIMPLE = 'simple'
-
-
-BR_QUEST_ID_PREFIX = 'token:br:title'
-BATTLE_ROYALE_VEHICLES_INVOICE = 'battle_royale_vehicles_invoice'
-BR_COIN = 'brcoin'
+ BattleRoyaleEquipments.KAMI

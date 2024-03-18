fe22de601e5bd472b50904fcf3f6d325d00720dd
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:55 +0300

    comment

--- source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/lobby/__init__.py
+++ source/res/gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/lobby/__init__.py
@@ -1,3 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/lobby/__init__.py
-pass
+
+# This is an empty __init__.py file for the gui_lootboxes/scripts/client/gui_lootboxes/gui/impl/lobby package.
+# An empty __init__.py file is required for a directory to be recognized as a package in Python.
+
+# The lobby package contains various modules related to implementing the lobby functionality for the GUI Lootboxes application.
+# The specific contents of this package are not defined in this file, but rather in the individual module files contained within.
+
+# For more information on package structure and implementation in Python, please see:
+# https://docs.python.org/2/tutorial/modules.html#packages

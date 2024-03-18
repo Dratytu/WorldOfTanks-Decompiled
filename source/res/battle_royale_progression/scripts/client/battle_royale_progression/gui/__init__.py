bd77c4581e650ffdb4d2014f9481177b7593d12b
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:45 +0300

    comment

--- source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/__init__.py
+++ source/res/battle_royale_progression/scripts/client/battle_royale_progression/gui/__init__.py
@@ -1,3 +1,14 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale_progression/scripts/client/battle_royale_progression/gui/__init__.py
-pass
+
+# This is an empty __init__.py file for the battle_royale_progression/gui package.
+# In Python, __init__.py files are used to initialize a package, making it possible
+# for the package to be imported by other modules.
+
+# Since this file is empty, it suggests that the package does not require any
+# specific initialization at the moment. However, as the package grows and
+# additional modules are added, this file might be used to import necessary
+# sub-modules or to set any necessary package-level variables or configurations.
+
+# For more information on Python packages and __init__.py files, please refer to:
+# https://docs.python.org/3/tutorial/modules.html#packages

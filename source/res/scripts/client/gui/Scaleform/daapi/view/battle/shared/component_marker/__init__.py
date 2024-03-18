683efbaffd125785268e7325565e9e8aadeacb1d
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:45 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/battle/shared/component_marker/__init__.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/battle/shared/component_marker/__init__.py
@@ -1,3 +1,15 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
+
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/battle/shared/component_marker/__init__.py
-pass
+
+# This is an empty __init__.py file for the component_marker package.
+# The purpose of this file is to prevent the package from being recognized as a package by Python,
+# which would cause issues when importing modules from this package.
+
+# By creating an empty __init__.py file, we ensure that the component_marker directory is treated
+# as a package by Python, allowing us to import its modules using the standard dot notation.
+
+# For example, if there was a module named 'marker.py' in this package,
+# we could import it in another Python script like this:
+# from gui.Scaleform.daapi.view.battle.shared.component_marker import marker
+

fd73f21fbc254c0789cca09f59c3344529730117
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:16 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/daapi/view/meta/MissionsPremiumViewMeta.py
+++ source/res/scripts/client/gui/Scaleform/daapi/view/meta/MissionsPremiumViewMeta.py
@@ -1,6 +1,11 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/gui/Scaleform/daapi/view/meta/MissionsPremiumViewMeta.py
+
+# Import the base metaclass for mission views
 from gui.Scaleform.daapi.view.meta.MissionsViewBaseMeta import MissionsViewBaseMeta
 
+# Define the MissionsPremiumViewMeta class, which inherits from MissionsViewBaseMeta
 class MissionsPremiumViewMeta(MissionsViewBaseMeta):
+    # The class body is empty, indicating that there are no additional methods or attributes specific to this metaclass
     pass
+

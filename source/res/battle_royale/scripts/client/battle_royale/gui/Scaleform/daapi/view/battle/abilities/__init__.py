b680a94524907c4d00ec70a58fb74f8cb22709c3
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:17 +0300

    comment

--- source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/abilities/__init__.py
+++ source/res/battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/abilities/__init__.py
@@ -1,3 +1,9 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/battle_royale/gui/Scaleform/daapi/view/battle/abilities/__init__.py
-pass
+
+# This script defines the module for battle Royale abilities in the Scaleform
+# daapi view for battle.
+
+# The file does not contain any code, and is likely a placeholder or a
+# container for related modules.
+

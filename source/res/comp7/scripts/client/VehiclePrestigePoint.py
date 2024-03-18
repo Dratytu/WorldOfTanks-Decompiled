7bf7ff73e9d10d9a0a544cf96c95e7c6bff0d1f8
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:14:54 +0300

    comment

--- source/res/comp7/scripts/client/VehiclePrestigePoint.py
+++ source/res/comp7/scripts/client/VehiclePrestigePoint.py
@@ -1,14 +1,35 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: comp7/scripts/client/VehiclePrestigePoint.py
-import BigWorld
+
+import BigWorld # Importing BigWorld module to inherit from DynamicScriptComponent
 
 class VehiclePrestigePoint(BigWorld.DynamicScriptComponent):
+    """
+    This class, VehiclePrestigePoint, inherits from BigWorld's DynamicScriptComponent class.
+    It represents a vehicle's prestige point component.
+    """
 
     def __init__(self):
-        BigWorld.DynamicScriptComponent.__init__(self)
+        """
+        Initializes the VehiclePrestigePoint class.
+        """
+        BigWorld.DynamicScriptComponent.__init__(self) # Calling the constructor of the parent class
 
     def onEnterWorld(self, *args):
+        """
+        This method is called when the vehicle enters the world.
+        It currently has no implementation.
+
+        :param args: Variable length argument list
+        """
         pass
 
     def onLeaveWorld(self, *args):
+        """
+        This method is called when the vehicle leaves the world.
+        It currently has no implementation.
+
+        :param args: Variable length argument list
+        """
         pass
+

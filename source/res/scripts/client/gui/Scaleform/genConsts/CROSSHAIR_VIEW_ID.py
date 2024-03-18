55ee55c07878d1f7e42a9f55b99c14ce326f646c
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:04:01 +0300

    comment

--- source/res/scripts/client/gui/Scaleform/genConsts/CROSSHAIR_VIEW_ID.py
+++ source/res/scripts/client/gui/Scaleform/genConsts/CROSSHAIR_VIEW_ID.py
@@ -1,10 +1,25 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
-# Embedded file name: scripts/client/gui/Scaleform/genConsts/CROSSHAIR_VIEW_ID.py
 
+# This script defines a class named CROSSHAIR_VIEW_ID
+# which contains constants used to identify different crosshair views.
 
 class CROSSHAIR_VIEW_ID(object):
+    # The UNDEFINED constant is used when the crosshair view is not defined or
+    # not yet set. It has a value of 0.
     UNDEFINED = 0
+
+    # The ARCADE constant is used to identify the standard crosshair view
+    # used in arcade mode. It has a value of 1.
     ARCADE = 1
+
+    # The SNIPER constant is used to identify the crosshair view used in
+    # sniper mode. It has a value of 2.
     SNIPER = 2
+
+    # The STRATEGIC constant is used to identify the crosshair view used in
+    # strategic mode. It has a value of 3.
     STRATEGIC = 3
+
+    # The POSTMORTEM constant is used to identify the crosshair view used in
+    # postmortem mode. It has a value of 4.
     POSTMORTEM = 4

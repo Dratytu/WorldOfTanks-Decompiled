9601a6d576de7cf806f1a28dc938608d0f8ad4d3
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:15:32 +0300

    comment

--- source/res/scripts/client/CircularFlyer.py
+++ source/res/scripts/client/CircularFlyer.py
@@ -1,5 +1,6 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: scripts/client/CircularFlyer.py
+
 import math
 import BigWorld
 import AnimationSequence
@@ -9,97 +10,132 @@ from vehicle_systems.stricted_loading import makeCallbackWeak
 import SoundGroups
 
 class CircularFlyer(BigWorld.UserDataObject):
+    """
+    A class that represents a flyer object moving in a circular path.
+    """
 
     def __init__(self):
+        """
+        Initializes a new instance of the CircularFlyer class.
+        """
         BigWorld.UserDataObject.__init__(self)
-        self.__prevTime = BigWorld.time()
-        self.__angularVelocity = 2 * math.pi / self.rotationPeriod
-        if not self.rotateClockwise:
-            self.__angularVelocity *= -1
-        self.__currentAngle = 0.0
-        self.__updateCallbackId = None
-        self.__model = None
-        self.__modelMatrix = None
-        self.__sound = None
-        self.__animator = None
-        BigWorld.loadResourceListBG((self.modelName, self.pixieName), makeCallbackWeak(self.__onResourcesLoaded))
-        return
+        self.__prev_time = BigWorld.time()  # Previous time for calculating delta time.
+        self.__angular_velocity = 2 * math.pi / self.rotation_period  # Angular velocity of the flyer.
+        if not self.rotate_clockwise:
+            self.__angular_velocity *= -1  # If rotating counter-clockwise, negate the angular velocity.
+        self.__current_angle = 0.0  # Current angle of the flyer in radians.
+        self.__update_callback_id = None  # Callback ID for updating the flyer's position.
+        self.__model = None  # The 3D model of the flyer.
+        self.__model_matrix = None  # The transformation matrix of the flyer's model.
+        self.__sound = None  # The sound associated with the flyer.
+        self.__animator = None  # The animation controller for the flyer's model.
+
+        # Load the flyer's resources in the background.
+        BigWorld.loadResourceListBG((self.model_name, self.pixie_name), makeCallbackWeak(self.__on_resources_loaded))
 
     def __del__(self):
+        """
+        Cleans up the CircularFlyer instance.
+        """
         self.__clear()
 
     def __clear(self):
-        if self.__updateCallbackId is not None:
-            BigWorld.cancelCallback(self.__updateCallbackId)
-        self.__updateCallbackId = None
+        """
+        Clears all resources associated with the CircularFlyer instance.
+        """
+        if self.__update_callback_id is not None:
+            BigWorld.cancelCallback(self.__update_callback_id)
+        self.__update_callback_id = None
+
         if self.__sound is not None:
             self.__sound.stop()
             self.__sound.releaseMatrix()
             self.__sound = None
+
         if self.__model is not None:
             self.__animator = None
             BigWorld.delModel(self.__model)
             self.__model = None
-        return
 
-    def __onResourcesLoaded(self, resourceRefs):
+    def __on_resources_loaded(self, resource_refs):
+        """
+        Called when the flyer's resources are loaded.
+
+        :param resource_refs: A dictionary containing the loaded resources.
+        """
         if self.guid not in BigWorld.userDataObjects:
             return
-        else:
-            self.__clear()
-            if self.modelName in resourceRefs.failedIDs:
-                return
-            try:
-                self.__model = resourceRefs[self.modelName]
-                self.__modelMatrix = Matrix()
-                self.__modelMatrix.setIdentity()
-                servo = BigWorld.Servo(self.__modelMatrix)
-                self.__model.addMotor(servo)
-                BigWorld.addModel(self.__model)
-                if self.actionName != '':
-                    clipResource = self.__model.deprecatedGetAnimationClipResource(self.actionName)
-                    if clipResource:
-                        spaceID = BigWorld.player().spaceID
-                        loader = AnimationSequence.Loader(clipResource, spaceID)
-                        animator = loader.loadSync()
-                        animator.bindTo(AnimationSequence.ModelWrapperContainer(self.__model, spaceID))
-                        animator.start()
-                        self.__animator = animator
-                if self.pixieName != '' and self.pixieName not in resourceRefs.failedIDs:
-                    pixieNode = self.__model.node(self.pixieHardPoint)
-                    pixieNode.attach(resourceRefs[self.pixieName])
-                if self.soundName != '':
-                    self.__sound = SoundGroups.g_instance.getSound3D(self.__modelMatrix, self.soundName)
-            except Exception:
-                LOG_CURRENT_EXCEPTION()
-                self.__model = None
-                return
 
-            self.__prevTime = BigWorld.time()
-            self.__update()
+        self.__clear()
+
+        if self.model_name in resource_refs.failedIDs:
             return
 
+        try:
+            self.__model = resource_refs[self.model_name]
+            self.__model_matrix = Matrix()
+            self.__model_matrix.setIdentity()
+
+            servo = BigWorld.Servo(self.__model_matrix)
+            self.__model.addMotor(servo)
+
+            BigWorld.addModel(self.__model)
+
+            if self.action_name != '':
+                clip_resource = self.__model.deprecatedGetAnimationClipResource(self.action_name)
+                if clip_resource:
+                    space_id = BigWorld.player().spaceID
+                    loader = AnimationSequence.Loader(clip_resource, space_id)
+                    animator = loader.loadSync()
+                    animator.bindTo(AnimationSequence.ModelWrapperContainer(self.__model, space_id))
+                    animator.start()
+                    self.__animator = animator
+
+            if self.pixie_name != '' and self.pixie_name not in resource_refs.failedIDs:
+                pixie_node = self.__model.node(self.pixie_hard_point)
+                pixie_node.attach(resource_refs[self.pixie_name])
+
+            if self.sound_name != '':
+                self.__sound = SoundGroups.g_instance.getSound3D(self.__model_matrix, self.sound_name)
+
+        except Exception:
+            LOG_CURRENT_EXCEPTION()
+            self.__model = None
+            return
+
+        self.__prev_time = BigWorld.time()
+        self.__update()
+
     def __update(self):
-        self.__updateCallbackId = None
-        self.__updateCallbackId = BigWorld.callback(0.0, self.__update)
-        curTime = BigWorld.time()
-        dt = curTime - self.__prevTime
-        self.__prevTime = curTime
-        self.__currentAngle += self.__angularVelocity * dt
-        if self.__currentAngle > 2 * math.pi:
-            self.__currentAngle -= 2 * math.pi
-        elif self.__currentAngle < -2 * math.pi:
-            self.__currentAngle += 2 * math.pi
-        radialPosition = Vector3(self.radius * math.sin(self.__currentAngle), 0, self.radius * math.cos(self.__currentAngle))
-        modelYaw = self.__currentAngle
-        if self.rotateClockwise:
-            modelYaw += math.pi / 2
+        """
+        Updates the flyer's position and orientation.
+        """
+        self.__update_callback_id = None
+        self.__update_callback_id = BigWorld.callback(0.0, self.__update)
+
+        cur_time = BigWorld.time()
+        dt = cur_time - self.__prev_time
+        self.__prev_time = cur_time
+
+        self.__current_angle += self.__angular_velocity * dt
+        if self.__current_angle > 2 * math.pi:
+            self.__current_angle -= 2 * math.pi
+        elif self.__current_angle < -2 * math.pi:
+            self.__current_angle += 2 * math.pi
+
+        radial_position = Vector3(self.radius * math.sin(self.__current_angle), 0, self.radius * math.cos(self.__current_angle))
+        model_yaw = self.__current_angle
+        if self.rotate_clockwise:
+            model_yaw += math.pi / 2
         else:
-            modelYaw -= math.pi / 2
-        localMatrix = Matrix()
-        localMatrix.setRotateY(modelYaw)
-        localMatrix.translation = radialPosition
-        self.__modelMatrix.setRotateYPR((self.yaw, self.pitch, self.roll))
-        self.__modelMatrix.translation = self.position
-        self.__modelMatrix.preMultiply(localMatrix)
-        return
+            model_yaw -= math.pi / 2
+
+        local_matrix = Matrix()
+        local_matrix.setRotateY(model_yaw)
+        local_matrix.translation = radial_position
+
+        self.__model_matrix.setRotateYPR((self.yaw, self.pitch, self.roll))
+        self.__model_matrix.translation = self.position
+
+        self.__model_matrix.preMultiply(local_matrix)
+

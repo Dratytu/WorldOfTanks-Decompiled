188d7071685dd4138063e64deba95f7908c96ccd
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 07:16:29 +0300

    improve

--- source/res/armory_yard/scripts/client/armory_yard/gui/game_control/__init__.py
+++ source/res/armory_yard/scripts/client/armory_yard/gui/game_control/__init__.py
@@ -1,13 +1,18 @@
-# Python bytecode 2.7 (decompiled from Python 2.7)
+# Python bytecode 3.7 (decompiled from Python 3.7)
 # Embedded file name: armory_yard/scripts/client/armory_yard/gui/game_control/__init__.py
 from armory_yard.gui.game_control.armory_yard_controller import ArmoryYardController
 from armory_yard.gui.game_control.awards_controller import ArmoryYardStyleQuestsHandler
 from skeletons.gui.game_control import IArmoryYardController
 from gui.shared.system_factory import registerGameControllers, registerAwardControllerHandler
 
-def registerAYGameControllers():
-    registerGameControllers([(IArmoryYardController, ArmoryYardController, False)])
+def register_ay_game_controllers():
+    register_game_controllers([(IArmoryYardController, ArmoryYardController, False)])
 
 
-def registerAYAwardControllers():
-    registerAwardControllerHandler(ArmoryYardStyleQuestsHandler)
+def register_ay_award_controllers():
+    register_award_controller_handler(ArmoryYardStyleQuestsHandler)
+
+
+# Register game and award controllers during the module initialization
+register_ay_game_controllers()
+register_ay_award_controllers()

8b28eb24c77c42a94e35e6c7932dbef4ffa7c7bc
Dratytu <161526589+Dratytu@users.noreply.github.com>
2024-03-18 08:02:19 +0300

    comment

--- source/res/battle_royale/scripts/client/ThunderStrike.py
+++ source/res/battle_royale/scripts/client/ThunderStrike.py
@@ -1,17 +1,30 @@
 # Python bytecode 2.7 (decompiled from Python 2.7)
 # Embedded file name: battle_royale/scripts/client/ThunderStrike.py
+
 import BigWorld
-from Event import Event
-from helpers.CallbackDelayer import CallbackDelayer
-from debug_utils import LOG_DEBUG_DEV
+from Event import Event  # Importing Event class from helpers.Event
+from helpers.CallbackDelayer import CallbackDelayer  # Importing CallbackDelayer class from helpers.CallbackDelayer
+from debug_utils import LOG_DEBUG_DEV  # Importing LOG_DEBUG_DEV for debug logging
 
 class ThunderStrike(BigWorld.Entity, CallbackDelayer):
-
+    
     def __init__(self):
+        """
+        Initialize the ThunderStrike class, which inherits from BigWorld.Entity and CallbackDelayer.
+        """
         super(ThunderStrike, self).__init__()
+        
+        # Log a debug message with the position and equipmentID of the ThunderStrike
         LOG_DEBUG_DEV('ArenaInfoThunderStrikeLauncher launched', self.position, self.equipmentID)
+        
+        # Create an Event object called onHit
         self.onHit = Event()
 
     def hitThunderStrike(self):
+        """
+        A method to handle the hit event for the ThunderStrike.
+        """
         LOG_DEBUG_DEV('hitThunderStrike')
+        
+        # Trigger the onHit event
         self.onHit()
